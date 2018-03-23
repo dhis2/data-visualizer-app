21d7d5049d79b45d8a0b15ecdacf72184260ae9c
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-03-23 13:59:36 +0100

    Component test framework

---
+++ config/testsContext.js
@@ -0,0 +1,20 @@
+const userSettings = {
+    keyDbLocale: 'ponyLang',
+};
+
+export function getStubContext() {
+    return {
+        i18n: {
+            t: () => {},
+        },
+        d2: {
+            currentUser: {
+                firstName: 'Rainbow',
+                surname: 'Dash',
+                userSettings: {
+                    get: key => userSettings[key],
+                },
+            },
+        },
+    };
+}
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+export class App extends Component {
     render() {
         return <div className="App">Data Visualizer App</div>;
     }
