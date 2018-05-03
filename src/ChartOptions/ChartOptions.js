8a0286f59eb84aaae6a2bc2536508d97c28572aa
Edoardo Sabadelli <edoardo@dhis2.org>
2018-05-03 13:13:48 +0200

    Render only the Options dialog content
    The title and hide button are part of the dialog.

--- src/ChartOptions/ChartOptions.js
+++ src/ChartOptions/ChartOptions.js
@@ -1,33 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui-next/styles';
-import Card, { CardContent } from 'material-ui-next/Card';
-import Button from 'material-ui-next/Button';
-import i18n from 'd2-i18n';
+
 import DataTab from './DataTab';
 import TabsBar from './TabsBar';
 import StyleTab from './StyleTab';
 import AxesAndLegendsTab from './AxesAndLegendsTab';
-
-export const styles = {
-    title: {
-        height: 24,
-        width: 747,
-    },
-    card: {
-        width: 795,
-        minWidth: 795,
-        height: 668,
-        minHeight: 668,
-    },
-    cardContent: {
-        height: 527,
-        width: 747,
-    },
-    hideButton: {
-        left: '90%',
-    },
-};
 
 export class ChartOptions extends Component {
     state = {
@@ -73,7 +50,6 @@ export class ChartOptions extends Component {
     };
 
     render = () => {
-        const { classes } = this.props;
         let showCurrentTab = [
             <DataTab
                 onChange={this.handleChange}
@@ -90,33 +66,23 @@ export class ChartOptions extends Component {
         ];
 
         return (
-            <Card className={classes.card}>
-                <CardContent className={classes.cardContent}>
-                    <h3 className={classes.title}>{i18n.t('Chart Options')}</h3>
-                    <TabsBar
-                        activeTab={this.state.activeTab}
-                        onChange={this.handleChange}
-                    />
-                    {showCurrentTab[this.state.activeTab]}
-                    <Button
-                        className={classes.hideButton}
-                        color={'primary'}
-                        size={'small'}
-                    >
-                        {i18n.t('Hide')}
-                    </Button>
-                </CardContent>
-            </Card>
+            <Fragment>
+                <TabsBar
+                    activeTab={this.state.activeTab}
+                    onChange={this.handleChange}
+                />
+                {showCurrentTab[this.state.activeTab]}
+            </Fragment>
         );
     };
 }
 
 ChartOptions.propTypes = {
     activeTab: PropTypes.number,
-    classes: PropTypes.object,
 };
 
 ChartOptions.defaultProps = {
     activeTab: 0,
 };
-export default withStyles(styles)(ChartOptions);
+
+export default ChartOptions;
