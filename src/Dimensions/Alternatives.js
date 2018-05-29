ed38f6ab703a70a78bd4cca73de7efcd4da1a168
joakimia <joakimamundsen91@gmail.com>
2018-05-29 09:39:06 +0200

    concat three first dimensions with the rest received from reducer

--- src/Dimensions/Alternatives.js
+++ src/Dimensions/Alternatives.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import { Star } from 'material-ui-icons';
-import * as fromReducers from '../reducers';
 
 const style = {
     // TODO: Move CSS into .css file when styling is done
@@ -11,7 +9,6 @@ const style = {
         paddingLeft: 12,
         height: 35,
     },
-
     iconStyle: {
         marginRight: 0,
     },
@@ -23,43 +20,34 @@ const style = {
     },
 };
 
-// these are missing from the API request
-//let testArr = ['Data', 'Period', 'Organisation Units'];
-
 export const Alternatives = ({ dimensions, searchFieldValue, onClick }) => {
     return (
         <List>
-            {dimensions
-                ? Object.entries(dimensions).map(
-                      (entry, index) =>
-                          entry[1].displayName
-                              .toLowerCase()
-                              .includes(searchFieldValue.toLowerCase()) ? (
-                              <ListItem
-                                  style={style.listItemStyle}
-                                  button
-                                  key={entry[1].id}
-                                  onClick={() => onClick(index)}
-                              >
-                                  <ListItemIcon style={style.iconStyle}>
-                                      {/* TODO: Replace placeholder for the icons displayed in the Dimension list */}
-                                      <Star />
-                                  </ListItemIcon>
-                                  <ListItemText
-                                      style={style.textStyle}
-                                      primary={entry[1].displayName}
-                                      disableTypography
-                                  />
-                              </ListItem>
-                          ) : null
-                  )
-                : null}
+            {dimensions.map(
+                (entry, index) =>
+                    entry
+                        .toLowerCase()
+                        .includes(searchFieldValue.toLowerCase()) ? (
+                        <ListItem
+                            style={style.listItemStyle}
+                            button
+                            key={index}
+                            onClick={() => onClick(index)}
+                        >
+                            <ListItemIcon style={style.iconStyle}>
+                                {/* TODO: Replace placeholder for the icons displayed in the Dimension list */}
+                                <Star />
+                            </ListItemIcon>
+                            <ListItemText
+                                style={style.textStyle}
+                                primary={entry}
+                                disableTypography
+                            />
+                        </ListItem>
+                    ) : null
+            )}
         </List>
     );
 };
 
-const mapStateToProps = state => ({
-    dimensions: fromReducers.fromDimensions.sGetFromState(state),
-});
-
-export default connect(mapStateToProps)(Alternatives);
+export default Alternatives;
--- src/Dimensions/Dimensions.js
+++ src/Dimensions/Dimensions.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import TextField from 'material-ui/TextField';
 import { Search } from 'material-ui-icons';
 import { colors } from '../colors';
 import Alternatives from './Alternatives';
 import DimensionsManager from './DimensionsManager';
 import i18n from '@dhis2/d2-i18n';
+import * as fromReducers from '../reducers';
+import * as fromActions from '../actions';
 
 const style = {
     divContainter: {
@@ -22,9 +25,14 @@ const style = {
 };
 
 export class Dimensions extends Component {
-    state = { searchFieldValue: '', dialogIsOpen: false };
+    state = {
+        searchFieldValue: '',
+        dialogIsOpen: false,
+        dimensions: ['Data', 'Periods', 'Organisation Units'],
+    };
 
     onClick = index => {
+        //this.props.tSetDimension('dimension', index); // (dispatch, getState)
         this.toggleDialog();
         this.setState({
             dialogIndexNum: index,
@@ -33,6 +41,17 @@ export class Dimensions extends Component {
 
     toggleDialog = () => {
         this.setState({ dialogIsOpen: !this.state.dialogIsOpen });
+    };
+
+    componentWillReceiveProps = nextProps => {
+        this.setState({
+            dimensions: [
+                ...this.state.dimensions,
+                ...Object.entries(nextProps.dimensions).map(
+                    entry => entry[1].displayName
+                ),
+            ],
+        });
     };
 
     render = () => {
@@ -53,6 +72,7 @@ export class Dimensions extends Component {
                 />
                 <Alternatives
                     searchFieldValue={this.state.searchFieldValue}
+                    dimensions={this.state.dimensions}
                     onClick={this.onClick}
                 />
             </div>
@@ -60,4 +80,11 @@ export class Dimensions extends Component {
     };
 }
 
-export default Dimensions;
+const mapStateToProps = state => ({
+    dimensions: fromReducers.fromDimensions.sGetFromState(state) || {},
+});
+
+export default connect(
+    mapStateToProps,
+    { tSetDimension: fromActions.fromDimensions.acSetDimensions }
+)(Dimensions);
