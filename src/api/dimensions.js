b1c1b640b602be53c2d03695f28b1f3653f19ba8
joakimia <joakimamundsen91@gmail.com>
2018-08-28 13:49:58 +0200

    adding api request for fetching recommended id

--- src/api/dimensions.js
+++ src/api/dimensions.js
@@ -12,3 +12,15 @@ export const apiFetchDimensions = () => {
         })
         .catch(onError);
 };
+
+// `/api/dimensions/recommendations?dimension=dx:idA;idB&dimension=ou:idA;idB`
+export const apiFetchRecommended = (idA, idB) => {
+    const fields = `dx:${idA};${idB}&dimension=ou:${idA};${idB}`,
+        url = `/dimensions/recommendations?dimensions=${fields}`;
+
+    return getInstance()
+        .then(d2 => {
+            return d2.Api.getApi().get(url);
+        })
+        .catch(onError);
+};
