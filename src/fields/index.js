2d0c11d4cebf19a5e2dbacc11de3baae4bf6c550
Jan Henrik Øverland <jan@dhis2.org>
2018-08-29 14:17:20 +0200

    Task/request fields (#17)
    * Add fields
    
    * Tmp
    
    * Add field objects
    
    * Add getters

--- src/fields/index.js
+++ src/fields/index.js
@@ -1,13 +1,29 @@
 import {
-    getAllFieldsByType,
-    excludeFields,
-    excludedFields,
+    getAllFieldObjectsByType,
+    extractName,
+    markExcluded,
+    moveExcludedToEnd,
 } from './baseFields';
 import { extendFields } from './nestedFields';
 
-export const getFieldsByType = type =>
-    getAllFieldsByType(type)
-        .filter(excludeFields)
+export { getAllFieldObjectsByType };
+
+export const getOptionsByType = type =>
+    getAllFieldObjectsByType(type).filter(fieldObj => fieldObj.option === true);
+
+export const getIncludedByType = type =>
+    getAllFieldObjectsByType(type).filter(fieldObj => !fieldObj.excluded);
+
+export const getExcludedByType = type =>
+    getAllFieldObjectsByType(type).filter(
+        fieldObj => fieldObj.excluded === true
+    );
+
+export const getFieldsStringByType = type =>
+    getAllFieldObjectsByType(type)
+        .map(markExcluded)
+        .map(extractName)
+        .sort()
+        .reduce(moveExcludedToEnd, null)
         .map(extendFields)
-        .concat(excludedFields.map(field => `!${field}`))
         .join(',');
