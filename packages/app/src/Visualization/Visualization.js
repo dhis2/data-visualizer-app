d04d38f7c05301b8e15e8d71b4b255f0d7010fe6
Edoardo Sabadelli <edoardo@dhis2.org>
2018-09-07 16:01:55 +0200

    Render chart from the current object in the store
    This uses d2's analytics.request and analytics.response classes and
    d2-charts-api for converting the AO into the config for highcharts

--- packages/app/src/Visualization/Visualization.js
+++ packages/app/src/Visualization/Visualization.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as fromReducers from '../reducers';
+import { createChart } from 'd2-charts-api';
 
-const Visualization = props => {
-    return (
-        <section className="canvas">
-            <div>Visualization Canvas</div>
-        </section>
-    );
+export class Visualization extends Component {
+    componentDidUpdate(prevProps) {
+        if (this.props.current !== prevProps.current) {
+            this.renderVisualization();
+        }
+    }
+
+    renderVisualization = async () => {
+        const { d2, current } = this.props;
+
+        if (current) {
+            const req = new d2.analytics.request().fromModel(current);
+
+            const rawResponse = await d2.analytics.aggregate.get(req);
+
+            const res = new d2.analytics.response(rawResponse);
+
+            createChart(res, current, 'visualization-container');
+        }
+    };
+
+    render() {
+        console.log('in viz render', this.props);
+        return (
+            <section className="canvas">
+                <div id="visualization-container">Visualization Canvas</div>
+            </section>
+        );
+    }
 };
 
-export default Visualization;
+const mapStateToProps = state => {
+    const current = fromReducers.fromCurrent.sGetFromState(state);
+
+    return { current };
+};
+
+export default connect(mapStateToProps)(Visualization);
