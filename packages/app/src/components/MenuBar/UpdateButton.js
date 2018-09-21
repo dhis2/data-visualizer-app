7a3669543aa918d28191360822b7ec43f180c75e
Jan Henrik Overland <jan@dhis2.org>
2018-09-21 16:07:25 +0200

    Use prop types

--- packages/app/src/components/MenuBar/UpdateButton.js
+++ packages/app/src/components/MenuBar/UpdateButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import i18n from '@dhis2/d2-i18n';
@@ -7,9 +8,14 @@ import { sGetUi } from '../../reducers/ui';
 import { colors } from '../../colors';
 import * as fromActions from '../../actions';
 
+const onClickWrapper = props => () => {
+    props.onUpdate(props.ui);
+    props.onClick();
+};
+
 const UpdateButton = props => (
     <Button
-        onClick={props.getOnClick(props.ui)}
+        onClick={onClickWrapper(props)}
         style={{
             backgroundColor: colors.accentPrimaryDark,
             color: colors.white,
@@ -26,12 +32,19 @@ const mapStateToProps = state => ({
     ui: sGetUi(state),
 });
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-    getOnClick: ui => () => {
-        dispatch(fromActions.fromCurrent.acSetCurrentFromUi(ui));
-        ownProps.onClick();
-    },
+const mapDispatchToProps = dispatch => ({
+    onUpdate: ui => dispatch(fromActions.fromCurrent.acSetCurrentFromUi(ui)),
 });
+
+UpdateButton.propTypes = {
+    ui: PropTypes.object.isRequired,
+    onUpdate: PropTypes.func.isRequired,
+    onClick: PropTypes.func,
+};
+
+UpdateButton.defaultProps = {
+    onClick: Function.prototype,
+};
 
 export default connect(
     mapStateToProps,
