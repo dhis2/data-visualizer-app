fe106f9756600617f2ec6ca06817afa7709f4f9c
Edoardo Sabadelli <edoardo@dhis2.org>
2018-09-27 15:03:21 +0200

    Link the onSave/onSaveAs callback to the save thunk

--- packages/app/src/components/MenuBar/MenuBar.js
+++ packages/app/src/components/MenuBar/MenuBar.js
@@ -6,12 +6,13 @@ import FileMenu from '@dhis2/d2-ui-file-menu';
 import UpdateButton from './UpdateButton';
 import VisualizationOptionsManager from '../VisualizationOptions/VisualizationOptionsManager';
 import * as fromActions from '../../actions';
+import { sGetCurrent } from '../../reducers/current';
 import './MenuBar.css';
 import history from '../../history';
 
 const onOpen = id => history.push(`/${id}`);
-
 const onNew = () => history.push('/');
+const getOnSave = props => details => props.onSaveVisualization(details);
 
 export const MenuBar = (props, context) => (
     <div className="menubar">
@@ -25,6 +26,8 @@ export const MenuBar = (props, context) => (
                 fileType={props.apiObjectName}
                 onOpen={onOpen}
                 onNew={onNew}
+                onSave={getOnSave(props)}
+                onSaveAs={getOnSave(props)}
                 onTranslate={() => console.log('translate callback')}
                 onError={() => console.log('error!')}
             />
@@ -43,12 +46,20 @@ MenuBar.contextTypes = {
     d2: PropTypes.object,
 };
 
+const mapStateToProps = state => ({
+    id: sGetCurrent(state).id,
+});
+
 const mapDispatchToProps = (dispatch, ownProps) => ({
     onLoadVisualizaton: id =>
         dispatch(fromActions.tDoLoadVisualization(ownProps.apiObjectName, id)),
+    onSaveVisualization: details =>
+        dispatch(
+            fromActions.tDoSaveVisualization(ownProps.apiObjectName, details)
+        ),
 });
 
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(MenuBar);
