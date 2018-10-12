4d854439f23aaa62937727ef1fcc7c4781fae173
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-10-12 10:36:04 +0200

    only render Tooltip if anchorEl is ready, and add tests

--- packages/app/src/components/Layout/Tooltip.js
+++ packages/app/src/components/Layout/Tooltip.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Popper from '@material-ui/core/Popper';
 import Paper from '@material-ui/core/Paper';
@@ -12,7 +13,7 @@ const labels = {
     noneSelected: i18n.t('None selected'),
 };
 
-class Tooltip extends React.Component {
+export class Tooltip extends React.Component {
     renderTooltip = names => (
         <Popper
             anchorEl={this.props.anchorEl}
@@ -60,6 +61,12 @@ class Tooltip extends React.Component {
         return displayNames.length ? this.renderTooltip(displayNames) : '';
     }
 }
+
+Tooltip.propTypes = {
+    open: PropTypes.bool.isRequired,
+    anchorEl: PropTypes.object.isRequired,
+    dimensionId: PropTypes.string.isRequired,
+};
 
 const mapStateToProps = (state, ownProps) => ({
     itemIds: sGetUiItems(state)[ownProps.dimensionId],
