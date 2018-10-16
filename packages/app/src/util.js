0af1b8b0738db2d7737ff740a1c84c9d991caf90
Jen Jones Arnesen <jjarnesen@gmail.com>
2018-10-16 18:53:55 +0200

    Keep arrayToIdMap pure, sort separately if needed (#46)
    Also fixes error in data dimension (SELECT ALL)
    
    [DHIS2-4944]

--- packages/app/src/util.js
+++ packages/app/src/util.js
@@ -2,7 +2,7 @@ import isObject from 'd2-utilizr/lib/isObject';
 
 // TODO, copied from dashboards-app
 export function arrayToIdMap(array) {
-    return sortArray(array).reduce((obj, item) => {
+    return array.reduce((obj, item) => {
         obj[item.id] = item;
         return obj;
     }, {});
