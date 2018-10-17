abeb1d8f07932a84704d75a99bf5c76303a4bc22
Joakim Amundsen <jamundsen@dhis2.org>
2018-10-17 11:20:25 +0200

    Fix/split recommendedicon file (#49)
    * moving icons.js to root folder, creating new file RecommendedIcon.js for the Dimension component
    
    * move styling consts to sub-folder, change unit tests

---
+++ packages/app/src/components/Dimensions/RecommendedIcon.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import i18n from '@dhis2/d2-i18n';
+import * as fromReducers from '../../reducers';
+import { styles } from './styles/RecommendedIcon.style';
+
+export class RecommendedIcon extends Component {
+    state = { anchorEl: null };
+
+    onMouseOver = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    onMouseExit = () => {
+        this.setState({ anchorEl: null });
+    };
+
+    checkIfRecommended = () => {
+        const { isRecommended, isSelected, id } = this.props;
+        return isRecommended.includes(id) && !isSelected;
+    };
+
+    showTooltip = () => {
+        const HINT_TEXT = i18n.t('Dimension recommended with selected data');
+
+        return (
+            <Popper
+                anchorEl={this.state.anchorEl}
+                open={Boolean(this.state.anchorEl)}
+                placement="bottom"
+            >
+                <Paper style={styles.toolTip}>{HINT_TEXT}</Paper>
+            </Popper>
+        );
+    };
+
+    render = () => {
+        const TooltipOnHover = Boolean(this.state.anchorEl)
+            ? this.showTooltip()
+            : null;
+
+        return this.checkIfRecommended() ? (
+            <div
+                style={styles.recommendedIcon}
+                onMouseOver={this.onMouseOver}
+                onMouseLeave={this.onMouseExit}
+            >
+                {TooltipOnHover}
+            </div>
+        ) : null;
+    };
+}
+
+const mapStateToProps = state => ({
+    isRecommended: fromReducers.fromRecommendedIds.sGetRecommendedIds(state),
+});
+
+RecommendedIcon.propTypes = {
+    id: PropTypes.string.isRequired,
+    isSelected: PropTypes.bool.isRequired,
+    isRecommended: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps)(RecommendedIcon);
---
+++ packages/app/src/components/Dimensions/styles/DimensionOptions.style.js
@@ -0,0 +1,12 @@
+export const styles = {
+    wrapper: {
+        height: 24,
+    },
+    dropDownButton: {
+        border: 'none',
+        background: 'none',
+        outline: 'none',
+        padding: 0,
+        height: 24,
+    },
+};
