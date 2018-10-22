87c4539593dedc42d23c2540225953de7ff82d2e
Jan Henrik Overland <jan@dhis2.org>
2018-10-22 12:55:29 +0200

    Add dimension option style

--- packages/app/src/components/Dimensions/styles/DimensionOptions.style.js
+++ packages/app/src/components/Dimensions/styles/DimensionOptions.style.js
@@ -3,10 +3,12 @@ export const styles = {
         height: 24,
     },
     dropDownButton: {
+        marginLeft: '6px',
         border: 'none',
         background: 'none',
         outline: 'none',
         padding: 0,
         height: 24,
+        cursor: 'pointer',
     },
 };
