fca30f2140528e592f6f615bbbf49d308d72d8d0
Jan Henrik Overland <jan@dhis2.org>
2018-10-22 12:47:37 +0200

    Add minor style fixes

--- packages/app/src/components/Dimensions/styles/Dimensions.style.js
+++ packages/app/src/components/Dimensions/styles/Dimensions.style.js
@@ -2,7 +2,7 @@ import { colors } from '../../../colors';
 
 export const styles = {
     divContainer: {
-        backgroundColor: colors.lightGrey,
+        backgroundColor: colors.snow,
         width: 250,
         padding: '12px 0 12px 12px',
     },
