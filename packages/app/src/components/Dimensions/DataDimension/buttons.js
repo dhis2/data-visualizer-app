862b2eb7b327cab8861f8164944fc3abcf874fad
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-10-22 11:06:47 +0200

    treeshaking for mui core

--- packages/app/src/components/Dimensions/DataDimension/buttons.js
+++ packages/app/src/components/Dimensions/DataDimension/buttons.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ArrowForward from '@material-ui/icons/ArrowForward';
 import ArrowBack from '@material-ui/icons/ArrowBack';
 import Close from '@material-ui/icons/Close';
-import { Button } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import i18n from '@dhis2/d2-i18n';
 import { colors } from '../../../colors';
 
