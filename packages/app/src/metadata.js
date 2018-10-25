5224c2a7d6c950f327fa84380ebf751ad4ac4311
Viktor Varland <viktor@vardevs.se>
2018-10-25 10:47:33 +0200

    chore: revert unintentional change on parenthesis

--- packages/app/src/metadata.js
+++ packages/app/src/metadata.js
@@ -49,4 +49,4 @@ export default (function() {
         ...relativePeriods,
         ...organisationUnits,
     }).reduce((obj, [key, value]) => ({ ...obj, [key]: { name: value } }), {});
-}());
+})();
