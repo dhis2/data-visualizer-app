c0841646272db091a0ac124317b8a86f7a108c9a
Jan Henrik Overland <jan@dhis2.org>
2018-10-25 16:07:07 +0200

    Add yoy reducer

--- packages/app/src/reducers/current.js
+++ packages/app/src/reducers/current.js
@@ -6,21 +6,47 @@ export const CLEAR_CURRENT = 'CLEAR_CURRENT';
 
 export const DEFAULT_CURRENT = {};
 
+const getYearOnYearCurrent = (state, action) => ({
+    ...state,
+    yearlySeries: action.value.yearOnYearSeries,
+    columns: [
+        {
+            dimension: 'pe',
+            items: [{ id: action.value.yearOnYearCategory }],
+        },
+    ],
+    rows: [
+        {
+            dimension: 'dx',
+            items: [{ id: action.value.itemsByDimension.dx[0] }],
+        },
+    ],
+    filters: getAxesFromUi(action.value).filters,
+    ...getOptionsFromUi(action.value),
+    type: action.value.type,
+});
+
 export default (state = DEFAULT_CURRENT, action) => {
     switch (action.type) {
         case SET_CURRENT: {
             return action.value;
         }
         case SET_CURRENT_FROM_UI: {
-            const axesFromUi = getAxesFromUi(action.value);
-            const optionsFromUi = getOptionsFromUi(action.value);
+            switch (action.value.type) {
+                case 'YEAR_ON_YEAR':
+                    return getYearOnYearCurrent(state, action);
+                default: {
+                    const axesFromUi = getAxesFromUi(action.value);
+                    const optionsFromUi = getOptionsFromUi(action.value);
 
-            return {
-                ...state,
-                ...axesFromUi,
-                ...optionsFromUi,
-                type: action.value.type,
-            };
+                    return {
+                        ...state,
+                        ...axesFromUi,
+                        ...optionsFromUi,
+                        type: action.value.type,
+                    };
+                }
+            }
         }
         case CLEAR_CURRENT:
             return DEFAULT_CURRENT;
