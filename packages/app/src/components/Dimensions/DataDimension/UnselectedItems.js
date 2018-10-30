1135123dfdd28e03f315395987c674e3e57d370c
Joakim Amundsen <jamundsen@dhis2.org>
2018-10-30 11:10:31 +0100

    Fix/data dimension toggling [DHIS2-4957] (#77)
    This PR includes a new file Item.js in the DataDimension folder, and toggler.js  in modules folder. 
    The multi-select behavior is similar to the current data visualizer ( you can press shift  / meta key ).

--- packages/app/src/components/Dimensions/DataDimension/UnselectedItems.js
+++ packages/app/src/components/Dimensions/DataDimension/UnselectedItems.js
@@ -1,53 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import throttle from 'lodash-es/throttle';
+import { Item } from './Item';
 import { AssignButton, SelectAllButton } from './buttons';
-import { colors } from '../../../modules/colors';
-
-const style = {
-    container: {
-        border: '1px solid #E0E0E0',
-        height: 376,
-    },
-    listContainer: {
-        listStyle: 'none',
-        overflowX: 'scroll',
-        height: 340,
-        width: 418,
-        borderBottom: 0,
-        paddingLeft: 0,
-        margin: 0,
-        userSelect: 'none',
-    },
-    listItem: {
-        display: 'flex',
-        margin: 5,
-    },
-    highlighted: {
-        backgroundColor: '#92C9F7',
-        borderRadius: 4,
-    },
-    unHighlighted: {
-        display: 'flex',
-        padding: 2,
-    },
-    text: {
-        fontFamily: 'Roboto',
-        fontSize: 14,
-        paddingLeft: 2,
-        paddingRight: 2,
-    },
-    icon: {
-        backgroundColor: colors.grey,
-        height: 6,
-        width: 6,
-        marginTop: 4,
-        marginLeft: 10,
-        marginRight: 5,
-    },
-};
-
-const UnselectedIcon = () => <div style={style.icon} />;
+import { toggler } from '../../../modules/toggler';
+import { styles } from './styles/UnselectedItems.style';
 
 export class UnselectedItems extends Component {
     constructor(props) {
@@ -55,7 +12,7 @@ export class UnselectedItems extends Component {
         this.ulRef = React.createRef();
     }
 
-    state = { highlighted: [] };
+    state = { highlighted: [], lastClickedIndex: 0 };
 
     onSelectClick = () => {
         this.props.onSelect(this.state.highlighted);
@@ -67,38 +24,49 @@ export class UnselectedItems extends Component {
         this.setState({ highlighted: [] });
     };
 
-    toggleHighlight = id => {
-        const higlightedItems = this.state.highlighted.includes(id)
-            ? this.state.highlighted.filter(
-                  dataDimId => dataDimId !== id && dataDimId
-              )
-            : [...this.state.highlighted, id];
-
-        this.setState({ highlighted: higlightedItems });
-    };
-
-    onDoubleClickItem = id => this.props.onSelect([id]);
-
-    renderListItem = dataDim => {
-        const itemStyle = this.state.highlighted.includes(dataDim.id)
-            ? { ...style.unHighlighted, ...style.highlighted }
-            : style.unHighlighted;
-
-        return (
-            <li
-                className="dimension-item"
-                key={dataDim.id}
-                style={style.listItem}
-                onDoubleClick={() => this.onDoubleClickItem(dataDim.id)}
-                onClick={() => this.toggleHighlight(dataDim.id)}
-            >
-                <div style={itemStyle}>
-                    <UnselectedIcon />
-                    <span style={style.text}>{dataDim.name}</span>
-                </div>
-            </li>
+    toggleHighlight = (isCtrlPressed, isShiftPressed, index, id) => {
+        const newState = toggler(
+            id,
+            isCtrlPressed,
+            isShiftPressed,
+            index,
+            this.state.lastClickedIndex,
+            this.state.highlighted,
+            this.props.items.map(item => item.id)
         );
+
+        this.setState({
+            highlighted: newState.ids,
+            lastClickedIndex: newState.lastClickedIndex,
+        });
     };
+
+    onDoubleClickItem = id => {
+        const highlighted = this.state.highlighted.filter(
+            dataDimId => dataDimId !== id
+        );
+
+        this.setState({ highlighted });
+        this.props.onSelect([id]);
+    };
+
+    renderListItem = (dataDim, index) => (
+        <li
+            className="dimension-item"
+            key={dataDim.id}
+            style={styles.listItem}
+            onDoubleClick={() => this.onDoubleClickItem(dataDim.id)}
+        >
+            <Item
+                id={dataDim.id}
+                index={index}
+                displayName={dataDim.name}
+                isHighlighted={!!this.state.highlighted.includes(dataDim.id)}
+                onItemClick={this.toggleHighlight}
+                className={'unselected'}
+            />
+        </li>
+    );
 
     requestMoreItems = throttle(() => {
         const node = this.ulRef.current;
@@ -113,11 +81,13 @@ export class UnselectedItems extends Component {
     }, 1000);
 
     render = () => {
-        const listItems = this.props.items.map(i => this.renderListItem(i));
+        const listItems = this.props.items.map((item, index) =>
+            this.renderListItem(item, index)
+        );
 
         return (
-            <div style={style.container} onScroll={this.requestMoreItems}>
-                <ul ref={this.ulRef} style={style.listContainer}>
+            <div style={styles.container} onScroll={this.requestMoreItems}>
+                <ul ref={this.ulRef} style={styles.listContainer}>
                     {listItems}
                 </ul>
                 <AssignButton action={this.onSelectClick} />
