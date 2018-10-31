5e36bcc87adfaab5eea2e301302905bdf711d392
Ilya Nee <neeilya@mail.ru>
2018-10-31 14:32:54 +0100

    Api function for fetching org unit root & disable fetching schemas for groups/levels

--- packages/app/src/components/Dimensions/OrgUnitDimension/OrgUnitDimension.js
+++ packages/app/src/components/Dimensions/OrgUnitDimension/OrgUnitDimension.js
@@ -107,16 +107,6 @@ export class OrgUnitDimension extends Component {
         apiFetchOrganisationUnits()
             .then(rootLevel => rootLevel.toArray()[0])
             .then(root => {
-                if (this.props.metadata[root.id] === undefined) {
-                    this.props.acAddMetadata({
-                        [root.id]: {
-                            id: root.id,
-                            displayName: root.displayName,
-                            name: root.name,
-                        },
-                    });
-                }
-
                 this.setState({
                     root,
                 });
@@ -125,9 +115,11 @@ export class OrgUnitDimension extends Component {
 
     loadOrgUnitGroups = () => {
         apiFetchOrganisationUnitGroups()
-            .then(collection => collection.toArray())
-            .then(options =>
-                transformOptionsIntoMetadata(options, this.props.metadata)
+            .then(organisationUnitGroups =>
+                transformOptionsIntoMetadata(
+                    organisationUnitGroups,
+                    this.props.metadata
+                )
             )
             .then(({ options, metadata }) => {
                 this.props.acAddMetadata(metadata);
@@ -137,10 +129,9 @@ export class OrgUnitDimension extends Component {
 
     loadOrgUnitLevels = () => {
         apiFetchOrganisationUnitLevels()
-            .then(collection => collection.toArray())
-            .then(levelOptions =>
+            .then(organisationUnitLevels =>
                 transformOptionsIntoMetadata(
-                    levelOptions,
+                    organisationUnitLevels,
                     this.props.metadata,
                     ['id', 'displayName', 'name', 'level']
                 )
