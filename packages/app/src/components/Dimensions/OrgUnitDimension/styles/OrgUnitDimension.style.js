c564143d8148cfce62afb25ede8ccc7a8322ffea
Ilya Nee <neeilya@mail.ru>
2018-10-31 10:39:16 +0100

    improvement/org unit dimension (#75)
    * Moving org unit dimension component into dedicated folder
    * Extractong dialog buttons & styles into separate files
    * Changes in rendering approach for dialog dimensions ("lazy" mount)
    * Indicating org unit tree loading with spinner
    * Tests for DialogManager
    * Tests for OrgUnitDimension

---
+++ packages/app/src/components/Dimensions/OrgUnitDimension/styles/OrgUnitDimension.style.js
@@ -0,0 +1,12 @@
+export default {
+    dialogContent: {
+        fontFamily: '"Roboto", sans-serif',
+        width: 760,
+    },
+    loader: {
+        marginTop: 40,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        display: 'block',
+    },
+};
