3e61b85e50e08def946f233f7475da2ea5e9f70a
Edoardo Sabadelli <edoardo@dhis2.org>
2018-11-06 12:28:30 +0100

    Fix CSS for grid cell collapse
    Basically it seems the highchart container (#visualization-container) must be removed from the flow
    in order to get the CSS grid to resize the parent container (#canvas).

--- packages/app/src/components/Visualization/styles/BlankCanvas.style.js
+++ packages/app/src/components/Visualization/styles/BlankCanvas.style.js
@@ -5,6 +5,8 @@ export default {
         display: 'flex',
         justifyContent: 'center',
         height: '100%',
+        width: '100%',
+        position: 'absolute',
     },
     inner: {
         maxWidth: 200,
