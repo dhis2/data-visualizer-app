6508e5e6bf9bfdd4ab4bf25e091d2ccc51f9cc9a
Edoardo Sabadelli <edoardo@dhis2.org>
2018-11-06 09:34:10 +0100

    Fix prettier

--- packages/app/src/assets/YearOverYearColumnIcon.js
+++ packages/app/src/assets/YearOverYearColumnIcon.js
@@ -7,15 +7,15 @@ const YearOverYearColumnIcon = ({
     <SvgIcon viewBox="0,0,48,48" style={style}>
         <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
             <g>
-                <polygon points="0 0 48 0 48 48 0 48"></polygon>
-                <polygon fill="#63A4FF" points="6 14 11 14 11 46 6 46"></polygon>
-                <polygon fill="#63A4FF" points="20 23 25 23 25 46 20 46"></polygon>
-                <polygon fill="#63A4FF" points="34 30 39 30 39 46 34 46"></polygon>
-                <polygon fill="#004BA0" points="11 26 16 26 16 46 11 46"></polygon>
-                <polygon fill="#004BA0" points="25 16 30 16 30 46 25 46"></polygon>
-                <polygon fill="#004BA0" points="39 9 44 9 44 46 39 46"></polygon>
-                <polygon fill="#9E9E9E" points="0 0 2 0 2 48 0 48"></polygon>
-                <polygon fill="#9E9E9E" points="0 46 48 46 48 48 0 48"></polygon>
+                <polygon points="0 0 48 0 48 48 0 48" />
+                <polygon fill="#63A4FF" points="6 14 11 14 11 46 6 46" />
+                <polygon fill="#63A4FF" points="20 23 25 23 25 46 20 46" />
+                <polygon fill="#63A4FF" points="34 30 39 30 39 46 34 46" />
+                <polygon fill="#004BA0" points="11 26 16 26 16 46 11 46" />
+                <polygon fill="#004BA0" points="25 16 30 16 30 46 25 46" />
+                <polygon fill="#004BA0" points="39 9 44 9 44 46 39 46" />
+                <polygon fill="#9E9E9E" points="0 0 2 0 2 48 0 48" />
+                <polygon fill="#9E9E9E" points="0 46 48 46 48 48 0 48" />
             </g>
         </g>
     </SvgIcon>
