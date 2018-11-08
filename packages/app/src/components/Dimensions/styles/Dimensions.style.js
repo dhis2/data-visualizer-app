d9be53abedfd67cf1aef20fb868f624e2ecf7333
Jan Henrik Overland <jan@dhis2.org>
2018-11-08 17:32:09 +0100

    textfield

--- packages/app/src/components/Dimensions/styles/Dimensions.style.js
+++ packages/app/src/components/Dimensions/styles/Dimensions.style.js
@@ -4,10 +4,10 @@ export const styles = {
     divContainer: {
         backgroundColor: colors.snow,
         padding: '8px 0 8px 8px',
-        // padding: '12px 0px 12px 12px',
     },
     searchIcon: {
         color: colors.grey,
+        padding: '3px',
     },
     textField: {
         marginBottom: '12px',
