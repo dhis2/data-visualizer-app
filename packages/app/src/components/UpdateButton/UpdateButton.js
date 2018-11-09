e8e21986aaea64d471cac6e73b55ad881de67b71
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-11-09 15:42:05 +0100

    clear interp when updating ui

--- packages/app/src/components/UpdateButton/UpdateButton.js
+++ packages/app/src/components/UpdateButton/UpdateButton.js
@@ -5,11 +5,16 @@ import Button from '@material-ui/core/Button';
 import i18n from '@dhis2/d2-i18n';
 
 import { sGetUi } from '../../reducers/ui';
+import { sGetCurrent } from '../../reducers/current';
 import * as fromActions from '../../actions';
+import history from '../../modules/history';
 import styles from './styles/UpdateButton.style';
 
 const onClickWrapper = props => () => {
     props.onUpdate(props.ui);
+    if (props.current && props.current.id) {
+        history.push(`/${props.current.id}`);
+    }
     props.onClick();
 };
 
@@ -27,6 +32,7 @@ const UpdateButton = props => (
 
 const mapStateToProps = state => ({
     ui: sGetUi(state),
+    current: sGetCurrent(state),
 });
 
 const mapDispatchToProps = dispatch => ({
