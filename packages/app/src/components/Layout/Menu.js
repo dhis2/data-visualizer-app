bab7819d91550f3d663126f11c04b1243a572335
Jan Henrik Overland <jan@dhis2.org>
2018-11-09 13:21:26 +0100

    remove unused import

--- packages/app/src/components/Layout/Menu.js
+++ packages/app/src/components/Layout/Menu.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Menu from '@material-ui/core/Menu';
 import IconButton from '@material-ui/core/IconButton';
-import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
 import MoreHorizontalIcon from '../../assets/MoreHorizontalIcon';
 import { styles } from './styles/Menu.style';
 
