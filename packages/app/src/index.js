39f0276a62cb9cde6eb305f35c82b18a1758f0e0
Edoardo Sabadelli <edoardo@dhis2.org>
2018-11-12 14:29:20 +0100

    Add translations for sharing/translate dialogs
    Keep them separated from the automatically generated ones

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -22,10 +22,12 @@ const configI18n = async userSettings => {
     const uiLocale = userSettings.uiLocale;
 
     if (uiLocale && uiLocale !== 'en') {
-        config.i18n.sources.add(`./i18n/i18n_module_${uiLocale}.properties`);
+        config.i18n.sources.add(
+            `./i18n_old/i18n_module_${uiLocale}.properties`
+        );
     }
 
-    config.i18n.sources.add('./i18n/i18n_module_en.properties');
+    config.i18n.sources.add('./i18n_old/i18n_module_en.properties');
     i18n.changeLanguage(uiLocale);
 };
 
