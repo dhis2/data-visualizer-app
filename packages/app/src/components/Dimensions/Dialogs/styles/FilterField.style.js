caca5b728366c6e297bb0ccf64ba8dd878d5b4cc
joakimia <joakimamundsen91@gmail.com>
2018-11-14 13:53:36 +0100

    remove unused styling rules

--- packages/app/src/components/Dimensions/Dialogs/styles/FilterField.style.js
+++ packages/app/src/components/Dimensions/Dialogs/styles/FilterField.style.js
@@ -1,12 +1,4 @@
-import { colors } from '../../../../modules/colors';
-
 export const styles = {
-    container: {
-        border: `1px solid ${colors.greyLight}`,
-        backgroundColor: colors.white,
-        height: 39,
-        borderBottom: 0,
-    },
     textField: {
         paddingRight: '7px',
         paddingTop: '5px',
@@ -14,15 +6,12 @@ export const styles = {
         fontSize: '14px',
     },
     searchIcon: {
-        paddingTop: 5,
-        paddingLeft: 5,
+        paddingTop: '5px',
+        paddingLeft: '5px',
     },
-
-    closeIcon: {},
-
     placeholder: {
-        fontSize: 14,
-        paddingTop: 12,
-        paddingLeft: 5,
+        fontSize: '14px',
+        paddingTop: '12px',
+        paddingLeft: '5px',
     },
 };
