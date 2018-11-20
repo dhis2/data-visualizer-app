324bb3192efcec9b36b0f698baf2ec35799b502a
Jan Henrik Øverland <jan@dhis2.org>
2018-11-20 14:31:59 +0100

    Rewrite from grid to flex (#129)
    * Replace grid
    
    * Adjust canvas
    
    * Test resize
    
    * Fix max-height problem
    
    * Add back outline rule
    
    * Add ipt scrollbar
    
    * Fix layout overflow
    
    * Remove test code
    
    * Cleanup
    
    * Use flex instead of explicit height
    
    * Update Vis test
    
    * Remove empty css
    
    * Remove commented css
    
    * Remove additional debounce dep
    
    * Remove unused prop

--- packages/app/src/components/Dimensions/styles/DimensionList.style.js
+++ packages/app/src/components/Dimensions/styles/DimensionList.style.js
@@ -1,6 +1,6 @@
 export const styles = {
     listContainer: {
-        height: 'calc(100vh - 157px)',
+        flex: 1,
         overflow: 'auto',
         padding: '0 8px 0 0',
         marginTop: '0px',
