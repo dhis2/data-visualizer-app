e22fd4fe82bcd1377a1707ab6c819fd00115b8d4
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-11-21 13:57:58 +0100

    styling

--- packages/app/src/components/Dimensions/Dialogs/buttons/RemoveDimensionButton.js
+++ packages/app/src/components/Dimensions/Dialogs/buttons/RemoveDimensionButton.js
@@ -4,24 +4,20 @@ import IconButton from '@material-ui/core/IconButton';
 import Close from '@material-ui/icons/Close';
 import { styles } from '../styles/buttons.style';
 
-export const RemoveDimensionButton = ({ showButton, isHighlighted, onClick }) =>
-    showButton === 'selected' ? (
-        <IconButton
-            style={styles.deleteButton}
-            disableRipple
-            onClick={e => {
-                e.stopPropagation();
-                onClick();
-            }}
-        >
-            <Close
-                style={isHighlighted ? styles.highlightedClose : styles.close}
-            />
-        </IconButton>
-    ) : null;
+export const RemoveDimensionButton = ({ isHighlighted, onClick }) => (
+    <IconButton
+        style={styles.deleteButton}
+        disableRipple
+        onClick={e => {
+            e.stopPropagation();
+            onClick();
+        }}
+    >
+        <Close style={isHighlighted ? styles.highlightedClose : styles.close} />
+    </IconButton>
+);
 
 RemoveDimensionButton.propTypes = {
-    showButton: PropTypes.string.isRequired,
     isHighlighted: PropTypes.bool.isRequired,
     onClick: PropTypes.func.isRequired,
 };
