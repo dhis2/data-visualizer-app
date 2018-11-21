4a967a33ace99eb9475b5c06ca6a2188bec32609
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-11-21 10:15:08 +0100

    sorting on SelectedItems

--- packages/app/src/components/Dimensions/Dialogs/SelectedItems.js
+++ packages/app/src/components/Dimensions/Dialogs/SelectedItems.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import i18n from '@dhis2/d2-i18n';
-import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import {
+    SortableContainer,
+    SortableElement,
+    arrayMove,
+} from 'react-sortable-hoc';
 
 import { Item } from './Item';
 import { UnAssignButton, DeselectAllButton } from './buttons';
 
 import { sGetMetadata } from '../../../reducers/metadata';
-
 import { toggler } from '../../../modules/toggler';
+
 import { styles } from './styles/SelectedItems.style';
 
 const Subtitle = () => (
@@ -18,6 +22,47 @@ const Subtitle = () => (
     </div>
 );
 
+const SortableItem = SortableElement(({ id, ...props }) => {
+    return (
+        <li
+            className="dimension-item"
+            id={id}
+            onDoubleClick={() => props.removeSelected(id)}
+        >
+            <Item
+                logme
+                id={id}
+                index={props.idx}
+                displayName={props.name}
+                isHighlighted={props.isHighlighted}
+                onItemClick={props.toggleHighlight}
+                onRemoveItem={props.removeSelected}
+                className="selected"
+            />
+        </li>
+    );
+});
+
+const SortableList = SortableContainer(props => {
+    return (
+        <ul style={props.style}>
+            {props.items.map((id, index) => {
+                return (
+                    <SortableItem
+                        id={id}
+                        index={index}
+                        idx={index}
+                        key={`item-${id}`}
+                        name={props.metadata[id].name}
+                        isHighlighted={props.highlighted.includes(id)}
+                        removeSelected={props.removeSelected}
+                        toggleHighlight={props.toggleHighlight}
+                    />
+                );
+            })}
+        </ul>
+    );
+});
 export class SelectedItems extends Component {
     state = { highlighted: [], lastClickedIndex: 0 };
 
@@ -56,37 +101,26 @@ export class SelectedItems extends Component {
         });
     };
 
-    renderListItem = (id, index) => (
-        <li
-            className="dimension-item"
-            id={id}
-            key={id}
-            onDoubleClick={() => this.onRemoveSelected(id)}
-        >
-            <Item
-                id={id}
-                index={index}
-                displayName={this.props.metadata[id].name}
-                isHighlighted={!!this.state.highlighted.includes(id)}
-                onItemClick={this.toggleHighlight}
-                onRemoveItem={this.onRemoveSelected}
-                className="selected"
-            />
-        </li>
-    );
+    onSortEnd = ({ oldIndex, newIndex }) => {
+        this.props.onReorder(arrayMove(this.props.items, oldIndex, newIndex));
+    };
 
     renderUl = items => <ul style={styles.list}>{items}</ul>;
 
     render = () => {
-        const dataDimensions = this.props.items.map((id, index) =>
-            SortableElement(this.renderListItem(id, index))
-        );
+        const listProps = {
+            style: styles.list,
+            items: this.props.items,
+            removeSelected: this.onRemoveSelected,
+            toggleHighlight: this.toggleHighlight,
+            metadata: this.props.metadata,
+            highlighted: this.state.highlighted,
+        };
 
         return (
             <div style={styles.container}>
                 <Subtitle />
-                {/* <ul style={styles.list}>{dataDimensions}</ul> */}
-                {SortableContainer(this.renderUl(dataDimensions))}
+                <SortableList distance={3} {...listProps} />
                 <UnAssignButton
                     className={this.props.className}
                     action={this.onDeselectClick}
@@ -100,6 +134,7 @@ export class SelectedItems extends Component {
 SelectedItems.propTypes = {
     items: PropTypes.array.isRequired,
     onDeselect: PropTypes.func.isRequired,
+    onReorder: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
