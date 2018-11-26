a9a7188e7e8927a93c774ad5ed26c9cb4fcc085e
joakimia <joakimamundsen91@gmail.com>
2018-11-26 18:14:22 +0100

    use getBoundingClientRect and marginTreshold to render dropDown over AddToLayoutButton if Menu is overflowing

--- packages/app/src/components/AddToLayoutButton/Menu.js
+++ packages/app/src/components/AddToLayoutButton/Menu.js
@@ -11,23 +11,29 @@ export const DropDown = ({
     onClick,
     onClose,
     menuItems,
-}) => (
-    <Fragment>
-        <DropDownButton onClick={onClick} open={Boolean(anchorEl)} />
-        <Menu
-            open={Boolean(anchorEl)}
-            onClose={onClose}
-            anchorEl={anchorEl}
-            getContentAnchorEl={null}
-            MenuListProps={{ className: classes.menuList }}
-            anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        >
-            {menuItems}
-        </Menu>
-    </Fragment>
-);
+}) => {
+    const isOverFowing =
+        anchorEl !== null &&
+        anchorEl.getBoundingClientRect().bottom + 100 > window.innerHeight;
 
+    return (
+        <Fragment>
+            <DropDownButton onClick={onClick} open={Boolean(anchorEl)} />
+            <Menu
+                open={Boolean(anchorEl)}
+                onClose={onClose}
+                anchorEl={anchorEl}
+                getContentAnchorEl={null}
+                MenuListProps={{ className: classes.menuList }}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                marginThreshold={isOverFowing ? 100 : 0}
+            >
+                {menuItems}
+            </Menu>
+        </Fragment>
+    );
+};
 DropDown.defaultProps = {
     anchorEl: null,
 };
