5eef3d8658d9e7e09309b6ddc8ec3e042cac2593
Austin McGee <>
2018-11-29 10:53:05 +0100

    Run prettier on test file

--- packages/app/src/components/ErrorBoundaries/__tests__/FatalErrorBoundary.spec.js
+++ packages/app/src/components/ErrorBoundaries/__tests__/FatalErrorBoundary.spec.js
@@ -3,36 +3,37 @@ import FatalErrorBoundary from '../FatalErrorBoundary';
 import { mount } from 'enzyme';
 
 const Something = () => {
-  // Placeholder
-  return null;
-}
+    // Placeholder
+    return null;
+};
 
 describe('FatalErrorBoundary', () => {
-  it('Should render the normal tree when no error occurs', () => {
-    const wrapper = mount(
-      <FatalErrorBoundary>
-        <div>
-          <span id="testme">Hello there!</span>
-        </div>
-      </FatalErrorBoundary>
-    );
+    it('Should render the normal tree when no error occurs', () => {
+        const wrapper = mount(
+            <FatalErrorBoundary>
+                <div>
+                    <span id="testme">Hello there!</span>
+                </div>
+            </FatalErrorBoundary>
+        );
 
-    expect(wrapper.find('span#testme').length).toBe(1);
-  })
+        expect(wrapper.find('span#testme').length).toBe(1);
+    });
 
-  it('Should render the error mask when an error is thrown', () => {
-    const wrapper = mount(
-      <FatalErrorBoundary>
-        <Something />
-      </FatalErrorBoundary>
-    )
+    it('Should render the error mask when an error is thrown', () => {
+        const wrapper = mount(
+            <FatalErrorBoundary>
+                <Something />
+            </FatalErrorBoundary>
+        );
 
-    expect(wrapper.find(Something).length).toBe(1);
+        expect(wrapper.find(Something).length).toBe(1);
 
-    const testErrorText = 'This is a test of the emergency alert system.  This is only a test.';
-    wrapper.find(Something).simulateError(new Error(testErrorText)); 
-    console.log(wrapper.debug());
-    expect(wrapper.contains('Something went wrong')).toBe(true);
-    expect(wrapper).toMatchSnapshot();
-  })
-})
+        const testErrorText =
+            'This is a test of the emergency alert system.  This is only a test.';
+        wrapper.find(Something).simulateError(new Error(testErrorText));
+        console.log(wrapper.debug());
+        expect(wrapper.contains('Something went wrong')).toBe(true);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
