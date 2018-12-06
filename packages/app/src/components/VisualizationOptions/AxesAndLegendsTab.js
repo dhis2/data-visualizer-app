16b4e265443c27d4418b189f254b566252acbbeb
joakimia <joakimamundsen91@gmail.com>
2018-12-06 15:18:58 +0100

    moving HideLegend, Title HideTitle, Subtitle and HideSubtitile to styling tab

--- packages/app/src/components/VisualizationOptions/AxesAndLegendsTab.js
+++ packages/app/src/components/VisualizationOptions/AxesAndLegendsTab.js
@@ -8,45 +8,7 @@ import RangeAxisSteps from './Options/RangeAxisSteps';
 import RangeAxisDecimals from './Options/RangeAxisDecimals';
 import RangeAxisLabel from './Options/RangeAxisLabel';
 import DomainAxisLabel from './Options/DomainAxisLabel';
-import HideLegend from './Options/HideLegend';
-import HideTitle from './Options/HideTitle';
-import Title from './Options/Title';
-import HideSubtitle from './Options/HideSubtitle';
-import Subtitle from './Options/Subtitle';
-
-const styles = {
-    numberField: {
-        minHeight: 55,
-        marginRight: 20,
-        width: 120,
-    },
-    rangeAxistextField: {
-        minHeight: 55,
-        width: '40%',
-        marginRight: '60%',
-    },
-    domainAxisTextField: {
-        width: '40%',
-        marginRight: 80,
-    },
-    domainAxisSubtitleTextField: {
-        minHeight: 55,
-        width: '40%',
-        marginRight: 80,
-    },
-    inputLabeltextSize: {
-        fontSize: 13,
-    },
-    coverRestofRowSpace: {
-        minHeight: 55,
-        width: 120,
-        marginRight: '65%',
-    },
-    hideChartLegendCheckbox: {
-        minHeight: 55,
-        marginRight: '80%',
-    },
-};
+import styles from './styles/VisualizationOptions.style';
 
 export const AxesAndLegendsTab = ({ classes }) => (
     <FormGroup>
@@ -58,15 +20,6 @@ export const AxesAndLegendsTab = ({ classes }) => (
         <RangeAxisDecimals />
         <RangeAxisLabel />
         <DomainAxisLabel />
-        <HideLegend />
-        <FormGroup row={true}>
-            <Title />
-            <HideTitle />
-        </FormGroup>
-        <FormGroup row={true}>
-            <Subtitle />
-            <HideSubtitle />
-        </FormGroup>
     </FormGroup>
 );
 
--- packages/app/src/components/VisualizationOptions/StyleTab.js
+++ packages/app/src/components/VisualizationOptions/StyleTab.js
@@ -3,12 +3,25 @@ import { withStyles } from '@material-ui/core/styles';
 import FormGroup from '@material-ui/core/FormGroup';
 
 import NoSpaceBetweenColumns from './Options/NoSpaceBetweenColumns';
-
-const styles = {};
+import HideLegend from './Options/HideLegend';
+import HideTitle from './Options/HideTitle';
+import Title from './Options/Title';
+import HideSubtitle from './Options/HideSubtitle';
+import Subtitle from './Options/Subtitle';
+import styles from './styles/VisualizationOptions.style';
 
 export const StyleTab = ({ classes }) => (
     <FormGroup>
         <NoSpaceBetweenColumns />
+        <HideLegend />
+        <FormGroup row={true}>
+            <Title />
+            <HideTitle />
+        </FormGroup>
+        <FormGroup row={true}>
+            <Subtitle />
+            <HideSubtitle />
+        </FormGroup>
     </FormGroup>
 );
 
