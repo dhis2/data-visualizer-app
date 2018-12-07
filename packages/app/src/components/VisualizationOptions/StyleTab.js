8e7f2e40261c9941df70e5efeb586da3cd4a0315
Joakim Amundsen <jamundsen@dhis2.org>
2018-12-07 10:25:52 +0100

    Fix/styling fixes option axes tab (#183)

--- packages/app/src/components/VisualizationOptions/StyleTab.js
+++ packages/app/src/components/VisualizationOptions/StyleTab.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import FormGroup from '@material-ui/core/FormGroup';
+import { withStyles } from '@material-ui/core/styles';
 
 import NoSpaceBetweenColumns from './Options/NoSpaceBetweenColumns';
 import HideLegend from './Options/HideLegend';
@@ -14,7 +15,7 @@ export const StyleTab = ({ classes }) => (
     <FormGroup>
         <NoSpaceBetweenColumns />
         <HideLegend />
-        <FormGroup row={true}>
+        <FormGroup className={classes.titleFormGroup} row={true}>
             <Title />
             <HideTitle />
         </FormGroup>
@@ -24,5 +25,9 @@ export const StyleTab = ({ classes }) => (
         </FormGroup>
     </FormGroup>
 );
+
+StyleTab.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(StyleTab);
--- packages/app/src/components/VisualizationOptions/__tests__/StyleTab.spec.js
+++ packages/app/src/components/VisualizationOptions/__tests__/StyleTab.spec.js
@@ -3,13 +3,16 @@ import { shallow } from 'enzyme';
 import FormGroup from '@material-ui/core/FormGroup';
 
 import { StyleTab } from '../StyleTab';
+import HideTitle from '../Options/HideTitle';
+import HideSubtitle from '../Options/HideSubtitle';
 
 describe('The Style tab', () => {
     let shallowStyleTab;
+    const props = { classes: {} };
 
     const styleTab = () => {
         if (!shallowStyleTab) {
-            shallowStyleTab = shallow(<StyleTab />);
+            shallowStyleTab = shallow(<StyleTab {...props} />);
         }
         return shallowStyleTab;
     };
@@ -25,7 +28,7 @@ describe('The Style tab', () => {
 
         expect(wrappingDiv.children().length).toBe(4);
     });
-    ['HideLegend', 'Title', 'HideTitle', 'Subtitle', 'HideSubtitle'].forEach(
+    ['HideLegend', 'Title', HideTitle, 'Subtitle', HideSubtitle].forEach(
         component => {
             it(`should render a ${component} component`, () => {
                 expect(styleTab().find(component).length).toBe(1);
