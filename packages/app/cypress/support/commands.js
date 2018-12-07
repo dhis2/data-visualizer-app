b5811e23e4420bc3b79fd79bdbc062f6c7aa6e7e
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-12-07 12:53:30 +0100

    add initial smoke test

--- packages/app/cypress/support/commands.js
+++ packages/app/cypress/support/commands.js
@@ -1,25 +1,22 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (username, password) => {
+    const uname = username || Cypress.config('username');
+    const pswd = password || Cypress.config('password');
+    cy.request({
+        method: 'POST',
+        url: Cypress.config('loginUrl'),
+        body: {
+            j_username: uname,
+            j_password: pswd,
+        },
+        form: true,
+        log: true,
+    });
+});
+
+Cypress.Commands.add('persistLogin', () => {
+    Cypress.Cookies.preserveOnce('JSESSIONID');
+});
+
+Cypress.Commands.add('loadPage', () => {
+    cy.visit(Cypress.config('testUrl'));
+});
