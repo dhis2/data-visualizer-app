5052938ae20378f497b6830cdc04b0d387cae087
Edoardo Sabadelli <edoardo@dhis2.org>
2018-12-11 15:12:00 +0100

    Remove unused code

--- packages/plugin/src/api/analytics.js
+++ packages/plugin/src/api/analytics.js
@@ -1,50 +1,6 @@
 import { getInstance } from 'd2';
-import { FIXED_DIMENSIONS } from '../modules/fixedDimensions';
 
-const peId = FIXED_DIMENSIONS.pe.id;
-
-export const apiDownloadImage = async (type, formData) => {
-    const d2 = await getInstance();
-    const api = d2.Api.getApi();
-
-    const url = `${api.baseUrl}/svg.${type}`;
-
-    return api
-        .fetch(url, {
-            method: 'POST',
-            body: formData,
-            headers: new Headers(api.defaultHeaders),
-        })
-        .then(res => res.blob());
-};
-
-export const apiDownloadData = async (current, format, idScheme, path) => {
-    const d2 = await getInstance();
-    const api = d2.Api.getApi();
-
-    let req = new d2.analytics.request()
-        .fromModel(current, path === 'dataValueSet')
-        .withFormat(format);
-
-    if (path) {
-        req = req.withPath(path);
-    }
-
-    if (idScheme) {
-        req = req.withOutputIdScheme(idScheme);
-    }
-
-    const url = new URL(
-        `${api.baseUrl}/${req.buildUrl()}`,
-        `${window.location.origin}${window.location.pathname}`
-    );
-
-    Object.entries(req.buildQuery()).forEach(([key, value]) =>
-        url.searchParams.append(key, value)
-    );
-
-    return url;
-};
+const peId = 'pe';
 
 export const apiFetchAnalytics = async (current, options) => {
     const d2 = await getInstance();
