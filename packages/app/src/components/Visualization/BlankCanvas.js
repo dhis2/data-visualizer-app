34ea7955c3f85c21c19fc0ccf7aa55ab130a8d76
Edoardo Sabadelli <edoardo@dhis2.org>
2018-12-14 11:41:58 +0100

    DHIS2-5489: loading is now handled in the plugin
    BlankCanvas only renders the default canvas with the "welcome" message
    and any error catched in the Visualization component.

--- packages/app/src/components/Visualization/BlankCanvas.js
+++ packages/app/src/components/Visualization/BlankCanvas.js
@@ -1,30 +1,20 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import i18n from '@dhis2/d2-i18n';
-import LoadingMask from '../../widgets/LoadingMask';
 import styles from './styles/BlankCanvas.style';
-import { sGetLoadError, sGetIsLoading } from '../../reducers/loader';
-import chartErrorImg from '../../assets/chart-error-graphic.png';
+import { sGetLoadError } from '../../reducers/loader';
 
 export const visContainerId = 'visualization-container';
 export const defaultCanvasMessage =
     'Create a new visualization by adding dimensions to the layout';
 
-const getMessage = text => <p style={styles.title}>{text}</p>;
-
-export const BlankCanvas = ({ loading, error }) => {
-    const canvasContent = error ? (
-        <div>
-            <img src={chartErrorImg} alt={i18n.t('Chart error')} />
-            {getMessage(error)}
-        </div>
-    ) : (
-        getMessage(i18n.t(defaultCanvasMessage))
+export const BlankCanvas = ({ error }) => {
+    let canvasContent = (
+        <p style={styles.title}>{i18n.t(defaultCanvasMessage)}</p>
     );
 
     return (
         <Fragment>
-            {loading ? <LoadingMask /> : null}
             <div id={visContainerId} style={styles.outer}>
                 <div style={styles.inner}>{canvasContent}</div>
             </div>
@@ -34,7 +24,6 @@ export const BlankCanvas = ({ loading, error }) => {
 
 const mapStateToProps = state => ({
     error: sGetLoadError(state),
-    loading: sGetIsLoading(state),
 });
 
 export default connect(mapStateToProps)(BlankCanvas);
