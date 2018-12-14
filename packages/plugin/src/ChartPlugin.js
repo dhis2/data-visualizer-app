ee9645dc043ffb86ce597b764577319984c13992
Edoardo Sabadelli <edoardo@dhis2.org>
2018-12-14 11:45:43 +0100

    DHIS2-5489: first working version of the plugin

--- packages/plugin/src/ChartPlugin.js
+++ packages/plugin/src/ChartPlugin.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+
+import { createChart } from 'd2-charts-api';
 
 import { apiFetchVisualization } from './api/visualization';
 import {
@@ -9,81 +11,158 @@ import {
 import { isYearOverYear } from './modules/chartTypes';
 import { getOptionsForRequest } from './modules/options';
 import { computeGenericPeriodNames } from './modules/analytics';
-import { createChart } from 'd2-charts-api';
+import { BASE_FIELD_YEARLY_SERIES } from './modules/fields/baseFields';
+import LoadingMask from './widgets/LoadingMask';
 
 class ChartPlugin extends Component {
-    isActive = false;
+    constructor(props) {
+        super(props);
+
+        this.canvasRef = React.createRef();
+
+        this.recreateChart = Function.prototype;
+
+        this.state = {
+            isLoading: true,
+        };
+    }
 
     componentDidMount() {
-        this.isActive = true;
-
         this.renderChart();
     }
 
-    componentWillUnmount() {
-        this.isActive = false;
+    componentDidUpdate(prevProps) {
+        if (this.props.config !== prevProps.config) {
+            this.renderChart();
+            return;
+        }
+
+        if (this.props.filters !== prevProps.filters) {
+            this.renderChart();
+            return;
+        }
+
+        if (this.props.id !== prevProps.id) {
+            this.recreateChart(0); // disable animation
+            return;
+        }
     }
 
-    getOptions = vis => {
+    getRequestOptions = (visualization, filters) => {
         const options = getOptionsForRequest().reduce(
             (map, [option, props]) => {
                 // only add parameter if value !== default
-                if (vis[option] !== props.defaultValue) {
-                    map[option] = vis[option];
+                if (visualization[option] !== props.defaultValue) {
+                    map[option] = visualization[option];
                 }
 
                 return map;
             },
             {}
         );
+
+        // interpretation filter
+        if (filters.relativePeriodDate) {
+            options.relativePeriodDate = filters.relativePeriodDate;
+        }
+
+        // global filters
+        if (filters.userOrgUnit) {
+            options.userOrgUnit = filters.userOrgUnits;
+        }
+
+        return options;
     };
 
-    getConfigById = async id => {
-        const config = await apiFetchVisualization('chart', id);
-
-        return config;
+    getConfigById = id => {
+        return apiFetchVisualization('chart', id);
     };
 
     renderChart = async () => {
-        if (!this.isActive) return;
+        const { config, filters, onChartGenerated, onError } = this.props;
 
-        const { id, config, containerId } = this.props;
+        try {
+            const visualization =
+                Object.keys(config).length === 1
+                    ? await this.getConfigById(config.id)
+                    : config;
 
-        const vis = id ? this.getConfigById(id) : config;
+            const options = this.getRequestOptions(visualization, filters);
 
-        const options = this.getOptions(vis);
+            const extraOptions = {};
+            let responses = [];
 
-        const extraOptions = {};
-        let responses = [];
+            if (isYearOverYear(visualization.type)) {
+                let yearlySeriesLabels = [];
 
-        if (isYearOverYear(vis.type)) {
-            let yearlySeriesLabels = [];
+                ({
+                    responses,
+                    yearlySeriesLabels,
+                } = await apiFetchAnalyticsForYearOverYear(
+                    visualization,
+                    options
+                ));
 
-            ({
-                responses,
-                yearlySeriesLabels,
-            } = await apiFetchAnalyticsForYearOverYear(vis, options));
+                extraOptions[BASE_FIELD_YEARLY_SERIES] = yearlySeriesLabels;
+                extraOptions.xAxisLabels = computeGenericPeriodNames(responses);
+            } else {
+                responses = await apiFetchAnalytics(visualization, options);
+            }
 
-            extraOptions.yearlySeries = yearlySeriesLabels;
-            extraOptions.xAxisLabels = computeGenericPeriodNames(responses);
-        } else {
-            responses = await apiFetchAnalytics(vis, options);
+            this.recreateChart = animation => {
+                const chartConfig = createChart(
+                    responses,
+                    visualization,
+                    this.canvasRef.current,
+                    {
+                        ...extraOptions,
+                        animation,
+                    }
+                );
+
+                onChartGenerated(
+                    chartConfig.chart.getSVGForExport({
+                        sourceHeight: 768,
+                        sourceWidth: 1024,
+                    })
+                );
+            };
+
+            this.recreateChart();
+
+            this.setState({ isLoading: false });
+        } catch (error) {
+            onError(error);
         }
-
-        createChart(responses, vis, containerId, extraOptions);
     };
 
     render() {
-        // TODO add loading
-        return <div id={this.props.containerId} />;
+        return (
+            <Fragment>
+                {this.state.isLoading ? <LoadingMask /> : null}
+                <div ref={this.canvasRef} style={{ height: '100%' }} />
+            </Fragment>
+        );
     }
 }
 
+ChartPlugin.defaultProps = {
+    config: {},
+    filters: {},
+    animation: 200,
+    onError: Function.prototype,
+    onChartGenerated: Function.prototype,
+    onResponsesReceived: Function.prototype,
+};
+
 ChartPlugin.propTypes = {
-    d2: PropTypes.object,
-    containerId: PropTypes.string,
     id: PropTypes.string,
-    config: PropTypes.object,
+    animation: PropTypes.number,
+    config: PropTypes.object.isRequired,
+    filters: PropTypes.object,
+    onError: PropTypes.func.isRequired,
+    onChartGenerated: PropTypes.func,
+    onResponsesReceived: PropTypes.func,
 };
 
 export default ChartPlugin;
