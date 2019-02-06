9c09c3b031f9e9641c0655a09639f3f67ec0910b
Ilya Nee <neeilya@icloud.com>
2019-02-06 14:08:10 +0100

    Api module for interacting with user data store

---
+++ packages/app/src/api/userDataStore.js
@@ -0,0 +1,36 @@
+import { getInstance } from 'd2';
+import { onError } from './index';
+
+export const NAMESPACE = 'analytics';
+
+export const hasNamespace = async d2 =>
+    await d2.currentUser.dataStore.has(NAMESPACE);
+
+export const getNamespace = async (d2, hasNamespace) =>
+    hasNamespace
+        ? await d2.currentUser.dataStore.get(NAMESPACE)
+        : await d2.currentUser.dataStore.create(NAMESPACE);
+
+export const apiCreate = async (data, key, namespace) => {
+    try {
+        const d2 = await getInstance();
+        const ns =
+            namespace || (await getNamespace(d2, await hasNamespace(d2)));
+
+        return ns.set(key, data);
+    } catch (error) {
+        return onError(error);
+    }
+};
+
+export const apiFetch = async (key, namespace) => {
+    try {
+        const d2 = await getInstance();
+        const ns =
+            namespace || (await getNamespace(d2, await hasNamespace(d2)));
+
+        return ns.get(key);
+    } catch (error) {
+        return onError(error);
+    }
+};
