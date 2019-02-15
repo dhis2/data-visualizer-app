e1684befb16eeb16cf1cdb508b287102e882419c
Jen Jones Arnesen <jennifer@dhis2.org>
2019-02-15 14:56:43 +0100

    Add tests for UnselectedItems

--- packages/shared/src/components/ItemSelector/__tests__/UnselectedItems.spec.js
+++ packages/shared/src/components/ItemSelector/__tests__/UnselectedItems.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { UnselectedItems } from '../UnselectedItems';
 
-describe('UnselectedItems component ', () => {
+describe('UnselectedItems component', () => {
     let props;
     let shallowItems;
     const unselectedItems = () => {
@@ -26,14 +26,64 @@ describe('UnselectedItems component ', () => {
         expect(unselectedItems()).toMatchSnapshot();
     });
 
-    it('matches the snapshot when list has items', () => {
-        props.items = [
-            {
-                id: 'rb',
-                name: 'rainbow',
-            },
-            { id: 'rr', name: 'rarity' },
-        ];
-        expect(unselectedItems()).toMatchSnapshot();
+    describe('list with items', () => {
+        beforeEach(() => {
+            props.items = [
+                {
+                    id: 'rb',
+                    name: 'rainbow',
+                },
+                { id: 'rr', name: 'rarity' },
+            ];
+        });
+
+        it('matches the snapshot when list has items', () => {
+            expect(unselectedItems()).toMatchSnapshot();
+        });
+
+        it('triggers onSelect all when "select all" button clicked', () => {
+            unselectedItems()
+                .find('SelectButton')
+                .simulate('click');
+
+            expect(props.onSelect).toHaveBeenCalled();
+            expect(props.onSelect).toHaveBeenCalledWith(['rb', 'rr']);
+        });
+
+        it('triggers onSelect when item double-clicked', () => {
+            unselectedItems()
+                .find('li')
+                .first()
+                .simulate('doubleClick');
+
+            expect(props.onSelect).toHaveBeenCalled();
+            expect(props.onSelect).toHaveBeenCalledWith(['rb']);
+        });
+
+        it('triggers onSelect when "assign" button clicked', () => {
+            const list = unselectedItems();
+            list.find('Item')
+                .first()
+                .simulate('itemClick', false, false, 0, 'rb');
+
+            list.find('ArrowButton')
+                .first()
+                .simulate('click');
+
+            expect(props.onSelect).toHaveBeenCalled();
+            expect(props.onSelect).toHaveBeenCalledWith(['rb']);
+        });
+
+        it('triggers requestMoreItems when scroll to bottom is reached', () => {});
+
+        it('renders only items matching the filter', () => {
+            props.filterText = 'b';
+            const items = unselectedItems().find('Item');
+
+            expect(items.length).toEqual(1);
+            expect(items.dive().text()).toEqual(
+                expect.stringContaining('rainbow')
+            );
+        });
     });
 });
