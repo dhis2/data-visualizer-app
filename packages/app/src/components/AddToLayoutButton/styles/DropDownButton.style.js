b9edb299875668e190c371acc6649d8ae7a70880
joakimia <joakimamundsen91@gmail.com>
2019-03-13 15:10:25 +0100

    adjust fixed height for addToLayoutButton

--- packages/app/src/components/AddToLayoutButton/styles/DropDownButton.style.js
+++ packages/app/src/components/AddToLayoutButton/styles/DropDownButton.style.js
@@ -4,7 +4,7 @@ export default {
     arrowDown: {
         borderRadius: '0px 2px 2px 0px',
         padding: '0px',
-        height: '36px',
+        height: '33px',
         minWidth: '40px',
     },
     arrowIcon: {
