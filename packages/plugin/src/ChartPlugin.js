bfe41b4eb2784bc2a5d854833510ad41e1ce0b14
Ilya Nee <neeilya@icloud.com>
2019-04-03 10:32:44 +0200

    fix: Fetching analytics for analytical object with undefined aggregationType in plugin (#232)
    * Disallow undefined value for analytics request options in plugin

--- packages/plugin/src/ChartPlugin.js
+++ packages/plugin/src/ChartPlugin.js
@@ -52,7 +52,10 @@ class ChartPlugin extends Component {
         const options = getOptionsForRequest().reduce(
             (map, [option, props]) => {
                 // only add parameter if value !== default
-                if (visualization[option] !== props.defaultValue) {
+                if (
+                    visualization[option] !== undefined &&
+                    visualization[option] !== props.defaultValue
+                ) {
                     map[option] = visualization[option];
                 }
 
