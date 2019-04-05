8c80f55353208804b328d849199ab26efcd0b83f
Ilya Nee <neeilya@icloud.com>
2019-04-05 11:50:04 +0200

    AxisSetup component

---
+++ packages/app/src/components/AxisSetup/constants.js
@@ -0,0 +1,7 @@
+export const axis1 = 1;
+export const axis2 = 2;
+
+export default {
+    axis1,
+    axis2,
+};
---
+++ packages/app/src/components/AxisSetup/styles/AxisSetup.style.js
@@ -0,0 +1,32 @@
+import { colors } from 'analytics-shared';
+
+export default {
+    tableContainer: {
+        marginTop: '20px',
+    },
+    nameColumn: {
+        width: '80%',
+    },
+    axisColumn: {
+        width: '10%',
+    },
+    coloredColumn: {
+        backgroundColor: colors.lightGrey,
+    },
+    tableCell: {
+        padding: '4px 24px',
+        borderBottom: `1px solid ${colors.greyLight}`,
+    },
+    head: {
+        borderBottom: 'none',
+    },
+    axisRadio: {
+        padding: '0px 10px',
+    },
+    dialogContent: {
+        marginBottom: '0px',
+    },
+    dialogActions: {
+        border: 'none',
+    },
+};
