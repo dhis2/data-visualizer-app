679d913a110807a902a9557aebfa8fc1b2fa6ea9
Edoardo Sabadelli <edoardo@dhis2.org>
2019-08-12 10:46:33 +0200

    chore: remove unused CSS

--- packages/app/src/components/DimensionsPanel/styles/DimensionsPanel.style.js
+++ packages/app/src/components/DimensionsPanel/styles/DimensionsPanel.style.js
@@ -6,10 +6,5 @@ export const styles = {
         display: 'flex',
         flexDirection: 'column',
         backgroundColor: colors.grey050,
-        padding: '8px 0 2px 8px',
-    },
-    textField: {
-        paddingBottom: '12px',
-        width: 'calc(100% - 8px)',
     },
 };
