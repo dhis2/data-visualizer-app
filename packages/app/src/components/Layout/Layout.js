f3fe9913f9d91e11ec2674da2b808bb3c95b88eb
Edoardo Sabadelli <edoardo@dhis2.org>
2019-09-10 15:37:25 +0200

    fix: revert Layout component. cleanup

--- packages/app/src/components/Layout/Layout.js
+++ packages/app/src/components/Layout/Layout.js
@@ -1,11 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import i18n from '@dhis2/d2-i18n';
-
-import { acSetLoadError, acClearLoadError } from '../../actions/loader';
-import { validateLayout } from '../../modules/layoutValidation';
-
-import { sGetCurrentFromUi } from '../../reducers/current';
 
 import DefaultLayout from './DefaultLayout/DefaultLayout';
 import YearOverYearLayout from './YearOverYearLayout/YearOverYearLayout';
@@ -47,43 +41,10 @@ const getLayoutByType = (type, props) => {
     return <Layout {...props} />;
 };
 
-class Layout extends Component {
-    validate = visualization => {
-        try {
-            validateLayout(visualization);
-
-            this.props.acClearLoadError();
-        } catch (err) {
-            this.onError(err);
-        }
-    };
-
-    onError = err => {
-        const error = (err && err.message) || i18n.t('Error validating layout');
-
-        this.props.acSetLoadError(error);
-    };
-    /*
-    componentDidUpdate(prevProps) {
-        if (this.props.type !== prevProps.type) {
-            this.validate({
-                ...this.props.currentFromUi,
-                type: this.props.type,
-            });
-        }
-    }
-*/
-    render() {
-        return getLayoutByType(this.props.type);
-    }
-}
+const Layout = props => getLayoutByType(props.type);
 
 const mapStateToProps = state => ({
     type: sGetUiType(state),
-    currentFromUi: sGetCurrentFromUi(state),
 });
 
-export default connect(
-    mapStateToProps,
-    { acSetLoadError, acClearLoadError }
-)(Layout);
+export default connect(mapStateToProps)(Layout);
