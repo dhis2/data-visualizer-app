c93ca82191e4a9749e33487a1ce2d9be20771737
Jan Henrik Øverland <jan@dhis2.org>
2019-09-23 14:58:00 +0200

    fix: single value without dx items (#313)
    * fix: single value without dx items
    
    * fix: pass array instead of object
    
    * fix: fix prettier
    
    * fix: code style
    
    * @dhis2/analytics@2.4.0

--- packages/app/src/modules/current.js
+++ packages/app/src/modules/current.js
@@ -6,6 +6,8 @@ import {
     DIMENSION_ID_DATA,
     DIMENSION_ID_PERIOD,
     dimensionCreate,
+    layoutGetDimensionItems,
+    layoutReplaceDimension,
 } from '@dhis2/analytics';
 
 import options from './options';
@@ -71,16 +73,20 @@ export const getSingleValueCurrentFromUi = (state, action) => {
         },
     };
 
-    // only save the first dx item
     const axesFromUi = getAxesFromUi(ui);
-    if (axesFromUi.columns.length && axesFromUi.columns[0].items.length) {
-        axesFromUi.columns[0].items = [axesFromUi.columns[0].items[0]];
-    }
+
+    // only save the first dx item
+    const dxItems = layoutGetDimensionItems(axesFromUi, DIMENSION_ID_DATA);
+    const singleValueAxesFromUi = layoutReplaceDimension(
+        axesFromUi,
+        DIMENSION_ID_DATA,
+        [dxItems[0]]
+    );
 
     return {
         ...state,
         [BASE_FIELD_TYPE]: ui.type,
-        ...axesFromUi,
+        ...singleValueAxesFromUi,
         ...getOptionsFromUi(ui),
     };
 };
