f223c270e059435c6504c1e3ccd314d44898d0dc
Edoardo Sabadelli <edoardo@dhis2.org>
2019-10-25 10:01:19 +0200

    refactor: differentiate title label for pivot table

--- packages/app/src/components/VisualizationOptions/Options/Title.js
+++ packages/app/src/components/VisualizationOptions/Options/Title.js
@@ -1,15 +1,32 @@
-import React from 'react'
-import TextBaseOption from './TextBaseOption'
-import i18n from '@dhis2/d2-i18n'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Title = () => (
+import i18n from '@dhis2/d2-i18n';
+import { PIVOT_TABLE } from '../../../modules/chartTypes';
+import { sGetUiType } from '../../../reducers/ui';
+
+import TextBaseOption from './TextBaseOption';
+
+const Title = ({ visualizationType }) => (
     <TextBaseOption
         type="text"
         option={{
             name: 'title',
-            label: i18n.t('Chart title'),
+            label:
+                visualizationType === PIVOT_TABLE
+                    ? i18n.t('Table title')
+                    : i18n.t('Chart title'),
         }}
     />
 )
 
-export default Title
+Title.propTypes = {
+    visualizationType: PropTypes.string,
+};
+
+const mapStateToProps = state => ({
+    visualizationType: sGetUiType(state),
+});
+
+export default connect(mapStateToProps)(Title);
