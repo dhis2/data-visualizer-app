b6c9864d5047f855b29e3b0f2e1a0af16bd49103
Edoardo Sabadelli <edoardo@dhis2.org>
2019-10-25 09:59:59 +0200

    refactor: use radio buttons in place of a select

--- packages/app/src/components/VisualizationOptions/Options/SortOrder.js
+++ packages/app/src/components/VisualizationOptions/Options/SortOrder.js
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import i18n from '@dhis2/d2-i18n'
 
-import SelectBaseOption from './SelectBaseOption'
-import styles from '../styles/VisualizationOptions.style'
+import RadioBaseOption from './RadioBaseOption';
+import styles from '../styles/VisualizationOptions.style';
 
 const SortOrder = ({ classes }) => (
-    <SelectBaseOption
-        className={classes.selectBaseOption}
+    <RadioBaseOption
+        row={true}
         option={{
             name: 'sortOrder',
             label: i18n.t('Sort order'),
             items: [
-                { id: 0, label: i18n.t('None') },
-                { id: -1, label: i18n.t('Low to high') },
-                { id: 1, label: i18n.t('High to low') },
+                { id: '-1', label: i18n.t('Low to high') },
+                { id: '1', label: i18n.t('High to low') },
+                { id: '0', label: i18n.t('None') },
             ],
         }}
     />
