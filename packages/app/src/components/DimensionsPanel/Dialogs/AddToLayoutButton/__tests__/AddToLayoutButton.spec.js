19650d16e9e8f2bececd860903b95329f6e7482b
Jan Henrik Overland <janhenrik.overland@gmail.com>
2019-11-04 16:05:45 +0100

    tmp fix add to button tests

--- packages/app/src/components/DimensionsPanel/Dialogs/AddToLayoutButton/__tests__/AddToLayoutButton.spec.js
+++ packages/app/src/components/DimensionsPanel/Dialogs/AddToLayoutButton/__tests__/AddToLayoutButton.spec.js
@@ -6,37 +6,39 @@ import { AddToLayoutButton } from '../AddToLayoutButton';
 
 describe('The AddToLayoutButton component ', () => {
     let props;
-    let shallowAddToButton;
+    let shallowButton;
 
-    const addToButton = () => {
-        if (!shallowAddToButton) {
-            shallowAddToButton = shallow(<AddToLayoutButton {...props} />);
+    const getShallowAddToLayoutButton = () => {
+        if (!shallowButton) {
+            shallowButton = shallow(<AddToLayoutButton {...props} />);
         }
-        return shallowAddToButton;
+        return shallowButton;
     };
 
     beforeEach(() => {
         props = {
             classes: {},
             closeDialog: jest.fn(),
-            currentLayout: {},
-            dialogId: 'id',
-            layoutType: '',
+            dimensionIdsInLayout: ['dx', 'pe', 'ou'],
+            dialogId: '',
             onAddDimension: jest.fn(),
         };
-        shallowAddToButton = undefined;
+        shallowButton = undefined;
     });
 
-    it('renders an updateButton when state "buttonType" is not equal to -1 ', () => {
-        const button = addToButton();
-        button.setState({ buttonType: 2 });
+    it('renders an update button if dialogid exists in layout', () => {
+        props.dialogId = 'dx';
 
-        expect(button.length).toEqual(1);
+        const button = getShallowAddToLayoutButton();
+
+        expect(button.find('button').attr('data-test')).toEqual(
+            'update-button'
+        );
+        // expect(button).to.have.descendants('[data-test="update-button"]');
     });
 
     it('renders two buttons, (DropDownIcon and "Add to series") if state buttonType is equal to -1 ', () => {
-        props.layoutType = 'COLUMN';
-        const button = addToButton();
+        const button = getShallowAddToLayoutButton();
         button.setState({ buttonType: -1 });
 
         const fragmentWrapper = button.find('div');
@@ -46,7 +48,7 @@ describe('The AddToLayoutButton component ', () => {
 
     it('renders only an "Add to filter" button if current chart type is year on year', () => {
         props.layoutType = 'YEAR_OVER_YEAR_LINE';
-        const button = addToButton();
+        const button = getShallowAddToLayoutButton();
         button.setState({ buttonType: -1 });
 
         const addToFilterButton = button.find(Button).first();
