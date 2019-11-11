a3dbb9fb20356b3aa303155de98e16795dcffaf6
Jan Henrik Øverland <jan@dhis2.org>
2019-11-11 14:46:30 +0100

    feat: use layout rules for add-to-layout ui (#362)
    * feat: generate add to layout button/menu based on rules
    
    * feat: refactor logic to use rules
    
    * upgrade @dhis2/analytics dep
    
    * tmp fix add to button tests
    
    * TODO fix tests
    
    * remove comment
    
    * replace local fn with analytics fn
    
    * todo: tmp commented out tests
    
    * remove folder

--- packages/app/src/components/DimensionsPanel/Dialogs/AddToLayoutButton/AddToLayoutButton.js
+++ packages/app/src/components/DimensionsPanel/Dialogs/AddToLayoutButton/AddToLayoutButton.js
@@ -4,15 +4,14 @@ import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import MenuItem from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
-import { AXIS_NAME_COLUMNS } from '@dhis2/analytics';
+import { getAvailableAxes } from '@dhis2/analytics';
 
 import UpdateButton from '../../../UpdateButton/UpdateButton';
 import Menu from './Menu';
 import {
     sGetUi,
-    sGetUiLayout,
     sGetUiActiveModalDialog,
-    sGetUiType,
+    sGetDimensionIdsFromLayout,
 } from '../../../../reducers/ui';
 import {
     acSetUiActiveModalDialog,
@@ -20,16 +19,8 @@ import {
 } from '../../../../actions/ui';
 import { acSetCurrentFromUi } from '../../../../actions/current';
 
-import { isYearOverYear } from '../../../../modules/chartTypes';
 import { ADD_TO_LAYOUT_OPTIONS } from '../../../../modules/layout';
 import styles from './styles/AddToLayoutButton.style';
-
-const UNSELECTED_BUTTON_TYPE = -1;
-const seriesItem = ADD_TO_LAYOUT_OPTIONS[0];
-const filterItem = ADD_TO_LAYOUT_OPTIONS[2];
-const itemsWithoutSeries = ADD_TO_LAYOUT_OPTIONS.filter(
-    option => option.axisName !== AXIS_NAME_COLUMNS
-);
 
 export class AddToLayoutButton extends Component {
     constructor(props) {
@@ -37,15 +28,7 @@ export class AddToLayoutButton extends Component {
         this.buttonRef = React.createRef();
     }
 
-    state = { anchorEl: null, buttonType: UNSELECTED_BUTTON_TYPE };
-
-    componentDidMount() {
-        const buttonType = Object.values(this.props.currentLayout).findIndex(
-            axisIds => axisIds.includes(this.props.dialogId)
-        );
-
-        this.setState({ buttonType });
-    }
+    state = { anchorEl: null };
 
     onClose = () => this.setState({ anchorEl: null });
 
@@ -62,31 +45,33 @@ export class AddToLayoutButton extends Component {
         this.props.closeDialog(null);
     };
 
-    renderMenuItems = () =>
-        itemsWithoutSeries.map(option => (
-            <MenuItem
-                className={this.props.classes.menuItem}
-                component="li"
-                key={option.axisName}
-                onClick={() => this.onUpdate(option.axisName)}
-            >
-                {option.name}
-            </MenuItem>
-        ));
+    getAxisMeta = axisNameArray =>
+        axisNameArray.map(axisName =>
+            ADD_TO_LAYOUT_OPTIONS.find(
+                axisMetaObj => axisMetaObj.axisName === axisName
+            )
+        );
 
-    renderUnselectedButton = () =>
-        isYearOverYear(this.props.layoutType) ? (
-            <Button
-                className={this.props.classes.button}
-                variant="contained"
-                color="primary"
-                disableRipple
-                disableFocusRipple
-                onClick={() => this.onUpdate(filterItem.axisName)}
-            >
-                {filterItem.name}
-            </Button>
-        ) : (
+    renderMenuItems = () =>
+        this.getAxisMeta(getAvailableAxes(this.props.ui.type))
+            .slice(1)
+            .map(axisMetaObj => (
+                <MenuItem
+                    className={this.props.classes.menuItem}
+                    component="li"
+                    key={axisMetaObj.axisName}
+                    onClick={() => this.onUpdate(axisMetaObj.axisName)}
+                >
+                    {axisMetaObj.name}
+                </MenuItem>
+            ));
+
+    renderAddToLayoutButton = () => {
+        const availableAxisMeta = this.getAxisMeta(
+            getAvailableAxes(this.props.ui.type)
+        );
+
+        return (
             <div ref={addToRef => (this.buttonRef = addToRef)}>
                 <Button
                     className={this.props.classes.button}
@@ -94,30 +79,37 @@ export class AddToLayoutButton extends Component {
                     color="primary"
                     disableRipple
                     disableFocusRipple
-                    onClick={() => this.onUpdate(seriesItem.axisName)}
+                    onClick={() => this.onUpdate(availableAxisMeta[0].name)}
                 >
-                    {seriesItem.name}
+                    {availableAxisMeta[0].name}
                 </Button>
-                <Menu
-                    onClose={this.onClose}
-                    onClick={this.onToggle}
-                    anchorEl={this.state.anchorEl}
-                    menuItems={this.renderMenuItems()}
-                    addToButtonRef={this.buttonRef}
-                />
+                {availableAxisMeta.length > 1 ? (
+                    <Menu
+                        onClose={this.onClose}
+                        onClick={this.onToggle}
+                        anchorEl={this.state.anchorEl}
+                        menuItems={this.renderMenuItems()}
+                        addToButtonRef={this.buttonRef}
+                    />
+                ) : null}
             </div>
         );
+    };
+
+    renderUpdateButton = () => (
+        <UpdateButton
+            className={this.props.className}
+            onClick={() => this.props.closeDialog(null)}
+        />
+    );
+
+    layoutHasDimension = dimensionId =>
+        this.props.dimensionIdsInLayout.includes(dimensionId);
 
     render() {
-        const displayButton =
-            this.state.buttonType === UNSELECTED_BUTTON_TYPE ? (
-                this.renderUnselectedButton()
-            ) : (
-                <UpdateButton
-                    className={this.props.className}
-                    onClick={() => this.props.closeDialog(null)}
-                />
-            );
+        const displayButton = this.layoutHasDimension(this.props.dialogId)
+            ? this.renderUpdateButton()
+            : this.renderAddToLayoutButton();
 
         return displayButton;
     }
@@ -125,18 +117,16 @@ export class AddToLayoutButton extends Component {
 
 AddToLayoutButton.propTypes = {
     classes: PropTypes.object.isRequired,
-    closeDialog: PropTypes.func.isRequired,
-    currentLayout: PropTypes.object.isRequired,
     dialogId: PropTypes.string.isRequired,
-    layoutType: PropTypes.string.isRequired,
+    dimensionIdsInLayout: PropTypes.array.isRequired,
+    closeDialog: PropTypes.func.isRequired,
     onAddDimension: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
     ui: sGetUi(state),
     dialogId: sGetUiActiveModalDialog(state),
-    layoutType: sGetUiType(state),
-    currentLayout: sGetUiLayout(state),
+    dimensionIdsInLayout: sGetDimensionIdsFromLayout(state),
 });
 
 export default connect(
