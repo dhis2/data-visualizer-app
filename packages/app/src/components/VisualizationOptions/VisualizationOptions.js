3c243974c5bb30a5f975f6b511e4901c5055da4a
Edoardo Sabadelli <edoardo@dhis2.org>
2019-11-19 09:58:23 +0100

    refactor: add more custom styles

--- packages/app/src/components/VisualizationOptions/VisualizationOptions.js
+++ packages/app/src/components/VisualizationOptions/VisualizationOptions.js
@@ -6,30 +6,39 @@ import { FieldSet, Legend, TabBar, Tab } from '@dhis2/ui-core';
 
 import {
     tabSection,
-    tabSectionLegend,
-    tabSectionLabel,
+    tabSectionTitle,
+    tabSectionOption,
+    tabSectionOptionItem,
+    tabSectionOptionToggleable,
+    tabSectionOptionComplexInline,
+    tabSectionOptionText,
+    tabBar,
 } from './styles/VisualizationOptions.style.js';
 
 import { sGetUiType } from '../../reducers/ui';
 import { getOptionsByType } from '../../modules/options/config';
 
 export class VisualizationOptions extends Component {
-    state = { activeTab: 0 }
+    state = { activeTabKey: undefined };
 
-    selectTab = tabId => {
-        this.setState({ activeTab: tabId })
-    }
+    selectTab = tabKey => {
+        this.setState({ activeTabKey: tabKey });
+    };
 
     generateTabContent = sections =>
         sections.map(({ key, label, content }) => (
-            <FieldSet key={key} className={tabSection.className}>
-                {label ? (
-                    <Legend className={tabSectionLegend.className}>
-                        {label}
-                    </Legend>
-                ) : null}
-                {content}
-            </FieldSet>
+            <div key={key} className={tabSection.className}>
+                <FieldSet>
+                    {label ? (
+                        <Legend>
+                            <span className={tabSectionTitle.className}>
+                                {label}
+                            </span>
+                        </Legend>
+                    ) : null}
+                    {content}
+                </FieldSet>
+            </div>
         ));
 
     generateTabs = tabs =>
@@ -41,29 +50,43 @@ export class VisualizationOptions extends Component {
 
     render() {
         const { visualizationType } = this.props;
-        const { activeTab } = this.state;
 
         const optionsConfig = getOptionsByType(visualizationType);
 
         const tabs = this.generateTabs(optionsConfig);
 
+        let activeTabIndex = tabs.findIndex(
+            tab => tab.key === this.state.activeTabKey
+        );
+
+        if (activeTabIndex < 0) {
+            activeTabIndex = 0;
+        }
+
         return (
             <Fragment>
-                <TabBar>
-                    {tabs.map(({ key, label }, index) => (
-                        <Tab
-                            key={key}
-                            onClick={() => this.selectTab(index)}
-                            selected={index === activeTab}
-                        >
-                            {label}
-                        </Tab>
-                    ))}
-                </TabBar>
-                {tabs[activeTab].content}
+                <div className={tabBar.className}>
+                    <TabBar>
+                        {tabs.map(({ key, label }, index) => (
+                            <Tab
+                                key={key}
+                                onClick={() => this.selectTab(key)}
+                                selected={index === activeTabIndex}
+                            >
+                                {label}
+                            </Tab>
+                        ))}
+                    </TabBar>
+                    {tabBar.styles}
+                </div>
+                {tabs[activeTabIndex].content}
                 {tabSection.styles}
-                {tabSectionLegend.styles}
-                {tabSectionLabel.styles}
+                {tabSectionTitle.styles}
+                {tabSectionOption.styles}
+                {tabSectionOptionItem.styles}
+                {tabSectionOptionToggleable.styles}
+                {tabSectionOptionComplexInline.styles}
+                {tabSectionOptionText.styles}
             </Fragment>
         )
     }
--- packages/app/src/components/VisualizationOptions/styles/VisualizationOptions.style.js
+++ packages/app/src/components/VisualizationOptions/styles/VisualizationOptions.style.js
@@ -3,26 +3,61 @@ import css from 'styled-jsx/css';
 
 import { colors } from '@dhis2/ui-core';
 
+export const tabBar = css.resolve`
+    div {
+        position: sticky;
+        top: 0;
+    }
+`;
+
 export const tabSection = css.resolve`
-    fieldset {
+    div {
         padding: 16px 0;
     }
 `;
 
-export const tabSectionLegend = css.resolve`
-    legend {
+export const tabSectionContent = css.resolve`
+    div {
+        overflow-y: auto;
+    }
+`;
+
+export const tabSectionTitle = css.resolve`
+    span {
+        display: inline-block;
+        padding-bottom: 16px;
         font-size: 16px;
-        padding-top: 16px;
         color: ${colors.grey900};
     }
 `;
 
-export const tabSectionLabel = css.resolve`
-    label.dense {
+export const tabSectionOption = css.resolve`
+    div:not(:last-child):not(.inline) {
         padding-bottom: 16px;
     }
+`;
 
-    label.grouped.dense {
+export const tabSectionOptionItem = css.resolve`
+    div:not(:last-child) {
         padding-bottom: 8px;
+    }
+`;
+
+export const tabSectionOptionText = css.resolve`
+    p {
+        font-size: 14px;
+    }
+`;
+
+export const tabSectionOptionToggleable = css.resolve`
+    div {
+        margin: 4px 0 0 23px;
+    }
+`;
+
+export const tabSectionOptionComplexInline = css.resolve`
+    div {
+        display: flex;
+        align-items: center;
     }
 `;
