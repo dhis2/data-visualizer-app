8b50f06f427c953b925441cc4c1971cab3437749
Edoardo Sabadelli <edoardo@dhis2.org>
2019-11-19 10:01:55 +0100

    refactor: temporary hack for opening a PT

--- packages/plugin/src/PivotPlugin.js
+++ packages/plugin/src/PivotPlugin.js
@@ -2,12 +2,14 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import isEqual from 'lodash-es/isEqual'
 import i18n from '@dhis2/d2-i18n'
+import { createVisualization } from '@dhis2/analytics'
 import {
-    createVisualization,
-    isYearOverYear,
-    isSingleValue,
-} from '@dhis2/analytics'
-
+    api as d2aApi,
+    table as d2aTable,
+    config as d2aConfig,
+    Layout as d2aLayout,
+    Response as d2aResponse,
+} from 'd2-analysis'
 import { apiFetchVisualization } from './api/visualization'
 import {
     apiFetchAnalytics,
@@ -17,6 +19,8 @@ import { getOptionsForRequest } from './modules/options'
 import { computeGenericPeriodNames } from './modules/analytics'
 import { BASE_FIELD_YEARLY_SERIES } from './modules/fields/baseFields'
 import LoadingMask from './widgets/LoadingMask'
+
+import './PivotPlugin.css'
 
 class PivotPlugin extends Component {
     constructor(props) {
@@ -32,17 +36,17 @@ class PivotPlugin extends Component {
     }
 
     componentDidMount() {
-        this.renderChart()
+        this.renderTable()
     }
 
     componentDidUpdate(prevProps) {
         if (!isEqual(this.props.config, prevProps.config)) {
-            this.renderChart()
+            this.renderTable()
             return
         }
 
         if (!isEqual(this.props.filters, prevProps.filters)) {
-            this.renderChart()
+            this.renderTable()
             return
         }
 
@@ -94,7 +98,7 @@ class PivotPlugin extends Component {
         return apiFetchVisualization(this.props.d2, 'chart', id)
     }
 
-    renderChart = async () => {
+    renderTable = async () => {
         const {
             config,
             filters,
@@ -104,6 +108,29 @@ class PivotPlugin extends Component {
             onError,
         } = this.props
 
+        const i18nManager = {
+            get: string => i18n.t(string),
+        }
+
+        const appManager = {
+            getLegendSetById: () => '',
+            getApiPath: () => '',
+        }
+
+        const uiManager = {}
+
+        const d2aOptionConfig = new d2aConfig.OptionConfig()
+        d2aOptionConfig.setI18nManager(i18nManager)
+        d2aOptionConfig.init()
+
+        const refs = {
+            api: d2aApi,
+            appManager,
+            uiManager,
+            i18nManager,
+            optionConfig: d2aOptionConfig,
+        }
+
         try {
             const visualization =
                 Object.keys(config).length === 1 && config.id
@@ -112,69 +139,52 @@ class PivotPlugin extends Component {
 
             const options = this.getRequestOptions(visualization, filters)
 
-            const extraOptions = {
-                dashboard: forDashboard,
-                noData: { text: i18n.t('No data') },
-            }
-
-            let responses = []
-
-            if (isYearOverYear(visualization.type)) {
-                let yearlySeriesLabels = []
-
-                ;({
-                    responses,
-                    yearlySeriesLabels,
-                } = await apiFetchAnalyticsForYearOverYear(
-                    this.props.d2,
-                    visualization,
-                    options
-                ))
-
-                extraOptions[BASE_FIELD_YEARLY_SERIES] = yearlySeriesLabels
-                extraOptions.xAxisLabels = computeGenericPeriodNames(responses)
-            } else {
-                responses = await apiFetchAnalytics(
-                    this.props.d2,
-                    visualization,
-                    options
-                )
-            }
+            const responses = await apiFetchAnalytics(
+                this.props.d2,
+                visualization,
+                options
+            )
 
             if (responses.length) {
                 onResponsesReceived(responses)
             }
 
-            this.recreateVisualization = (animation = this.props.animation) => {
-                const visualizationConfig = createVisualization(
-                    responses,
+            this.recreateVisualization = () => {
+                const remappedOptions = {
+                    showColTotals: visualization.colTotals,
+                    showRowTotals: visualization.rowTotals,
+                    showColSubTotals: visualization.colSubTotals,
+                    showRowSubTotals: visualization.rowSubTotals,
+                    numberType: visualization.numberType || 'VALUE', // TODO read default from options, perhaps better idea is to compute layout content in app
+                }
+
+                const layout = new d2aLayout(
+                    refs,
                     visualization,
-                    this.canvasRef.current,
-                    {
-                        ...extraOptions,
-                        animation,
-                    },
-                    undefined,
-                    undefined,
-                    isSingleValue(visualization.type) ? 'dhis' : 'highcharts' // output format
+                    remappedOptions
                 )
 
-                if (isSingleValue(visualization.type)) {
-                    onChartGenerated(visualizationConfig.visualization)
-                } else {
-                    onChartGenerated(
-                        visualizationConfig.visualization.getSVGForExport({
-                            sourceHeight: 768,
-                            sourceWidth: 1024,
-                        })
-                    )
-                }
+                const extraOptions = { renderLimit: 100000, trueTotals: true }
+
+                const pivotTable = new d2aTable.PivotTable(
+                    refs,
+                    layout,
+                    new d2aResponse(refs, responses[0].response),
+                    extraOptions
+                )
+
+                pivotTable.initialize()
+
+                pivotTable.build()
+
+                this.canvasRef.current.innerHTML = pivotTable.render()
             }
 
             this.recreateVisualization()
 
             this.setState({ isLoading: false })
         } catch (error) {
+            console.log(error)
             onError(error)
         }
     }
@@ -204,7 +214,6 @@ PivotPlugin.propTypes = {
     config: PropTypes.object.isRequired,
     d2: PropTypes.object.isRequired,
     onError: PropTypes.func.isRequired,
-    animation: PropTypes.number,
     filters: PropTypes.object,
     forDashboard: PropTypes.bool,
     id: PropTypes.number,
