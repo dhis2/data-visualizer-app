429c51eadb4927322b07a66974229722156e6e6d
Martin <martin@moid.se>
2019-11-25 14:47:35 +0100

    fix: dimension dialog update validation (#486)
    * fix: new pattern for centralising the validation logic for update button and add to layout button

--- packages/app/src/components/UpdateButton/UpdateButton.js
+++ packages/app/src/components/UpdateButton/UpdateButton.js
@@ -1,64 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 import i18n from '@dhis2/d2-i18n';
 
-import { sGetUi } from '../../reducers/ui';
-import { sGetCurrent, sGetCurrentFromUi } from '../../reducers/current';
-import * as fromActions from '../../actions';
-import history from '../../modules/history';
 import styles from './styles/UpdateButton.style';
-import { CURRENT_AO_KEY } from '../../api/userDataStore';
 
-import { acSetLoadError, acClearLoadError } from '../../actions/loader';
-import { validateLayout } from '../../modules/layoutValidation';
-
-const UpdateButton = ({
-    classes,
-    acClearLoadError,
-    acSetLoadError,
-    onUpdate,
-    ui,
-    current,
-    currentFromUi,
-    onClick,
-    flat,
-    ...props
-}) => {
-    const wrappedOnClick = () => {
-        // validate layout on update
-        // validation error message will be shown without loading the plugin first
-        try {
-            validateLayout(currentFromUi);
-
-            acClearLoadError();
-        } catch (err) {
-            acSetLoadError(
-                err && err.message
-                    ? err.message
-                    : i18n.t('Error validating layout')
-            );
-        }
-        onUpdate(ui);
-
-        const urlContainsCurrentAOKey =
-            history.location.pathname === '/' + CURRENT_AO_KEY;
-
-        const pathWithoutInterpretation =
-            current && current.id ? `/${current.id}` : '/';
-
-        if (
-            !urlContainsCurrentAOKey &&
-            history.location.pathname !== pathWithoutInterpretation
-        ) {
-            history.push(pathWithoutInterpretation);
-        }
-
-        onClick();
-    };
-
+const UpdateButton = ({ classes, flat, onClick, ...props }) => {
     return (
         <Button
             data-test="update-button"
@@ -68,7 +16,7 @@ const UpdateButton = ({
             }
             variant="contained"
             color="primary"
-            onClick={wrappedOnClick}
+            onClick={onClick}
             disableRipple={true}
             disableFocusRipple={true}
         >
@@ -77,32 +25,15 @@ const UpdateButton = ({
     );
 };
 
-const mapStateToProps = state => ({
-    ui: sGetUi(state),
-    current: sGetCurrent(state),
-    currentFromUi: sGetCurrentFromUi(state),
-});
-
-const mapDispatchToProps = {
-    onUpdate: fromActions.fromCurrent.acSetCurrentFromUi,
-    acSetLoadError,
-    acClearLoadError,
-};
-
 UpdateButton.propTypes = {
     classes: PropTypes.object.isRequired,
-    ui: PropTypes.object.isRequired,
-    onUpdate: PropTypes.func.isRequired,
-    onClick: PropTypes.func,
     flat: PropTypes.bool,
+    onClick: PropTypes.func,
 };
 
 UpdateButton.defaultProps = {
-    onClick: Function.prototype,
     flat: false,
+    onClick: Function.prototype,
 };
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(withStyles(styles)(UpdateButton));
+export default withStyles(styles)(UpdateButton);
