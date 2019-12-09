8f8633afb8cae40af853100f99188de8e3f66d4e
Martin Ohlson <martin@moid.se>
2019-12-09 11:12:11 +0100

    refactor: updated test to use correct default value for animation

--- packages/plugin/src/__tests__/ChartPlugin.spec.js
+++ packages/plugin/src/__tests__/ChartPlugin.spec.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import * as analytics from '@dhis2/analytics';
+import React from 'react'
+import { shallow } from 'enzyme'
+import * as analytics from '@dhis2/analytics'
 
-import LoadingMask from '../widgets/LoadingMask';
-import ChartPlugin from '../ChartPlugin';
-import * as api from '../api/analytics';
-import * as apiViz from '../api/visualization';
-import * as options from '../modules/options';
+import LoadingMask from '../widgets/LoadingMask'
+import ChartPlugin from '../ChartPlugin'
+import * as api from '../api/analytics'
+import * as apiViz from '../api/visualization'
+import * as options from '../modules/options'
 
-jest.mock('@dhis2/analytics');
+jest.mock('@dhis2/analytics')
 
 const dxMock = {
     dimension: 'dx',
@@ -17,7 +17,7 @@ const dxMock = {
             id: 'Uvn6LCg7dVU',
         },
     ],
-};
+}
 
 const peMock = {
     dimension: 'pe',
@@ -26,7 +26,7 @@ const peMock = {
             id: 'LAST_12_MONTHS',
         },
     ],
-};
+}
 
 const ouMock = {
     dimension: 'ou',
@@ -35,28 +35,28 @@ const ouMock = {
             id: 'ImspTQPwCqd',
         },
     ],
-};
+}
 
 const defaultCurrentMock = {
     type: analytics.VIS_TYPE_COLUMN,
     columns: [dxMock],
     rows: [peMock],
     filters: [ouMock],
-};
+}
 
 const yearOverYearCurrentMock = {
     type: analytics.VIS_TYPE_YEAR_OVER_YEAR_LINE,
     columns: [dxMock],
     rows: [peMock],
     yearlySeries: ['LAST_YEAR'],
-};
+}
 
 const singleValueCurrentMock = {
     type: analytics.VIS_TYPE_SINGLE_VALUE,
     columns: [dxMock],
     rows: [],
     filters: [ouMock, peMock],
-};
+}
 
 const metaDataMock = {
     items: {
@@ -68,25 +68,25 @@ const metaDataMock = {
     dimensions: {
         pe: ['p1', 'p2'],
     },
-};
+}
 
 const analyticsResponse = {
     metaData: metaDataMock,
-};
+}
 
 class MockAnalyticsResponse {
     constructor() {
-        return analyticsResponse;
+        return analyticsResponse
     }
 }
 
-const mockYoYSeriesLabels = ['rainbow', 'rarity'];
+const mockYoYSeriesLabels = ['rainbow', 'rarity']
 class MockYoYAnalyticsResponse {
     constructor() {
         return {
             responses: [analyticsResponse],
             yearlySeriesLabels: mockYoYSeriesLabels,
-        };
+        }
     }
 }
 
@@ -94,29 +94,29 @@ const createVisualizationMock = {
     visualization: {
         getSVGForExport: () => '<svg />',
     },
-};
+}
 
 const isYearOverYearMockResponse = visType => {
-    return visType === analytics.VIS_TYPE_YEAR_OVER_YEAR_LINE;
-};
+    return visType === analytics.VIS_TYPE_YEAR_OVER_YEAR_LINE
+}
 
 const isSingleValueMockResponse = visType => {
-    return visType === analytics.VIS_TYPE_SINGLE_VALUE;
-};
+    return visType === analytics.VIS_TYPE_SINGLE_VALUE
+}
 
 describe('ChartPlugin', () => {
     options.getOptionsForRequest = () => [
         ['option1', { defaultValue: 'abc' }],
         ['option2', { defaultValue: null }],
-    ];
-    let props;
-    let shallowChartPlugin;
+    ]
+    let props
+    let shallowChartPlugin
     const canvas = () => {
         if (!shallowChartPlugin) {
-            shallowChartPlugin = shallow(<ChartPlugin {...props} />);
+            shallowChartPlugin = shallow(<ChartPlugin {...props} />)
         }
-        return shallowChartPlugin;
-    };
+        return shallowChartPlugin
+    }
 
     beforeEach(() => {
         props = {
@@ -129,235 +129,235 @@ describe('ChartPlugin', () => {
             onChartGenerated: jest.fn(),
             onResponsesReceived: jest.fn(),
             onError: jest.fn(),
-        };
-        shallowChartPlugin = undefined;
+        }
+        shallowChartPlugin = undefined
 
         apiViz.apiFetchVisualization = jest
             .fn()
-            .mockResolvedValue(defaultCurrentMock);
+            .mockResolvedValue(defaultCurrentMock)
 
         api.apiFetchAnalytics = jest
             .fn()
-            .mockResolvedValue([new MockAnalyticsResponse()]);
-    });
+            .mockResolvedValue([new MockAnalyticsResponse()])
+    })
 
     it('renders the loading indicator', () => {
-        props.loading = true;
+        props.loading = true
         expect(
             canvas()
                 .find(LoadingMask)
                 .exists()
-        ).toBeTruthy();
-    });
+        ).toBeTruthy()
+    })
 
     describe('createVisualization success', () => {
         beforeEach(() => {
             analytics.createVisualization = jest
                 .fn()
-                .mockReturnValue(createVisualizationMock);
-        });
+                .mockReturnValue(createVisualizationMock)
+        })
 
         it('renders a div', done => {
-            expect(canvas().find('div').length).toBeGreaterThan(0);
-            done();
-        });
+            expect(canvas().find('div').length).toBeGreaterThan(0)
+            done()
+        })
 
         it('uses the style passed as prop', done => {
             expect(
                 canvas()
                     .find('div')
                     .prop('style')
-            ).toEqual(props.style);
-            done();
-        });
+            ).toEqual(props.style)
+            done()
+        })
 
         it('calls createVisualization', done => {
-            canvas();
+            canvas()
 
             setTimeout(() => {
-                expect(analytics.createVisualization).toHaveBeenCalled();
-                done();
-            });
-        });
+                expect(analytics.createVisualization).toHaveBeenCalled()
+                done()
+            })
+        })
 
         it('includes only options that do not have default value in request', done => {
             props.config = {
                 ...defaultCurrentMock,
                 option1: 'def',
                 option2: null,
-            };
+            }
 
-            canvas();
+            canvas()
 
             setTimeout(() => {
-                expect(api.apiFetchAnalytics).toHaveBeenCalled();
+                expect(api.apiFetchAnalytics).toHaveBeenCalled()
                 expect(api.apiFetchAnalytics.mock.calls[0][2]).toEqual({
                     option1: 'def',
-                });
+                })
 
-                done();
-            });
-        });
+                done()
+            })
+        })
 
         it('fetches the AO by id when only id is passed in config', done => {
-            props.forDashboard = true;
+            props.forDashboard = true
             props.config = {
                 id: 'test1',
-            };
+            }
 
-            canvas();
+            canvas()
 
             setTimeout(() => {
-                expect(apiViz.apiFetchVisualization).toHaveBeenCalled();
+                expect(apiViz.apiFetchVisualization).toHaveBeenCalled()
                 expect(apiViz.apiFetchVisualization).toHaveBeenCalledWith(
                     props.d2,
                     'chart',
                     'test1'
-                );
+                )
 
-                done();
-            });
-        });
+                done()
+            })
+        })
 
         it('calls onResponsesReceived callback', done => {
-            canvas();
+            canvas()
 
             setTimeout(() => {
-                expect(props.onResponsesReceived).toHaveBeenCalled();
+                expect(props.onResponsesReceived).toHaveBeenCalled()
                 expect(props.onResponsesReceived).toHaveBeenCalledWith([
                     analyticsResponse,
-                ]);
-                done();
-            });
-        });
+                ])
+                done()
+            })
+        })
 
         it('calls onChartGenerated callback', done => {
-            canvas();
+            canvas()
 
             setTimeout(() => {
-                expect(props.onChartGenerated).toHaveBeenCalled();
+                expect(props.onChartGenerated).toHaveBeenCalled()
                 expect(props.onChartGenerated).toHaveBeenCalledWith(
                     createVisualizationMock.visualization.getSVGForExport()
-                );
-                done();
-            });
-        });
+                )
+                done()
+            })
+        })
 
         it('calls onError callback when an exception is thrown', done => {
-            api.apiFetchAnalytics = jest.fn().mockRejectedValue('error');
+            api.apiFetchAnalytics = jest.fn().mockRejectedValue('error')
 
-            canvas();
+            canvas()
 
             setTimeout(() => {
-                expect(props.onError).toHaveBeenCalled();
-                done();
-            });
-        });
+                expect(props.onError).toHaveBeenCalled()
+                done()
+            })
+        })
 
         it('sets period when interpretation selected', done => {
-            const period = 'eons ago';
-            props.filters.relativePeriodDate = period;
+            const period = 'eons ago'
+            props.filters.relativePeriodDate = period
 
-            canvas();
+            canvas()
 
             setTimeout(() => {
-                expect(api.apiFetchAnalytics).toHaveBeenCalled();
+                expect(api.apiFetchAnalytics).toHaveBeenCalled()
                 expect(api.apiFetchAnalytics.mock.calls[0][2]).toHaveProperty(
                     'relativePeriodDate',
                     period
-                );
+                )
 
-                done();
-            });
-        });
+                done()
+            })
+        })
 
         describe('Year-on-year chart', () => {
             beforeEach(() => {
                 props.config = {
                     ...yearOverYearCurrentMock,
                     option1: 'def',
-                };
+                }
 
                 api.apiFetchAnalyticsForYearOverYear = jest
                     .fn()
-                    .mockResolvedValue(new MockYoYAnalyticsResponse());
+                    .mockResolvedValue(new MockYoYAnalyticsResponse())
 
                 analytics.isYearOverYear = jest
                     .fn()
                     .mockReturnValue(
                         isYearOverYearMockResponse(props.config.type)
-                    );
-            });
+                    )
+            })
 
             it('makes year-on-year analytics request', done => {
-                canvas();
+                canvas()
 
                 setTimeout(() => {
                     expect(
                         api.apiFetchAnalyticsForYearOverYear
-                    ).toHaveBeenCalled();
+                    ).toHaveBeenCalled()
                     expect(
                         api.apiFetchAnalyticsForYearOverYear.mock.calls[0][1]
                     ).toEqual({
                         ...yearOverYearCurrentMock,
                         option1: 'def',
-                    });
+                    })
 
-                    done();
-                });
-            });
+                    done()
+                })
+            })
 
             it('provides extra options to createVisualization', done => {
-                canvas();
+                canvas()
 
                 setTimeout(() => {
-                    expect(analytics.createVisualization).toHaveBeenCalled();
+                    expect(analytics.createVisualization).toHaveBeenCalled()
 
                     const expectedExtraOptions = {
                         yearlySeries: mockYoYSeriesLabels,
                         xAxisLabels: ['period 1', 'period 2'],
                         noData: { text: 'No data' },
-                    };
+                    }
 
                     expect(
                         analytics.createVisualization.mock.calls[0][3]
                     ).toEqual({
-                        animation: undefined,
+                        animation: 200, // default prop
                         dashboard: false,
                         ...expectedExtraOptions,
-                    });
+                    })
 
-                    done();
-                });
-            });
-        });
+                    done()
+                })
+            })
+        })
 
         describe('Single value visualization', () => {
             beforeEach(() => {
                 props.config = {
                     ...singleValueCurrentMock,
-                };
+                }
 
                 analytics.isSingleValue = jest
                     .fn()
                     .mockReturnValue(
                         isSingleValueMockResponse(props.config.type)
-                    );
-            });
+                    )
+            })
 
             it('provides dhis as output format to createChart', done => {
-                canvas();
+                canvas()
 
                 setTimeout(() => {
-                    expect(analytics.createVisualization).toHaveBeenCalled();
+                    expect(analytics.createVisualization).toHaveBeenCalled()
 
                     expect(
                         analytics.createVisualization.mock.calls[0][6]
-                    ).toEqual('dhis');
+                    ).toEqual('dhis')
 
-                    done();
-                });
-            });
-        });
-    });
-});
+                    done()
+                })
+            })
+        })
+    })
+})
