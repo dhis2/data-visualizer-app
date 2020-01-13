c9a9da2a6c2c79dbe067d6e3081928e8c2154429
Jan Henrik Overland <janhenrik.overland@gmail.com>
2020-01-13 15:48:41 +0100

    fix: add dataStatistics on vis load

---
+++ packages/app/src/api/dataStatistics.js
@@ -0,0 +1,12 @@
+import { getInstance } from 'd2';
+
+const EVENT_TYPE_CHART_VIEW = 'CHART_VIEW';
+
+export const apiPostDataStatistics = async (id) => {
+    const d2 = await getInstance();
+    const api = d2.Api.getApi();
+
+    const url = `${api.baseUrl}/dataStatistics?eventType=${EVENT_TYPE_CHART_VIEW}&favorite=${id}`;
+
+    return api.post(url);
+};
