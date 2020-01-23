c042b7983a0f4ea2a1952607239fc362ff01d616
Martin Ohlson <martin@moid.se>
2020-01-23 19:36:46 +0530

    fix: changed svg ids to prevent collision with other svgs

--- packages/app/src/assets/YearOverYearLineIcon.js
+++ packages/app/src/assets/YearOverYearLineIcon.js
@@ -8,25 +8,25 @@ const YearOverYearLineIcon = ({
     <SvgIcon viewBox="0,0,48,48" style={style}>
         <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
             <g>
-                <mask id="mask-2" fill="white">
+                <mask id="mask-yoyl-icon" fill="white">
                     <rect x="0" y="0" width="48" height="48" />
                 </mask>
                 <polygon
                     stroke="#1976D2"
                     strokeWidth="2"
-                    mask="url(#mask-2)"
+                    mask="url(#mask-yoyl-icon)"
                     points="1 28 10 35 18 16 34 35 50 36 50 51 -3 51 -3 28"
                 />
                 <polygon
                     stroke="#004BA0"
                     strokeWidth="2"
-                    mask="url(#mask-2)"
+                    mask="url(#mask-yoyl-icon)"
                     points="1 20 8 20 19 29 36 17 50 28 50 51 -3 51 -3 20"
                 />
                 <polygon
                     stroke="#63A4FF"
                     strokeWidth="2"
-                    mask="url(#mask-2)"
+                    mask="url(#mask-yoyl-icon)"
                     points="1 35 19 39 28 31 38 28 50 43 50 66 -3 66 -3 35"
                 />
                 <rect fill="#9E9E9E" x="0" y="0" width="2" height="48" />
