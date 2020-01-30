8d19cbaea086414a48c60e7dd2f0639d3890b57f
Edoardo Sabadelli <edoardo@dhis2.org>
2020-01-30 12:35:53 +0100

    fix: use strings for Select options
    ui-core does not accept numeric values for the Select options.

--- packages/app/src/components/VisualizationOptions/Options/SortOrder.js
+++ packages/app/src/components/VisualizationOptions/Options/SortOrder.js
@@ -1,89 +1,25 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React from 'react'
 
 import i18n from '@dhis2/d2-i18n'
 
-import { Checkbox, SingleSelectField, SingleSelectOption } from '@dhis2/ui-core'
-
-import { sGetUiOptions } from '../../../reducers/ui'
-import { acSetUiOptions } from '../../../actions/ui'
-
-import {
-    tabSectionOptionItem,
-    tabSectionOptionToggleable,
-} from '../styles/VisualizationOptions.style.js'
-
+import SelectBaseOption from './SelectBaseOption'
 import { options } from '../../../modules/options'
 
 const optionName = 'sortOrder'
 const defaultValue = options[optionName].defaultValue
 
-const SortOrder = ({ value, onChange }) => {
-    const [enabled, setEnabled] = useState(Boolean(value))
-
-    const options = [
-        { value: -1, label: i18n.t('Low to high') },
-        { value: 1, label: i18n.t('High to low') },
-    ]
-
-    const selected = options.find(option => option.value === value) || {}
-
-    const onToggle = checked => {
-        setEnabled(checked)
-
-        onChange({
-            sortOrder: checked ? -1 : defaultValue,
-        })
-    }
-
-    return (
-        <div className={enabled ? '' : tabSectionOptionItem.className}>
-            <Checkbox
-                checked={enabled}
-                label={i18n.t('Custom sort order')}
-                name="sortOrder-toggle"
-                onChange={({ checked }) => onToggle(checked)}
-                dense
-            />
-            {enabled ? (
-                <div className={tabSectionOptionToggleable.className}>
-                    <SingleSelectField
-                        name="sortOrder-select"
-                        selected={selected}
-                        onChange={({ selected }) =>
-                            onChange({
-                                sortOrder: selected.value,
-                            })
-                        }
-                        inputWidth="280px"
-                        dense
-                    >
-                        {options.map(({ value, label }) => (
-                            <SingleSelectOption
-                                key={value}
-                                value={value}
-                                label={label}
-                            />
-                        ))}
-                    </SingleSelectField>
-                </div>
-            ) : null}
-        </div>
-    )
-}
-
-SortOrder.propTypes = {
-    value: PropTypes.number.isRequired,
-    onChange: PropTypes.func.isRequired,
-}
-
-const mapStateToProps = state => ({
-    value: sGetUiOptions(state)[optionName] || defaultValue,
-})
-
-const mapDispatchToProps = dispatch => ({
-    onChange: value => dispatch(acSetUiOptions(value)),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(SortOrder)
+const SortOrder = () => (
+    <SelectBaseOption
+        label={i18n.t('Custom sort order')}
+        toggleable={true}
+        option={{
+            name: optionName,
+            defaultValue: defaultValue,
+            items: [
+                { value: '-1', label: i18n.t('Low to high') },
+                { value: '1', label: i18n.t('High to low') },
+            ],
+        }}
+    />
+)
+export default SortOrder
