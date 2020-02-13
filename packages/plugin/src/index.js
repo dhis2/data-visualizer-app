86095d6b12d4463a13a55e9f7359440aee41752d
Edoardo Sabadelli <edoardo@dhis2.org>
2020-02-13 15:23:32 +0100

    feat: fetch legendSet and pass as prop to plugins

--- packages/plugin/src/index.js
+++ packages/plugin/src/index.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { VIS_TYPE_PIVOT_TABLE } from '@dhis2/analytics'
+import { useDataEngine } from '@dhis2/app-runtime'
 
+import { apiFetchLegendSet } from './api/legendSets'
 import ChartPlugin from './ChartPlugin'
 import PivotPlugin from './PivotPlugin'
 
 const VisualizationPlugin = props => {
+    const engine = useDataEngine()
+    const [legendSet, setLegendSet] = useState(null)
+
+    useEffect(() => {
+        const fetchLegendSet = async engine => {
+            if (
+                props.visualization.legendSet &&
+                props.visualization.legendSet.id
+            ) {
+                const response = await apiFetchLegendSet(
+                    engine,
+                    props.visualization.legendSet.id
+                )
+
+                if (response && response.legendSet) {
+                    setLegendSet(response.legendSet)
+                }
+            }
+        }
+
+        fetchLegendSet(engine)
+    }, [props.visualization.legendSet]) // eslint-disable-line react-hooks/exhaustive-deps
+
     if (
         !props.visualization.type ||
         props.visualization.type === VIS_TYPE_PIVOT_TABLE
     ) {
-        return <PivotPlugin {...props} />
+        return <PivotPlugin {...props} legendSet={legendSet} />
     } else {
-        return <ChartPlugin {...props} />
+        return <ChartPlugin {...props} legendSet={legendSet} />
     }
 }
 
