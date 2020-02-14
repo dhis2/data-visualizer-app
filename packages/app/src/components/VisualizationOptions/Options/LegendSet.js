8aa6586a6bc8e1718dd4dbde0697760ff2b62c0b
Edoardo Sabadelli <edoardo@dhis2.org>
2020-02-14 16:26:26 +0100

    fix: fix default state for LegendSet option
    The selection for Legend type was lost when switching between tabs,
    causing the app to crash.
    Use a constant for the option name and get the default value from the
    options list.
    Moved also the query to the top.

--- packages/app/src/components/VisualizationOptions/Options/LegendSet.js
+++ packages/app/src/components/VisualizationOptions/Options/LegendSet.js
@@ -18,11 +18,27 @@ import LegendDisplayStrategy from './LegendDisplayStrategy'
 import { sGetUiOptions } from '../../../reducers/ui'
 import { acSetUiOptions } from '../../../actions/ui'
 
+import { options } from '../../../modules/options'
+
 import {
     tabSection,
     tabSectionOption,
     tabSectionTitle,
 } from '../styles/VisualizationOptions.style.js'
+
+const optionName = 'legendSet'
+const defaultValue = options[optionName].defaultValue
+
+const query = {
+    legendSets: {
+        resource: 'legendSets.json',
+        params: {
+            fields:
+                'id,displayName~rename(name),legends[id,displayName~rename(name),startValue,endValue,color]',
+            paging: false,
+        },
+    },
+}
 
 const LegendSelect = ({ value, loading, options, onFocus, onChange }) => {
     const selected =
@@ -41,10 +57,8 @@ const LegendSelect = ({ value, loading, options, onFocus, onChange }) => {
             onFocus={onFocus}
             onChange={({ selected }) =>
                 onChange({
-                    legendSet: {
-                        id: selected.value,
-                        displayName: selected.label,
-                    },
+                    id: selected.value,
+                    displayName: selected.label,
                 })
             }
         >
@@ -80,17 +94,6 @@ const LegendSetup = ({ value, onChange }) => {
 
     const onSelectFocus = async () => {
         if (!isLoaded) {
-            const query = {
-                legendSets: {
-                    resource: 'legendSets.json',
-                    params: () => ({
-                        fields:
-                            'id,displayName~rename(name),legends[id,displayName~rename(name),startValue,endValue,color]',
-                        paging: false,
-                    }),
-                },
-            }
-
             const { legendSets } = await engine.query(query)
 
             if (legendSets) {
@@ -128,15 +131,8 @@ const LegendSet = ({ value, legendDisplayStrategy, onChange }) => {
     const onCheckboxChange = ({ checked }) => {
         setLegendSetEnabled(checked)
 
-        if (checked && !legendDisplayStrategy) {
-            onChange({
-                legendDisplayStrategy: 'FIXED',
-            })
-        } else {
-            onChange({
-                legendSet: undefined,
-                legendDisplayStrategy: undefined,
-            })
+        if (!checked) {
+            onChange(defaultValue)
         }
     }
 
@@ -193,12 +189,13 @@ LegendSet.propTypes = {
 }
 
 const mapStateToProps = state => ({
-    value: sGetUiOptions(state).legendSet || {},
+    value: sGetUiOptions(state)[optionName] || {},
     legendDisplayStrategy: sGetUiOptions(state).legendDisplayStrategy,
 })
 
 const mapDispatchToProps = dispatch => ({
-    onChange: legendSetOptions => dispatch(acSetUiOptions(legendSetOptions)),
+    onChange: ({ id, displayName }) =>
+        dispatch(acSetUiOptions({ [optionName]: { id, displayName } })),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(LegendSet)
