ec78234470f7dd89613cc10b77d52fd52e12ff42
Martin <martin@moid.se>
2020-02-25 09:36:01 +0100

    fix: single value legend (DHIS2-8348) (#722)
    Adds Single Value legend options. Since SV can only change the text color, the "style" option thats present for other visualisation types has been hidden.
    
    Updates Analytics dep to v4.1.3, which adds the actual visual implementation of the SV legends.

---
+++ packages/app/src/modules/options/singleValueConfig.js
@@ -0,0 +1,48 @@
+/* eslint-disable react/jsx-key */
+import React from 'react'
+import i18n from '@dhis2/d2-i18n'
+
+import AggregationType from '../../components/VisualizationOptions/Options/AggregationType'
+import HideTitle from '../../components/VisualizationOptions/Options/HideTitle'
+import HideSubtitle from '../../components/VisualizationOptions/Options/HideSubtitle'
+import Legend from '../../components/VisualizationOptions/Options/Legend'
+
+export default [
+    {
+        key: 'data-tab',
+        label: i18n.t('Data'),
+        content: [
+            {
+                key: 'data-advanced',
+                label: i18n.t('Advanced'),
+                content: React.Children.toArray([<AggregationType />]),
+            },
+        ],
+    },
+    {
+        key: 'legend-tab',
+        label: i18n.t('Legend'),
+        content: [
+            {
+                key: 'legend-section-1',
+                content: React.Children.toArray([
+                    <Legend hideStyleOptions={true} />,
+                ]),
+            },
+        ],
+    },
+    {
+        key: 'style-tab',
+        label: i18n.t('Style'),
+        content: [
+            {
+                key: 'style-titles',
+                label: i18n.t('Titles'),
+                content: React.Children.toArray([
+                    <HideTitle />,
+                    <HideSubtitle />,
+                ]),
+            },
+        ],
+    },
+]
