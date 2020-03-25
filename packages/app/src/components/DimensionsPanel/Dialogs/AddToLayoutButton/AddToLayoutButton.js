c95b2865a1c888cf443caf5447e5fb92045e6e6b
Martin Ohlson <martin@moid.se>
2020-03-25 14:58:03 +0100

    fix: replaces MUI for AddToLayoutButton  TECH-319

--- packages/app/src/components/DimensionsPanel/Dialogs/AddToLayoutButton/AddToLayoutButton.js
+++ packages/app/src/components/DimensionsPanel/Dialogs/AddToLayoutButton/AddToLayoutButton.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import Button from '@material-ui/core/Button'
-import MenuItem from '@material-ui/core/Button'
-import { withStyles } from '@material-ui/core/styles'
 import {
     getAvailableAxes,
     getAxisNameByLayoutType,
     getLayoutTypeByVisType,
 } from '@dhis2/analytics'
+import { SplitButton, Menu, MenuItem, Button } from '@dhis2/ui-core'
 
-import Menu from './Menu'
 import { sGetUiActiveModalDialog, sGetUiType } from '../../../../reducers/ui'
 import { acAddUiLayoutDimensions } from '../../../../actions/ui'
 
-import styles from './styles/AddToLayoutButton.style'
 import i18n from '@dhis2/d2-i18n'
 
 export class AddToLayoutButton extends Component {
@@ -45,32 +41,37 @@ export class AddToLayoutButton extends Component {
             .slice(1)
             .map(axis => (
                 <MenuItem
-                    className={this.props.classes.menuItem}
-                    component="li"
                     key={axis}
                     onClick={() => this.onUpdate(axis)}
-                >
-                    {i18n.t(`Add to {{axisName}}`, {
+                    label={i18n.t(`Add to {{axisName}}`, {
                         axisName: getAxisNameByLayoutType(
                             axis,
                             getLayoutTypeByVisType(this.props.visType)
                         ),
                     })}
-                </MenuItem>
+                />
             ))
 
     render() {
         const availableAxes = getAvailableAxes(this.props.visType)
 
+        const ATLButton = availableAxes.length > 1 ? SplitButton : Button
+
+        // TODO: Fix bug with menu items rendering behind the dialog
+        // TODO: Remove files Menu.s and DropDownButton.js (and their respective style.js) if they are now unused?
+
         return (
             <div ref={addToRef => (this.buttonRef = addToRef)}>
-                <Button
-                    className={this.props.classes.button}
-                    variant="contained"
-                    color="primary"
-                    disableRipple
-                    disableFocusRipple
+                <ATLButton
+                    component={
+                        availableAxes.length > 1 ? (
+                            <Menu maxWidth="380px">
+                                {this.renderMenuItems()}
+                            </Menu>
+                        ) : null
+                    }
                     onClick={() => this.onUpdate(availableAxes[0])}
+                    primary
                 >
                     {i18n.t(`Add to {{axisName}}`, {
                         axisName: getAxisNameByLayoutType(
@@ -78,23 +79,13 @@ export class AddToLayoutButton extends Component {
                             getLayoutTypeByVisType(this.props.visType)
                         ),
                     })}
-                </Button>
-                {availableAxes.length > 1 ? (
-                    <Menu
-                        onClose={this.onClose}
-                        onClick={this.onToggle}
-                        anchorEl={this.state.anchorEl}
-                        menuItems={this.renderMenuItems()}
-                        addToButtonRef={this.buttonRef}
-                    />
-                ) : null}
+                </ATLButton>
             </div>
         )
     }
 }
 
 AddToLayoutButton.propTypes = {
-    classes: PropTypes.object.isRequired,
     dialogId: PropTypes.string.isRequired,
     visType: PropTypes.string.isRequired,
     onAddDimension: PropTypes.func.isRequired,
@@ -108,4 +99,4 @@ const mapStateToProps = state => ({
 
 export default connect(mapStateToProps, {
     onAddDimension: acAddUiLayoutDimensions,
-})(withStyles(styles)(AddToLayoutButton))
+})(AddToLayoutButton)
