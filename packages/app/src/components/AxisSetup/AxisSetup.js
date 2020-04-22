36e4a79c9d6135db4e4f5871adf066c3296a780f
Martin <martin@moid.se>
2020-04-22 16:05:44 +0200

    fix: AxisSetup MUI -> ui-core (TECH-359) (#876)
    * fix: mui to ui-core table (TECH-359)
    Updates the chart axes modal (AxisSetup) to use ui-core instead of MUI.
    https://jira.dhis2.org/browse/TECH-359

--- packages/app/src/components/AxisSetup/AxisSetup.js
+++ packages/app/src/components/AxisSetup/AxisSetup.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import i18n from '@dhis2/d2-i18n'
-import isEqual from 'lodash-es/isEqual'
+import {
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    Button,
+    Radio,
+    Modal,
+    ModalTitle,
+    ModalContent,
+    ModalActions,
+    ButtonStrip,
+} from '@dhis2/ui-core'
 
-import { withStyles } from '@material-ui/core/styles'
-import Dialog from '@material-ui/core/Dialog'
-import DialogActions from '@material-ui/core/DialogActions'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import DialogContent from '@material-ui/core/DialogContent'
-import Button from '@material-ui/core/Button'
-import Radio from '@material-ui/core/Radio'
-import Table from '@material-ui/core/Table'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import TableCell from '@material-ui/core/TableCell'
-import TableBody from '@material-ui/core/TableBody'
-
-import styles from './styles/AxisSetup.style'
+import styles from './styles/AxisSetup.module.css'
 import { axis1, axis2 } from './constants'
 import { sGetUiActiveModalDialog, DEFAULT_UI, sGetUi } from '../../reducers/ui'
 import { sGetAxisSetupItems } from '../../reducers'
@@ -26,50 +26,31 @@ import { acSetCurrentFromUi } from '../../actions/current'
 
 export const AXIS_SETUP_DIALOG_ID = 'axisSetup'
 
-class AxisSetup extends Component {
-    state = {
-        items: undefined,
-    }
+const AxisSetup = ({ isOpen, onUpdateClick, initItems, ui, onCancelClick }) => {
+    const [items, setItems] = useState()
 
-    componentDidMount() {
-        this.setItems(this.props.items)
-    }
+    useEffect(() => {
+        setItems(
+            initItems.reduce((itemsMap, item) => {
+                itemsMap[item.id] = item
+                return itemsMap
+            }, {})
+        )
+    }, [initItems])
 
-    componentDidUpdate(prevProps) {
-        const oldItems = prevProps.items
-        const newItems = this.props.items
-
-        if (!isEqual(oldItems, newItems)) {
-            this.setItems(newItems)
-        }
-    }
-
-    setItems = items => {
-        const itemsMap = items.reduce((itemsMap, item) => {
-            itemsMap[item.id] = item
-            return itemsMap
-        }, {})
-
-        this.setState({
-            items: itemsMap,
-        })
-    }
-
-    onAxisChange = (item, axis) => {
-        this.setState({
-            items: {
-                ...this.state.items,
-                [item.id]: {
-                    ...item,
-                    axis,
-                },
+    const onAxisChange = (item, axis) => {
+        setItems({
+            ...items,
+            [item.id]: {
+                ...item,
+                axis,
             },
         })
     }
 
-    getAxes = () => {
-        const axes = Object.keys(this.state.items).reduce((map, id) => {
-            const axis = this.state.items[id].axis
+    const getAxes = () => {
+        const axes = Object.keys(items).reduce((map, id) => {
+            const axis = items[id].axis
 
             if (axis > 0) {
                 map[id] = axis
@@ -81,66 +62,46 @@ class AxisSetup extends Component {
         return Object.keys(axes).length > 0 ? axes : DEFAULT_UI.axes
     }
 
-    renderTable() {
-        const { classes } = this.props
-
+    const renderTable = () => {
         return (
-            <Table>
+            <Table className={styles.table}>
                 <colgroup>
-                    <col className={classes.nameColumn} />
-                    <col
-                        className={`${classes.axisColumn} ${classes.coloredColumn}`}
-                    />
-                    <col className={classes.axisColumn} />
+                    <col className={styles.nameColumn} />
+                    <col className={styles.coloredColumn} />
+                    <col className={styles.axisColumn} />
                 </colgroup>
                 <TableHead>
                     <TableRow>
-                        <TableCell className={classes.head} />
-                        <TableCell
-                            className={`${classes.tableCell} ${classes.head}`}
-                            align="center"
-                        >
+                        <TableCell />
+                        <TableCell className={styles.centered}>
                             {i18n.t('Axis 1')}
                         </TableCell>
-                        <TableCell
-                            className={`${classes.tableCell} ${classes.head}`}
-                            align="center"
-                        >
+                        <TableCell className={styles.centered}>
                             {i18n.t('Axis 2')}
                         </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {Object.keys(this.state.items).map(id => {
-                        const item = this.state.items[id]
+                    {Object.keys(items).map(id => {
+                        const item = items[id]
 
                         return (
                             <TableRow key={`multiaxis-table-row-${id}`}>
-                                <TableCell className={classes.tableCell}>
-                                    {item.name}
-                                </TableCell>
-                                <TableCell
-                                    className={classes.tableCell}
-                                    align="center"
-                                >
+                                <TableCell>{item.name}</TableCell>
+                                <TableCell>
                                     <Radio
-                                        onClick={() =>
-                                            this.onAxisChange(item, axis1)
+                                        onChange={() =>
+                                            onAxisChange(item, axis1)
                                         }
                                         checked={item.axis === axis1}
-                                        className={classes.axisRadio}
                                     />
                                 </TableCell>
-                                <TableCell
-                                    className={classes.tableCell}
-                                    align="center"
-                                >
+                                <TableCell className={styles.centered}>
                                     <Radio
-                                        onClick={() =>
-                                            this.onAxisChange(item, axis2)
+                                        onChange={() =>
+                                            onAxisChange(item, axis2)
                                         }
                                         checked={item.axis === axis2}
-                                        className={classes.axisRadio}
                                     />
                                 </TableCell>
                             </TableRow>
@@ -150,61 +111,40 @@ class AxisSetup extends Component {
             </Table>
         )
     }
-
-    render() {
-        const { classes, isOpen, dialogMaxWidth, onCancelClick } = this.props
-
-        return (
-            <Dialog
-                open={isOpen}
-                maxWidth={dialogMaxWidth}
-                onClose={onCancelClick}
-                disableEnforceFocus
-            >
-                <DialogTitle>{i18n.t('Manage axes')}</DialogTitle>
-                <DialogContent className={classes.dialogContent}>
-                    <p className={classes.helpText}>
+    return (
+        isOpen && (
+            <Modal onClose={onCancelClick}>
+                <ModalTitle>{i18n.t('Manage axes')}</ModalTitle>
+                <ModalContent>
+                    <p>
                         {i18n.t(
                             'A chart can have two axes. Each axis will have its own scale. Set the axis for each data selection below.'
                         )}
                     </p>
-                    <div className={classes.tableContainer}>
-                        {this.state.items && this.renderTable()}
-                    </div>
-                </DialogContent>
-                <DialogActions className={classes.dialogActions}>
-                    <Button
-                        color="primary"
-                        disableRipple
-                        disableFocusRipple
-                        onClick={onCancelClick}
-                    >
-                        {i18n.t('Cancel')}
-                    </Button>
-                    <Button
-                        color="primary"
-                        variant="contained"
-                        onClick={() =>
-                            this.props.onUpdateClick(
-                                this.getAxes(),
-                                this.props.ui
-                            )
-                        }
-                    >
-                        {i18n.t('Update')}
-                    </Button>
-                </DialogActions>
-            </Dialog>
+                    {items && renderTable()}
+                </ModalContent>
+                <ModalActions>
+                    <ButtonStrip end>
+                        <Button onClick={onCancelClick}>
+                            {i18n.t('Cancel')}
+                        </Button>
+                        <Button
+                            primary
+                            onClick={() => onUpdateClick(getAxes(), ui)}
+                        >
+                            {i18n.t('Update')}
+                        </Button>
+                    </ButtonStrip>
+                </ModalActions>
+            </Modal>
         )
-    }
+    )
 }
 
 AxisSetup.propTypes = {
     isOpen: PropTypes.bool.isRequired,
     onUpdateClick: PropTypes.func.isRequired,
-    classes: PropTypes.object,
-    dialogMaxWidth: PropTypes.string,
-    items: PropTypes.arrayOf(
+    initItems: PropTypes.arrayOf(
         PropTypes.shape({
             axis: PropTypes.number.isRequired,
             id: PropTypes.string.isRequired,
@@ -216,17 +156,15 @@ AxisSetup.propTypes = {
 }
 
 AxisSetup.defaultProps = {
-    classes: {},
     isOpen: false,
-    items: [],
+    initItems: [],
     onUpdateClick: Function.prototype,
     onCancelClick: Function.prototype,
-    dialogMaxWidth: 'md',
 }
 
 const mapStateToProps = state => ({
     isOpen: sGetUiActiveModalDialog(state) === AXIS_SETUP_DIALOG_ID,
-    items: sGetAxisSetupItems(state),
+    initItems: sGetAxisSetupItems(state),
     ui: sGetUi(state),
 })
 
@@ -244,7 +182,4 @@ const mapDispatchToProps = dispatch => ({
     onCancelClick: () => dispatch(acSetUiActiveModalDialog()),
 })
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(withStyles(styles)(AxisSetup))
+export default connect(mapStateToProps, mapDispatchToProps)(AxisSetup)
