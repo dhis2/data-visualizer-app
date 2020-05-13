32275d60427771a2a37f6ecc840ec7a9152ba27a
Jen Jones Arnesen <jennifer@dhis2.org>
2020-05-13 13:20:23 -0700

    fix: return actual translation strings in default metadata

--- packages/plugin/src/ChartPlugin.js
+++ packages/plugin/src/ChartPlugin.js
@@ -111,7 +111,6 @@ class ChartPlugin extends Component {
 
             const extraOptions = {
                 dashboard: forDashboard,
-                noData: { text: i18n.t('No data') }
             };
 
             let responses = [];
