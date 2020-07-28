57f5a27e1a61154d12d07453cb406a4635e82f43
Martin Ohlson <martin@moid.se>
2020-07-28 10:35:12 +0200

    fix: new base type for positive numbers + value is number (DHIS2-9194)

--- packages/app/src/components/VisualizationOptions/Options/RangeAxisSteps.js
+++ packages/app/src/components/VisualizationOptions/Options/RangeAxisSteps.js
@@ -2,11 +2,10 @@ import React from 'react'
 
 import i18n from '@dhis2/d2-i18n'
 
-import TextBaseOption from './TextBaseOption'
+import PositiveNumberBaseType from './PositiveNumberBaseType'
 
 export const RangeAxisSteps = () => (
-    <TextBaseOption
-        type="number"
+    <PositiveNumberBaseType
         width="96px"
         helpText={i18n.t(
             'The number of axis steps between the min and max values'
