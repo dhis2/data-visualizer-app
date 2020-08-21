66af040d4bf828b30022d97995ace9891d4c10bc
Martin Ohlson <martin@moid.se>
2020-08-21 10:21:28 +0200

    fix: change ui to ui-core for 34

--- packages/app/src/components/VisualizationOptions/Options/PositiveNumberBaseType.js
+++ packages/app/src/components/VisualizationOptions/Options/PositiveNumberBaseType.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { InputField } from '@dhis2/ui'
+import { InputField } from '@dhis2/ui-core'
 
 import { sGetUiOptions } from '../../../reducers/ui'
 import { acSetUiOptions } from '../../../actions/ui'
