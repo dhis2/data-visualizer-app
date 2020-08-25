b0996072769123a02249c63cb41cf347d90ad2f2
Martin <martin@moid.se>
2020-08-25 09:55:30 +0200

    fix: Decimals and Steps options only allow positive values (DHIS2-9002, DHIS2-9194) (#1161) v34 (#1210)

--- packages/app/src/components/VisualizationOptions/Options/RangeAxisSteps.js
+++ packages/app/src/components/VisualizationOptions/Options/RangeAxisSteps.js
@@ -2,14 +2,13 @@ import React from 'react'
 
 import i18n from '@dhis2/d2-i18n'
 
-import TextBaseOption from './TextBaseOption'
+import PositiveNumberBaseType from './PositiveNumberBaseType'
 
 export const RangeAxisSteps = () => (
-    <TextBaseOption
-        type="number"
+    <PositiveNumberBaseType
         width="96px"
         helpText={i18n.t(
-            'The number of axis steps between the min and max values'
+            'Number of axis tick steps, including the min and max. A value of 2 or lower will be ignored.'
         )}
         label={i18n.t('Steps')}
         placeholder={i18n.t('Auto')}
