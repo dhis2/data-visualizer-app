972a175d85c946cda5161152a7f988c3ac389e5a
Martin <martin@moid.se>
2020-08-25 09:55:30 +0200

    fix: cherry-pick b09960 + adapt code to v33

--- packages/app/src/components/VisualizationOptions/Options/RangeAxisSteps.js
+++ packages/app/src/components/VisualizationOptions/Options/RangeAxisSteps.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import TextBaseOption from './TextBaseOption';
 import i18n from '@dhis2/d2-i18n';
 import { sGetUiOptions } from '../../../reducers/ui';
 
+import PositiveNumberBaseType from './PositiveNumberBaseType';
+
 export const RangeAxisSteps = ({ showHelperText }) => (
-    <TextBaseOption
-        type="number"
+    <PositiveNumberBaseType
+        width="96px"
+        placeholder={i18n.t('Auto')}
         option={{
             name: 'rangeAxisSteps',
             label: i18n.t('Range axis tick steps'),
             helperText: showHelperText
                 ? i18n.t(
-                      `Tick steps may extend the chart beyond 'Range axis max'`
+                      'Number of axis tick steps, including the min and max. A value of 2 or lower will be ignored.'
                   )
                 : null,
         }}
