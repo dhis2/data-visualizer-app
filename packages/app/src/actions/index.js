a210fefd8829dca19d72e7e7ceacac6e56a7469f
Martin Ohlson <martin@moid.se>
2020-08-28 14:16:04 +0200

    fix: set a default name if saved vis is untitled

--- packages/app/src/actions/index.js
+++ packages/app/src/actions/index.js
@@ -31,6 +31,7 @@ import {
     GenericServerError,
     VisualizationNotFoundError,
 } from '../modules/error'
+import { getDisplayNameByVisType } from '@dhis2/analytics'
 
 export {
     fromVisualization,
@@ -187,9 +188,22 @@ export const tDoSaveVisualization = ({ name, description }, copy) => async (
         if (copy) {
             delete visualization.id
         }
-
         if (name) {
             visualization.name = name
+        } else {
+            visualization.name = i18n.t(
+                'Untitled {{visualizationType}} visualization, {{date}}',
+                {
+                    visualizationType: getDisplayNameByVisType(
+                        visualization.type
+                    ),
+                    date: new Date().toLocaleDateString(undefined, {
+                        year: 'numeric',
+                        month: 'short',
+                        day: '2-digit',
+                    }),
+                }
+            )
         }
 
         if (description) {
