d69161d5c36734738f599f8a152e3ae865713e51
Martin Ohlson <martin@moid.se>
2020-11-05 16:48:36 +0100

    chore: comment out WIP code

--- packages/app/cypress/utils/store.js
+++ packages/app/cypress/utils/store.js
@@ -7,8 +7,8 @@ export const expectStoreCurrentColumnsToHaveLength = length =>
         .its('columns')
         .should('have.length', length)
 
-export const expectStoreCurrentFilterDimensionToHaveItemsLength = (
-    filterDimension,
-    itemsLength
-) => cy.getReduxState('current').its('filters')
+// export const expectStoreCurrentFilterDimensionToHaveItemsLength = (
+//     filterDimension,
+//     itemsLength
+// ) => cy.getReduxState('current').its('filters')
 // FIXME: Implement filters.find(filter => filter.dimension === dimensionId).items.length === indicators.length)
