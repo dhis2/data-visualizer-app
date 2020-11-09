82d25ad7577c9803dfb3e3f2e07ef53095a73a0f
Martin Ohlson <martin@moid.se>
2020-11-09 12:43:05 +0100

    chore: update dimensions panel todo comment

--- packages/app/cypress/integration/dimensionsPanel.cy.js
+++ packages/app/cypress/integration/dimensionsPanel.cy.js
@@ -1,22 +1,4 @@
-/* 
-//  TODO:
-
-  ✓ Test that the following can be added by clicking on the item in the panel:
-      ✓ data item
-      ✓ period
-      ✓ org unit
-      ✓ assigned categories
-      ✓ dynamic dimension
-  ✓ Test that the "Filter dimensions" field works
-    Test drag-and-drop to the layout
-  ✓ Test the context menu: 
-      ✓ AC for Data
-      ✓ Add dimensions
-      ✓ Move dimensions
-      ✓ Remove dimensions
-  ✓ Check the green recommended dot
-
-*/
+//  TODO: Test drag-and-drop to the layout
 
 import {
     getFixedDimensions,
