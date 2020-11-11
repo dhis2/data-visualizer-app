4b4ebffca11fac878ef8c69b7d943a871a3de168
Edoardo Sabadelli <edoardo@dhis2.org>
2020-11-11 16:43:43 +0100

    refactor: convert to dataEngine

--- packages/app/src/api/settings.js
+++ packages/app/src/api/settings.js
@@ -2,13 +2,19 @@ import { onError } from './index'
 import { SYSTEM_SETTINGS } from '../modules/settings'
 
 const systemSettingsQuery = {
-    systemSettings: {
-        resource: 'systemSettings',
-        params: {
-            key: SYSTEM_SETTINGS,
-        },
+    resource: 'systemSettings',
+    params: {
+        key: SYSTEM_SETTINGS,
     },
 }
 
-export const apiFetchSystemSettings = dataEngine =>
-    dataEngine.query(systemSettingsQuery, { onError })
+export const apiFetchSystemSettings = async dataEngine => {
+    const systemSettingsData = await dataEngine.query(
+        { systemSettings: systemSettingsQuery },
+        {
+            onError,
+        }
+    )
+
+    return systemSettingsData.systemSettings
+}
