bbaf3e7143884e75d986f84a0d354d3dced48991
Martin Ohlson <martin@moid.se>
2020-11-17 16:07:33 +0100

    test: typo from prev commit

--- cypress/integration/layout.cy.js
+++ cypress/integration/layout.cy.js
@@ -31,11 +31,10 @@ import { goToStartPage } from '../elements/StartScreen'
 const TEST_FIXED_DIMS = Object.values(getFixedDimensions())
 const TEST_DYNAMIC_DIMS = Object.values(getDynamicDimensions())
 
-beforeEach(() => {
-    goToStartPage()
-})
-
 describe('interacting with the dimensions panel', () => {
+    before(() => {
+        goToStartPage()
+    })
     describe('opening items by clicking them', () => {
         TEST_FIXED_DIMS.forEach(dim => {
             it(`opens and closes ${dim.name}`, () => {
@@ -46,40 +45,44 @@ describe('interacting with the dimensions panel', () => {
             })
         })
     })
-})
-describe('moving items by using the context menu', () => {
-    const TEST_AXIS = AXIS_ID_FILTERS
-    const TEST_DIM = TEST_FIXED_DIMS.find(dim => dim.id === DIMENSION_ID_DATA)
-    it(`moves ${TEST_DIM.name} to ${TEST_AXIS}`, () => {
-        openContextMenu(TEST_DIM.id)
-        clickContextMenuMove(TEST_DIM.id, TEST_AXIS)
-        expectAxisToHaveDimension(TEST_AXIS, TEST_DIM.id)
+    describe('moving items by using the context menu', () => {
+        const TEST_AXIS = AXIS_ID_FILTERS
+        const TEST_DIM = TEST_FIXED_DIMS.find(
+            dim => dim.id === DIMENSION_ID_DATA
+        )
+        it(`moves ${TEST_DIM.name} to ${TEST_AXIS}`, () => {
+            openContextMenu(TEST_DIM.id)
+            clickContextMenuMove(TEST_DIM.id, TEST_AXIS)
+            expectAxisToHaveDimension(TEST_AXIS, TEST_DIM.id)
+        })
     })
-})
-describe('removing item by using the context menu', () => {
-    const TEST_DIM = TEST_FIXED_DIMS.find(dim => dim.id === DIMENSION_ID_PERIOD)
-    it(`removes ${TEST_DIM.name}`, () => {
-        openContextMenu(TEST_DIM.id)
-        clickContextMenuRemove(TEST_DIM.id)
-        expectLayoutToNotHaveDimension(TEST_DIM.id)
+    describe('removing item by using the context menu', () => {
+        const TEST_DIM = TEST_FIXED_DIMS.find(
+            dim => dim.id === DIMENSION_ID_PERIOD
+        )
+        it(`removes ${TEST_DIM.name}`, () => {
+            openContextMenu(TEST_DIM.id)
+            clickContextMenuRemove(TEST_DIM.id)
+            expectLayoutToNotHaveDimension(TEST_DIM.id)
+        })
     })
-})
-describe('handling AC by using the Data item context menu', () => {
-    const TEST_DIM = TEST_DYNAMIC_DIMS.find(
-        dim => dim.id === DIMENSION_ID_ASSIGNED_CATEGORIES
-    )
-    const TEST_AXIS = AXIS_ID_COLUMNS
-    it(`adds and removes ${TEST_DIM.name}`, () => {
-        // add
-        openContextMenu(DIMENSION_ID_DATA)
-        clickContextMenuDimSubMenu(TEST_DIM.id)
-        clickContextMenuAdd(TEST_DIM.id, TEST_AXIS)
-        expectDimensionToHaveSelectedStyle(TEST_DIM.id)
-        expectAxisToHaveDimension(TEST_AXIS, TEST_DIM.id)
+    describe('handling AC by using the Data item context menu', () => {
+        const TEST_DIM = TEST_DYNAMIC_DIMS.find(
+            dim => dim.id === DIMENSION_ID_ASSIGNED_CATEGORIES
+        )
+        const TEST_AXIS = AXIS_ID_COLUMNS
+        it(`adds and removes ${TEST_DIM.name}`, () => {
+            // add
+            openContextMenu(DIMENSION_ID_DATA)
+            clickContextMenuDimSubMenu(TEST_DIM.id)
+            clickContextMenuAdd(TEST_DIM.id, TEST_AXIS)
+            expectDimensionToHaveSelectedStyle(TEST_DIM.id)
+            expectAxisToHaveDimension(TEST_AXIS, TEST_DIM.id)
 
-        // remove
-        openContextMenu(DIMENSION_ID_DATA)
-        clickContextMenuRemove(TEST_DIM.id)
-        expectAxisToNotHaveDimension(TEST_AXIS, TEST_DIM.id)
+            // remove
+            openContextMenu(DIMENSION_ID_DATA)
+            clickContextMenuRemove(TEST_DIM.id)
+            expectAxisToNotHaveDimension(TEST_AXIS, TEST_DIM.id)
+        })
     })
 })
