f9da56bc606236f119589f8a07c241eada60ecb5
Martin Ohlson <martin@moid.se>
2020-11-18 12:16:12 +0100

    chore: remove WIP code from commit

--- cypress/support/server/loginAndPersistSession.js
+++ cypress/support/server/loginAndPersistSession.js
@@ -14,19 +14,6 @@ export default function loginAndPersistSession() {
     })
 
     before(() => {
-        const url = Cypress.env('dhis2_base_url')
-        const user = Cypress.env('dhis2_base_username')
-        const pass = Cypress.env('dhis2_base_password')
-        const withprefix = Cypress.env('withprefix')
-        const withoutprefix = Cypress.env('withoutprefix')
-        const urlcaps = Cypress.env('DHIS2_BASE_URL')
-
-        cy.log('url', url)
-        cy.log('user', user)
-        cy.log('pass', pass)
-        cy.log('withprefix', withprefix)
-        cy.log('withoutprefix', withoutprefix)
-        cy.log('urlcaps', urlcaps)
         cy.login()
     })
 }
