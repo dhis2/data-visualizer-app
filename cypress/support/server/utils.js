25e36b7492201acf73c506542879c3f435122747
Martin Ohlson <martin@moid.se>
2020-11-19 13:42:29 +0100

    chore: remove test stub code

--- cypress/support/server/utils.js
+++ cypress/support/server/utils.js
@@ -1,9 +1,3 @@
-import {
-    API_STUB_MODES,
-    DEFAULT_API_STUB_MODE,
-    NETWORK_FIXTURES_FILE_PATH,
-} from './constants.js'
-
 export const getApiBaseUrl = () => {
     const baseUrl = Cypress.env('dhis2_base_url') || ''
 
@@ -15,17 +9,3 @@ export const getApiBaseUrl = () => {
 
     return baseUrl
 }
-
-export const getDefaultMode = () =>
-    Cypress.env('dhis2_api_stub_mode') || DEFAULT_API_STUB_MODE
-
-export const isDisabledMode = () =>
-    Cypress.env('dhis2_api_stub_mode') === API_STUB_MODES.DISABLED
-
-export const isCaptureMode = () =>
-    Cypress.env('dhis2_api_stub_mode') === API_STUB_MODES.CAPTURE
-
-export const isStubMode = () =>
-    Cypress.env('dhis2_api_stub_mode') === API_STUB_MODES.STUB
-
-export const getFileName = () => NETWORK_FIXTURES_FILE_PATH
