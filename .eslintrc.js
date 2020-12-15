d9ba168d64a9976caf6e8d223c377044335cbd6f
Edoardo Sabadelli <edoardo@dhis2.org>
2020-12-15 14:42:48 +0100

    chore: generated configs after cli-style update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 const { config } = require('@dhis2/cli-style')
 
 module.exports = {
-    extends: [config.eslintReact, 'plugin:cypress/recommended'],
-    plugins: ['react-hooks', 'cypress'],
-    rules: {
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
-    },
+    extends: [config.eslintReact],
 }
