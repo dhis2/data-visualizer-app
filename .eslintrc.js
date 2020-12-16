ed92bb35cda086c9110b2eb23c7a98f51e8b233c
Edoardo Sabadelli <edoardo@dhis2.org>
2020-12-16 11:06:05 +0100

    chore: restore custom rules section in .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 const { config } = require('@dhis2/cli-style')
 
 module.exports = {
-    extends: [config.eslintReact],
+    extends: [config.eslintReact, 'plugin:cypress/recommended'],
+    plugins: ['react-hooks', 'cypress'],
+    rules: {
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+    },
 }
