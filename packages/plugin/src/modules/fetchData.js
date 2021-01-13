9a2c54a6be238e5b89c56315ba77f9356de538ec
Edoardo Sabadelli <edoardo@dhis2.org>
2021-01-13 09:49:41 +0100

    fix: pass a periodId/xAxisIndex matrix in extraOptions

--- packages/plugin/src/modules/fetchData.js
+++ packages/plugin/src/modules/fetchData.js
@@ -1,9 +1,20 @@
-import { isYearOverYear } from '@dhis2/analytics'
+import {
+    isYearOverYear,
+    getRelativePeriodsOptionsById,
+    WEEKS,
+    DAYS,
+    DIMENSION_ID_PERIOD,
+    layoutGetDimensionItems,
+} from '@dhis2/analytics'
 import {
     apiFetchAnalyticsForYearOverYear,
     apiFetchAnalytics,
 } from '../api/analytics'
-import { computeGenericPeriodNames } from './analytics'
+import {
+    computeGenericPeriodNames,
+    computeYoYMatrix,
+    computeGenericPeriodNamesFromMatrix,
+} from './analytics'
 import { getRequestOptions } from './getRequestOptions'
 
 export const fetchData = async ({
@@ -28,12 +39,48 @@ export const fetchData = async ({
             options
         )
 
+        const peItems = layoutGetDimensionItems(
+            visualization,
+            DIMENSION_ID_PERIOD
+        )
+        const usesRelativeWeeksPeriod = getRelativePeriodsOptionsById(WEEKS)
+            .getPeriods()
+            .find(p => p.id === peItems[0].id)
+        const usesRelativeDaysPeriod = getRelativePeriodsOptionsById(DAYS)
+            .getPeriods()
+            .find(p => p.id === peItems[0].id)
+
+        const periodKeyAxisIndexMatrix = computeYoYMatrix(
+            responses,
+            usesRelativeWeeksPeriod,
+            usesRelativeDaysPeriod
+        )
+        const periodKeyAxisIndexMap = periodKeyAxisIndexMatrix.reduce(
+            (map, periodKeys, index) => {
+                periodKeys.forEach(periodKey => (map[periodKey] = index))
+
+                return map
+            },
+            {}
+        )
+        console.log('periodKeyAxisIndexMap', periodKeyAxisIndexMap)
+
+        const xAxisLabels =
+            usesRelativeWeeksPeriod || usesRelativeDaysPeriod
+                ? computeGenericPeriodNamesFromMatrix(
+                      periodKeyAxisIndexMatrix,
+                      usesRelativeWeeksPeriod,
+                      usesRelativeDaysPeriod
+                  )
+                : computeGenericPeriodNames(responses)
+
         return {
             responses,
             extraOptions: {
                 ...extraOptions,
                 yearlySeries: yearlySeriesLabels,
-                xAxisLabels: computeGenericPeriodNames(responses),
+                xAxisLabels,
+                periodKeyAxisIndexMap,
             },
         }
     }
