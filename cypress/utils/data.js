9509c10ec40b45353cdc178a751e2ad66922cba6
Martin Ohlson <martin@moid.se>
2021-02-22 10:03:05 +0100

    test: update test data to use correct dash

--- cypress/utils/data.js
+++ cypress/utils/data.js
@@ -80,21 +80,21 @@ export const TEST_INDICATORS = [
 export const TEST_DATA_SETS = [
     {
         id: 'VTdjfLXXmoi.REPORTING_RATE',
-        name: 'Clinical Monitoring Checklist – Reporting rate',
+        name: 'Clinical Monitoring Checklist - Reporting rate',
     },
     {
         id: 'TuL8IOPzpHh.EXPECTED_REPORTS',
-        name: 'EPI Stock – Expected reports',
+        name: 'EPI Stock - Expected reports',
     },
     {
         id: 'V8MHeZHIrcP.REPORTING_RATE_ON_TIME',
-        name: 'Facility Assessment – Reporting rate on time',
+        name: 'Facility Assessment - Reporting rate on time',
     },
     {
         id: 'VTdjfLXXmoi.ACTUAL_REPORTS',
-        name: 'Clinical Monitoring Checklist – Actual reports',
+        name: 'Clinical Monitoring Checklist - Actual reports',
     },
-    { id: 'rsyjyJmYD4J.REPORTING_RATE', name: 'Expenditures – Reporting rate' },
+    { id: 'rsyjyJmYD4J.REPORTING_RATE', name: 'Expenditures - Reporting rate' },
 ]
 
 export const TEST_CUSTOM_DIMENSIONS = [
