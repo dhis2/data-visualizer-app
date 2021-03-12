75d77548e5d44728f778a15cd6b79c5555d40718
Martin Ohlson <martin@moid.se>
2021-03-12 09:28:04 +0100

    fix: rename const

--- packages/app/src/api/dataStatistics.js
+++ packages/app/src/api/dataStatistics.js
@@ -1,12 +1,12 @@
 import { onError } from './index'
 
-export const EVENT_TYPE_CHART_VIEW = 'VISUALIZATION_VIEW'
+export const EVENT_TYPE = 'VISUALIZATION_VIEW'
 
 const dataStatisticMutation = {
     resource: 'dataStatistics',
     params: ({ id }) => ({
         favorite: id,
-        eventType: EVENT_TYPE_CHART_VIEW,
+        eventType: EVENT_TYPE,
     }),
     type: 'create',
 }
--- packages/app/src/api/mostViewedVisualizations.js
+++ packages/app/src/api/mostViewedVisualizations.js
@@ -1,13 +1,13 @@
 // TODO move into component StartScreen?
 
-import { EVENT_TYPE_CHART_VIEW } from './dataStatistics'
+import { EVENT_TYPE } from './dataStatistics'
 
 // most likely is not needed anywhere else
 export const apiFetchMostViewedVisualizations = (dataEngine, pageSize) => {
     const visualizationQuery = {
         resource: 'dataStatistics/favorites',
         params: {
-            eventType: EVENT_TYPE_CHART_VIEW,
+            eventType: EVENT_TYPE,
             pageSize: pageSize || 10,
         },
     }
