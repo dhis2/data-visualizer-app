46c67db49b2fa7bee25d19c6319c9988845fbb03
Martin Ohlson <martin@moid.se>
2021-04-20 16:03:42 +0200

    feat: first draft for the vertical tabs

--- packages/app/src/modules/options/sections/templates/verticalAxis.js
+++ packages/app/src/modules/options/sections/templates/verticalAxis.js
@@ -1,7 +1,7 @@
 import i18n from '@dhis2/d2-i18n'
 
 export default ({ content, helpText, axisId }) => ({
-    key: `axes-vertical-axis-${axisId}`,
+    key: axisId ? `axes-vertical-axis-${axisId}` : 'axes-vertical-axis',
     label: axisId
         ? i18n.t('Vertical (y) axis {{axisId}}', {
               axisId: Number(axisId.slice(-1)) + 1,
--- packages/app/src/modules/options/tabs/axes.js
+++ packages/app/src/modules/options/tabs/axes.js
@@ -1,7 +1,53 @@
+import React, { useState } from 'react'
 import i18n from '@dhis2/d2-i18n'
+import PropTypes from 'prop-types'
+
+import { tabSectionTitle } from '../../../components/VisualizationOptions/styles/VisualizationOptions.style'
+import VerticalTabBar from '../../../components/VerticalTabBar/VerticalTabBar'
+import VerticalTab from '../../../components/VerticalTabBar/VerticalTab'
 
 export default content => ({
     key: 'axes-tab',
     label: i18n.t('Axes'),
-    content,
+    content: [
+        {
+            key: 'axis-tabs',
+            content: React.Children.toArray([
+                <AxisTabs items={content} key="tabs" />,
+            ]),
+        },
+    ],
 })
+
+const AxisTabs = ({ items }) => {
+    const [selectedTabIndex, setSelectedTabIndex] = useState(0)
+
+    return (
+        <div style={{ display: 'flex' }}>
+            <div style={{ width: '200px' }}>
+                <VerticalTabBar>
+                    {items.map(({ label }, index) => (
+                        <VerticalTab
+                            key={index}
+                            onClick={() => setSelectedTabIndex(index)}
+                            selected={index === selectedTabIndex}
+                        >
+                            {label}
+                        </VerticalTab>
+                    ))}
+                </VerticalTabBar>
+            </div>
+            <div>
+                <span className={tabSectionTitle.className}>
+                    {items[selectedTabIndex].label}
+                </span>
+                {items[selectedTabIndex].content}
+            </div>
+            {tabSectionTitle.styles}
+        </div>
+    )
+}
+
+AxisTabs.propTypes = {
+    items: PropTypes.array,
+}
