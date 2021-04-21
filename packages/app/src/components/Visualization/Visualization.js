3b7dec84f783cc6b305b0c48489412ee8bf35218
Martin Ohlson <martin@moid.se>
2021-04-21 13:22:53 +0200

    fix: add a custom error for no org units

--- packages/app/src/components/Visualization/Visualization.js
+++ packages/app/src/components/Visualization/Visualization.js
@@ -27,6 +27,7 @@ import {
     MultipleIndicatorAsFilterError,
     NoDataOrDataElementGroupSetError,
     CombinationDEGSRRError,
+    NoOrgUnitResponseError,
 } from '../../modules/error'
 import LoadingMask from '../../widgets/LoadingMask'
 import { sGetSettingsDisplayNameProperty } from '../../reducers/settings'
@@ -61,6 +62,10 @@ export class Visualization extends Component {
                 case 'E7112':
                     error = new CombinationDEGSRRError()
                     break
+                case 'E7124':
+                    error = new NoOrgUnitResponseError()
+                    break
+
                 default:
                     error = response
             }
