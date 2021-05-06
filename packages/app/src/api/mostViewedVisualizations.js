80355803b67da81465e2157bb5d9682154bb1e70
Martin <martin@moid.se>
2021-05-06 11:24:12 +0200

    fix: start screen most viewed updates v36 (DHIS2-11065) (#1712)
    * fix: pass username to the favorites endpoint (#1711)
    * fix: start screen vis type crash (#1709)

--- packages/app/src/api/mostViewedVisualizations.js
+++ packages/app/src/api/mostViewedVisualizations.js
@@ -1,10 +1,15 @@
-export const apiFetchMostViewedVisualizations = (engine, pageSize) => {
+export const apiFetchMostViewedVisualizations = (
+    engine,
+    pageSize,
+    username
+) => {
     const visualizationQuery = {
         visualization: {
             resource: 'dataStatistics/favorites',
             params: {
                 eventType: 'VISUALIZATION_VIEW',
                 pageSize: pageSize || 10,
+                ...(username ? { username } : {}),
             },
         },
     }
