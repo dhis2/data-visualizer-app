0c5d40418d021e81a43a146716e437a26b0f663d
Jen Jones Arnesen <jennifer@dhis2.org>
2021-07-08 08:16:57 +0200

    fix: enable save test with scatter

--- cypress/utils/random.js
+++ cypress/utils/random.js
@@ -1,4 +1,4 @@
-import { visTypeDisplayNames, VIS_TYPE_SCATTER } from '@dhis2/analytics'
+import { visTypeDisplayNames } from '@dhis2/analytics'
 
 export const generateRandomChar = () =>
     Math.random()
@@ -12,12 +12,7 @@ export const generateRandomNumber = (min, max) =>
 export const getRandomArrayItem = array =>
     array[generateRandomNumber(0, array.length - 1)]
 
-export const getRandomVisType = (includeScatter = true) => {
-    const names = Object.assign({}, visTypeDisplayNames)
-    if (!includeScatter) {
-        delete names[VIS_TYPE_SCATTER]
-    }
-    return getRandomArrayItem(Object.keys(names))
-}
+export const getRandomVisType = () =>
+    getRandomArrayItem(Object.keys(visTypeDisplayNames))
 
 export const generateRandomBool = () => Math.random() < 0.5
