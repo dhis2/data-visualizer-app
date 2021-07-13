c09b37bb3d45d31f64561da5c26a857164fc39b3
Edoardo Sabadelli <edoardo@dhis2.org>
2021-07-13 15:11:15 +0200

    feat: add components for fixed row and fixed column header options

---
+++ packages/app/src/components/VisualizationOptions/Options/FixedColumnHeaders.js
@@ -0,0 +1,14 @@
+import i18n from '@dhis2/d2-i18n'
+import React from 'react'
+import CheckboxBaseOption from './CheckboxBaseOption'
+
+const FixedColumnHeaders = () => (
+    <CheckboxBaseOption
+        label={i18n.t('Fixed column headers to top of table')}
+        option={{
+            name: 'fixedColumnHeaders',
+        }}
+    />
+)
+
+export default FixedColumnHeaders
