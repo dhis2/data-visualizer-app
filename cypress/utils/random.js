6eb5d2b5f9a51dde6d2103412f2f1ef62c0a0711
Edoardo Sabadelli <edoardo@dhis2.org>
2022-01-14 11:59:07 +0100

    test: avoid import from app, defined directly the list instead

--- cypress/utils/random.js
+++ cypress/utils/random.js
@@ -1,4 +1,38 @@
-import { visTypeDescriptions } from '../../packages/app/src/modules/visualization.js'
+import {
+    VIS_TYPE_PIVOT_TABLE,
+    VIS_TYPE_COLUMN,
+    VIS_TYPE_STACKED_COLUMN,
+    VIS_TYPE_BAR,
+    VIS_TYPE_STACKED_BAR,
+    VIS_TYPE_LINE,
+    VIS_TYPE_AREA,
+    VIS_TYPE_STACKED_AREA,
+    VIS_TYPE_PIE,
+    VIS_TYPE_RADAR,
+    VIS_TYPE_GAUGE,
+    VIS_TYPE_YEAR_OVER_YEAR_LINE,
+    VIS_TYPE_YEAR_OVER_YEAR_COLUMN,
+    VIS_TYPE_SINGLE_VALUE,
+    VIS_TYPE_SCATTER,
+} from '@dhis2/analytics'
+
+const visTypes = [
+    VIS_TYPE_PIVOT_TABLE,
+    VIS_TYPE_COLUMN,
+    VIS_TYPE_STACKED_COLUMN,
+    VIS_TYPE_BAR,
+    VIS_TYPE_STACKED_BAR,
+    VIS_TYPE_LINE,
+    VIS_TYPE_AREA,
+    VIS_TYPE_STACKED_AREA,
+    VIS_TYPE_PIE,
+    VIS_TYPE_RADAR,
+    VIS_TYPE_GAUGE,
+    VIS_TYPE_YEAR_OVER_YEAR_LINE,
+    VIS_TYPE_YEAR_OVER_YEAR_COLUMN,
+    VIS_TYPE_SINGLE_VALUE,
+    VIS_TYPE_SCATTER,
+]
 
 export const generateRandomChar = () =>
     Math.random()
@@ -12,7 +46,6 @@ export const generateRandomNumber = (min, max) =>
 export const getRandomArrayItem = (array) =>
     array[generateRandomNumber(0, array.length - 1)]
 
-export const getRandomVisType = () =>
-    getRandomArrayItem(Object.keys(visTypeDescriptions))
+export const getRandomVisType = () => getRandomArrayItem(visTypes)
 
 export const generateRandomBool = () => Math.random() < 0.5
