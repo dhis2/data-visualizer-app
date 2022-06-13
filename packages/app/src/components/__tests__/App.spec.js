1eb365689c384061122aac140e392c9c2a7710a1
Edoardo Sabadelli <edoardo@dhis2.org>
2022-06-13 09:10:57 +0200

    fix: update tests

--- packages/app/src/components/__tests__/App.spec.js
+++ packages/app/src/components/__tests__/App.spec.js
@@ -10,6 +10,12 @@ import { DEFAULT_CURRENT } from '../../reducers/current.js'
 import { UnconnectedApp as App } from '../App.js'
 import { Snackbar } from '../Snackbar/Snackbar.js'
 
+jest.mock('@dhis2/analytics', () => ({
+    apiFetchOrganisationUnitLevels: jest.fn(),
+    getPredefinedDimensions: () => {},
+    visTypeDisplayNames: {},
+}))
+
 jest.mock(
     '../Visualization/Visualization',
     () =>
