ce885bec09e2ffcfdb3502ed0349d7a64b19e220
Edoardo Sabadelli <edoardo@dhis2.org>
2022-07-21 12:30:55 +0200

    refactor: add new module files for settings

---
+++ src/modules/userSettings.js
@@ -0,0 +1,10 @@
+export const USER_SETTINGS_UI_LOCALE = 'keyUiLocale'
+export const USER_SETTINGS_DISPLAY_PROPERTY = 'keyAnalysisDisplayProperty'
+
+export const DERIVED_USER_SETTINGS_DISPLAY_NAME_PROPERTY =
+    'DERIVED_USER_SETTINGS_DISPLAY_NAME_PROPERTY'
+
+export const userSettingsKeys = [
+    USER_SETTINGS_UI_LOCALE,
+    USER_SETTINGS_DISPLAY_PROPERTY,
+]
