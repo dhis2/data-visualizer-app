927e99b9d708f5a342cc9f79f48da8fc56a13536
Edoardo Sabadelli <edoardo@dhis2.org>
2023-02-17 14:07:03 +0100

    feat: implement data icon option for SV visualization DHIS2-10496

---
+++ src/components/VisualizationOptions/Options/DataIcon.js
@@ -0,0 +1,17 @@
+import i18n from '@dhis2/d2-i18n'
+import React from 'react'
+import { CheckboxBaseOption } from './CheckboxBaseOption.js'
+
+const DataIcon = () => (
+    <CheckboxBaseOption
+        label={i18n.t('Show data item icon')}
+        helpText={i18n.t(
+            'If the data item has an icon, display it next to the value'
+        )}
+        option={{
+            name: 'icons',
+        }}
+    />
+)
+
+export default DataIcon
