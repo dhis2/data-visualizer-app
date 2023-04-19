bf6d5bf388d7ff462dac421f6a15b1ddd9d83f53
Jen Jones Arnesen <jennifer@dhis2.org>
2023-04-19 16:46:13 +0200

    fix: confirm item got selected

--- cypress/elements/dimensionModal/dataDimension.js
+++ cypress/elements/dimensionModal/dataDimension.js
@@ -3,6 +3,7 @@ import { clearInput, typeInput } from '../common.js'
 import {
     expectDimensionModalToBeVisible,
     expectSourceToNotBeLoading,
+    expectItemToBeSelected,
     selectItemByDoubleClick,
 } from './index.js'
 
@@ -60,6 +61,7 @@ export const selectIndicators = (indicators) => {
     indicators.forEach((item) => {
         cy.get('input[placeholder*="Search by data item name"]').type(item)
         selectItemByDoubleClick(item)
+        expectItemToBeSelected(item)
         cy.get('input[placeholder*="Search by data item name"]').clear()
     })
 }
