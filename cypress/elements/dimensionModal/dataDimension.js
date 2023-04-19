c6a885fa1b20b1a4e26030685d6ca5e8757f1516
Jen Jones Arnesen <jennifer@dhis2.org>
2023-04-19 15:36:06 +0200

    fix: clear the typed field after selecting the item 2

--- cypress/elements/dimensionModal/dataDimension.js
+++ cypress/elements/dimensionModal/dataDimension.js
@@ -60,7 +60,7 @@ export const selectIndicators = (indicators) => {
     indicators.forEach((item) => {
         cy.get('input[placeholder*="Search by data item name"]').type(item)
         selectItemByDoubleClick(item)
-        cy.get('input[placeholder*="Search by data item name"]').type('')
+        cy.get('input[placeholder*="Search by data item name"]').clear()
     })
 }
 
