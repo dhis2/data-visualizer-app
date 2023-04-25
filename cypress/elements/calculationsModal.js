f1c4600a976851dc11cd3f03986c234a3ef2811d
martinkrulltott <martin@moid.se>
2023-04-25 16:15:37 +0200

    test: save, load, delete an EDI

--- cypress/elements/calculationsModal.js
+++ cypress/elements/calculationsModal.js
@@ -1,3 +1,6 @@
+import { typeInput } from './common.js'
+
+const calculationModalEl = 'calculation-modal'
 const modalTitleEl = 'calculation-modal-title'
 
 export const clickNewCalculationButton = () =>
@@ -7,7 +10,55 @@ export const clickNewCalculationButton = () =>
         .click()
 
 export const expectCalculationsModalToBeVisible = () =>
-    cy.getBySel('calculation-modal').should('be.visible')
+    cy.getBySel(calculationModalEl).should('be.visible')
 
 export const expectCalculationsModalTitleToContain = (text) =>
     cy.getBySel(modalTitleEl).should('contain', text)
+
+export const expectDimensionsListToHaveLength = (length) => {
+    cy.getBySelLike('dimension-list')
+        .findBySelLike('data-element-option')
+        .should('have.length', length)
+}
+
+export const expectFormulaFieldToContainItem = (item) =>
+    cy
+        .getBySel('formula-field')
+        .findBySelLike('formula-item-EXPRESSION_TYPE_DATA')
+        .contains(item)
+        .should('exist')
+
+export const selectItemFromDimensionsListByDoubleClick = (item) => {
+    cy.getBySelLike('dimension-list')
+        .findBySelLike('data-element-option')
+        .should('have.length.least', 1)
+        .contains(item)
+        .dblclick()
+}
+
+export const inputCalculationLabel = (label) =>
+    typeInput('calculation-label', label)
+
+export const clickSaveButton = () =>
+    cy
+        .getBySel(calculationModalEl)
+        .find('button')
+        .contains('Save calculation')
+        .click()
+
+export const clickCancelButton = () =>
+    cy.getBySel(calculationModalEl).find('button').contains('Cancel').click()
+
+export const clickDeleteButton = () =>
+    cy
+        .getBySel(calculationModalEl)
+        .find('button')
+        .contains('Delete calculation')
+        .click()
+
+export const clickConfirmDeleteButton = () =>
+    cy
+        .getBySel('calculation-delete-modal')
+        .find('button')
+        .contains('Yes, delete')
+        .click()
--- cypress/integration/dimensions/calculations.cy.js
+++ cypress/integration/dimensions/calculations.cy.js
@@ -1,17 +1,34 @@
-import { DIMENSION_ID_DATA } from '@dhis2/analytics'
+import { DIMENSION_ID_DATA, VIS_TYPE_COLUMN } from '@dhis2/analytics'
 import {
+    clickCancelButton,
+    clickConfirmDeleteButton,
+    clickDeleteButton,
     clickNewCalculationButton,
+    clickSaveButton,
     expectCalculationsModalTitleToContain,
     expectCalculationsModalToBeVisible,
+    expectDimensionsListToHaveLength,
+    expectFormulaFieldToContainItem,
+    inputCalculationLabel,
+    selectItemFromDimensionsListByDoubleClick,
 } from '../../elements/calculationsModal.js'
-import { expectDataDimensionModalToBeVisible } from '../../elements/dimensionModal/index.js'
+import { expectVisualizationToBeVisible } from '../../elements/chart.js'
+import { expectAppToNotBeLoading } from '../../elements/common.js'
+import {
+    clickDimensionModalUpdateButton,
+    clickEDIEditButton,
+    expectDataDimensionModalToBeVisible,
+    expectItemToBeSelected,
+    expectNoDataItemsToBeSelected,
+} from '../../elements/dimensionModal/index.js'
 import { openDimension } from '../../elements/dimensionsPanel.js'
+import { saveNewAO } from '../../elements/fileMenu/save.js'
 import { goToStartPage } from '../../elements/startScreen.js'
 
 const PAGE_SIZE = 50
 const DATA_ELEMENTS_URL = '**/dataElements?*'
 
-describe('Data dimension', () => {
+describe('Calculations', () => {
     beforeEach(() => {
         goToStartPage()
     })
@@ -36,11 +53,45 @@ describe('Data dimension', () => {
             'Totals only'
         )
 
-        cy.getBySelLike('dimension-list')
-            .children()
-            .should('have.length', PAGE_SIZE + 1)
+        expectDimensionsListToHaveLength(PAGE_SIZE)
     })
+    it('can save, load and delete', () => {
+        const TEST_DATA_ELEMENT = 'ANC 2nd visit'
+        const TEST_LABEL = `EDI ${new Date().toLocaleString()}`
+        const TEST_AO_NAME = `TEST with EDI ${new Date().toLocaleString()}`
 
+        // create
+        openDimension(DIMENSION_ID_DATA)
+        clickNewCalculationButton()
+        selectItemFromDimensionsListByDoubleClick(TEST_DATA_ELEMENT)
+        expectFormulaFieldToContainItem(TEST_DATA_ELEMENT)
+        inputCalculationLabel(TEST_LABEL)
+        clickSaveButton()
+        expectItemToBeSelected(TEST_LABEL)
+        clickEDIEditButton(TEST_LABEL)
+        expectFormulaFieldToContainItem(TEST_DATA_ELEMENT)
+        clickCancelButton()
+        clickDimensionModalUpdateButton()
+        expectVisualizationToBeVisible(VIS_TYPE_COLUMN)
+        cy.getBySel('layout-chip-dx').trigger('mouseover')
+        cy.getBySelLike('tooltip-content').contains(TEST_LABEL)
+
+        // save
+        saveNewAO(TEST_AO_NAME)
+        expectAppToNotBeLoading()
+        expectVisualizationToBeVisible(VIS_TYPE_COLUMN)
+
+        // load
+        openDimension(DIMENSION_ID_DATA)
+        expectItemToBeSelected(TEST_LABEL)
+        clickEDIEditButton(TEST_LABEL)
+        expectFormulaFieldToContainItem(TEST_DATA_ELEMENT)
+
+        // delete
+        clickDeleteButton()
+        clickConfirmDeleteButton()
+        expectNoDataItemsToBeSelected()
+    })
     /*
 
         --search/filter
@@ -73,21 +124,18 @@ describe('Data dimension', () => {
         EDI can't be saved without a valid formula (check with Joe if this is still valid)
 
         --saving a formula
-        enter a label
-        create a valid formula
-        validate the formula
-        save
-        EDI displays correctly in the visualization
+        -enter a label
+        -create a valid formula
+        -validate the formula
+        -save
+        -EDI displays correctly in the visualization
 
         --opening a saved formula
         unselected formula is listed under "Data Type: Calculations"
-            tooltip shows on hover
             has label and formula when opened
         selected formula is selected and not listed under "Data Type: Calculations"
-            tooltip shows on hover
             has label and formula when opened
         editing and saving works
-            edited formula shows in tooltip on hover
             edited name shows in list of data items
             edited EDI displays correctly in visualization
 
