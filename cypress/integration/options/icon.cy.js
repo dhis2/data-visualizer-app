ca6865e033003332a5ee0b09c0e3ccd9fdaf8750
martinkrulltott <martin@moid.se>
2023-06-02 16:37:55 +0200

    test: draft for sv icon and legend (untested locally)

--- cypress/integration/options/icon.cy.js
+++ cypress/integration/options/icon.cy.js
@@ -1,5 +1,6 @@
 import {
     DIMENSION_ID_DATA,
+    DIMENSION_ID_PERIOD,
     DIMENSION_TYPE_DATA_ELEMENT,
     DIMENSION_TYPE_DATA_SET,
     DIMENSION_TYPE_EVENT_DATA_ITEM,
@@ -15,18 +16,30 @@ import {
     switchSubGroupTo,
 } from '../../elements/dimensionModal/dataDimension.js'
 import {
+    clickDimensionModalHideButton,
     clickDimensionModalUpdateButton,
     expectDataDimensionModalToBeVisible,
     inputSearchTerm,
     selectItemByDoubleClick,
+    selectRelativePeriods,
+    unselectAllItemsByButton,
 } from '../../elements/dimensionModal/index.js'
 import { openDimension } from '../../elements/dimensionsPanel.js'
 import { clickMenuBarOptionsButton } from '../../elements/menuBar.js'
 import {
+    OPTIONS_TAB_LEGEND,
     OPTIONS_TAB_STYLE,
     clickOptionsModalHideButton,
+    clickOptionsModalUpdateButton,
     clickOptionsTab,
 } from '../../elements/optionsModal/index.js'
+import {
+    changeDisplayStrategyToFixed,
+    changeDisplayStyleToText,
+    changeFixedLegendSet,
+    expectSingleValueToHaveIconColor,
+    toggleLegend,
+} from '../../elements/optionsModal/legend.js'
 import { goToStartPage } from '../../elements/startScreen.js'
 import { changeVisType } from '../../elements/visualizationTypeSelector.js'
 
@@ -74,7 +87,7 @@ describe('Icon', () => {
         expectIconToBeHidden()
     })
     TEST_TYPES.forEach((type) => {
-        it('icon shows when option is enabled', () => {
+        it(`icon shows when option is enabled for ${type}`, () => {
             // enable the icon
             clickMenuBarOptionsButton()
             clickOptionsTab(OPTIONS_TAB_STYLE)
@@ -103,24 +116,59 @@ describe('Icon', () => {
             expectIconToBeVisible()
         })
     })
+    it('icon gets correct color when a legend is in use', () => {
+        // enable the icon
+        clickMenuBarOptionsButton()
+        clickOptionsTab(OPTIONS_TAB_STYLE)
+        clickCheckbox('option-show-data-item-icon')
+
+        // enable the legend
+        clickOptionsTab(OPTIONS_TAB_LEGEND)
+        toggleLegend()
+        changeDisplayStrategyToFixed()
+        changeFixedLegendSet('E2E legend')
+        clickOptionsModalHideButton()
+
+        // select a period
+        openDimension(DIMENSION_ID_PERIOD)
+        unselectAllItemsByButton()
+        selectRelativePeriods(['This year'], 'Years')
+        clickDimensionModalHideButton()
+
+        // select a data item
+        openDimension(DIMENSION_ID_DATA)
+        expectDataDimensionModalToBeVisible()
+        inputSearchTerm(TEST_ITEMS[DIMENSION_TYPE_INDICATOR])
+        selectItemByDoubleClick(TEST_ITEMS[DIMENSION_TYPE_INDICATOR])
+        clickDimensionModalUpdateButton()
+
+        // default text color is applied to icon
+        expectVisualizationToBeVisible(VIS_TYPE_SINGLE_VALUE)
+        expectIconToBeVisible()
+        expectSingleValueToHaveIconColor('#212934')
+
+        // switch to use a data item that will trigger the contrast color
+        openDimension(DIMENSION_ID_DATA)
+        expectDataDimensionModalToBeVisible()
+        unselectAllItemsByButton()
+        inputSearchTerm(TEST_ITEMS[DIMENSION_TYPE_DATA_ELEMENT])
+        selectItemByDoubleClick(TEST_ITEMS[DIMENSION_TYPE_DATA_ELEMENT])
+        clickDimensionModalUpdateButton()
+
+        // contrast color is applied to icon
+        expectVisualizationToBeVisible(VIS_TYPE_SINGLE_VALUE)
+        expectIconToBeVisible()
+        expectSingleValueToHaveIconColor('#2166ac')
+
+        // switch to apply legend to text color
+        clickMenuBarOptionsButton()
+        clickOptionsTab(OPTIONS_TAB_LEGEND)
+        changeDisplayStyleToText()
+        clickOptionsModalUpdateButton()
+
+        // legend color is applied to icon
+        expectVisualizationToBeVisible(VIS_TYPE_SINGLE_VALUE)
+        expectIconToBeVisible()
+        expectSingleValueToHaveIconColor('#ffffff')
+    })
 })
-
-// TODO:
-
-// icon color matches value color when a legend is set, i.e.
-//    1. gets the legend color when legend is set to changes text color
-//    2. gets the contrast color when legend is set to changes background color and contrast is applicable
-//    3. doesn’t get the legend color when legend is set to changes background color and no contrast is applicable
-//    4. all of 3. but for a “filled” icon, (like  :black_square_button: vs :white_square_button:)
-
-/*
-
-ICONS:
-
-Indicator: ANC 2 Coverage - Preg woman, black background
-Data element: ANC 2nd visit - Preg woman, outline
-Data set: Child Health - Preg woman, filled
-Event data items: Child Programme MCH Weight (g) - Scale, filled
-Program indicator: Child Programme Average weight - Scale, outline
-
-*/
