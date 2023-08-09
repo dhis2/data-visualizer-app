1279c9fbc947df28595a12292a180639f588bf58
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2023-08-09 11:54:56 +0200

    feat: use Toolbar and ToolbarSidebar from analytics (#2358)
    * feat: use Toolbar and ToolbarSidebar from analytics
    
    * feat: use UpdateButton and HoverMenubar from analytics
    
    * feat: update download menu to use hovermenu components from analytics
    
    * feat: use hover menu components from analytics for options menu
    
    * feat: use interpretations button from analytics
    
    * chore: update pot file
    
    * chore: upgrade @dhis2/analytics to latest
    
    * chore: fix typo in component name
    
    * fix: adjust e2e element slectors
    
    * chore: fix missing and redundant imports
    
    * chore: fix odd linter error which only fires on ci
    
    * fix: adjust data test name
    
    * chore: fix lines e2e test
    
    * fix: adjust closeFileMenuWithClick command
    
    * chore: fix failing legens e2e test
    
    * chore: fix legend e2e test even better
    
    * chore: fix scatter e2e
    
    * chore: fix icon e2e
    
    * chore: fix start e2e
    
    * chore: clean up unused imports
    
    * fix: remove divider from plain data source submenu
    
    * chore: upgrade @dhis2/analytics to get toolbar UI improvements
    
    * fix: tweak viz type selector styles to match toolbar
    
    * chore: remove yarn start command which was only used for development
    
    * chore: upgrade analytics to get decreased padding
    
    * fix: ensure `MenuSectionHeader` is `dense` when in `HoverMenuBar`
    
    ---------
    
    Co-authored-by: Jan Henrik Øverland <janhenrik.overland@gmail.com>

--- src/components/MenuBar/InterpretationsButton.js
+++ src/components/MenuBar/InterpretationsButton.js
@@ -1,45 +1,24 @@
-import i18n from '@dhis2/d2-i18n'
-import { IconChevronRight24, IconChevronLeft24 } from '@dhis2/ui'
-import PropTypes from 'prop-types'
+import { InterpretationsAndDetailsToggler } from '@dhis2/analytics'
 import React from 'react'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { acToggleUiRightSidebarOpen } from '../../actions/ui.js'
 import { sGetCurrent } from '../../reducers/current.js'
 import { sGetUiRightSidebarOpen } from '../../reducers/ui.js'
-import MenuButton from '../MenuButton/MenuButton.js'
-import styles from './styles/InterpretationsButton.module.css'
 
-export const UnconnectedInterpretationsButton = (props) => (
-    <MenuButton disabled={!props.id} onClick={props.onClick}>
-        {props.rightSidebarOpen ? (
-            <div className={styles.iconWrapper}>
-                <IconChevronRight24 />
-            </div>
-        ) : (
-            <div className={styles.iconWrapper}>
-                <IconChevronLeft24 />
-            </div>
-        )}
-        {i18n.t('Interpretations')}
-    </MenuButton>
-)
+export const InterpretationsButton = () => {
+    const isShowing = useSelector(sGetUiRightSidebarOpen)
+    const current = useSelector(sGetCurrent)
+    const isDisabled = !current?.id
+    const dispatch = useDispatch()
+    const onClick = () => {
+        dispatch(acToggleUiRightSidebarOpen())
+    }
 
-UnconnectedInterpretationsButton.propTypes = {
-    id: PropTypes.string,
-    rightSidebarOpen: PropTypes.bool,
-    onClick: PropTypes.func,
+    return (
+        <InterpretationsAndDetailsToggler
+            disabled={isDisabled}
+            onClick={onClick}
+            isShowing={isShowing}
+        />
+    )
 }
-
-const mapStateToProps = (state) => ({
-    rightSidebarOpen: sGetUiRightSidebarOpen(state),
-    id: (sGetCurrent(state) || {}).id,
-})
-
-const mapDispatchToProps = (dispatch) => ({
-    onClick: () => dispatch(acToggleUiRightSidebarOpen()),
-})
-
-export const InterpretationsButton = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(UnconnectedInterpretationsButton)
