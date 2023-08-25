a548ee0a003297d34e92a286878ec1cca110faf0
Edoardo Sabadelli <edoardo@dhis2.org>
2023-08-25 15:26:00 +0200

    test: add helper functions for totals options

---
+++ cypress/elements/optionsModal/totals.js
@@ -0,0 +1,100 @@
+export const colTotalsOptionEl = 'option-col-totals'
+const colSubTotalsOptionEl = 'option-col-subtotals'
+const rowTotalsOptionEl = 'option-row-totals'
+const rowSubTotalsOptionEl = 'option-row-subtotals'
+
+export const expectColumnsTotalsToBeDisabled = () =>
+    cy
+        .getBySel(colTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('be.disabled')
+
+export const expectColumnsTotalsToBeEnabled = () =>
+    cy
+        .getBySel(colTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('not.be.disabled')
+
+export const expectColumnsTotalsToBeChecked = () =>
+    cy
+        .getBySel(colTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('be.checked')
+
+export const expectColumnsTotalsToBeUnchecked = () =>
+    cy
+        .getBySel(colTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('not.be.checked')
+
+export const expectColumnsSubTotalsToBeDisabled = () =>
+    cy
+        .getBySel(colSubTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('be.disabled')
+
+export const expectColumnsSubTotalsToBeEnabled = () =>
+    cy
+        .getBySel(colSubTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('be.enabled')
+
+export const expectColumnsSubTotalsToBeChecked = () =>
+    cy
+        .getBySel(colSubTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('be.checked')
+
+export const expectColumnsSubTotalsToBeUnchecked = () =>
+    cy
+        .getBySel(colSubTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('not.be.checked')
+
+export const expectRowsTotalsToBeDisabled = () =>
+    cy
+        .getBySel(rowTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('be.disabled')
+
+export const expectRowsTotalsToBeEnabled = () =>
+    cy
+        .getBySel(rowTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('be.enabled')
+
+export const expectRowsTotalsToBeChecked = () =>
+    cy
+        .getBySel(rowTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('be.checked')
+
+export const expectRowsTotalsToBeUnchecked = () =>
+    cy
+        .getBySel(rowTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('not.be.checked')
+
+export const expectRowsSubTotalsToBeDisabled = () =>
+    cy
+        .getBySel(rowSubTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('be.disabled')
+
+export const expectRowsSubTotalsToBeEnabled = () =>
+    cy
+        .getBySel(rowSubTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('be.enabled')
+
+export const expectRowsSubTotalsToBeChecked = () =>
+    cy
+        .getBySel(rowSubTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('be.checked')
+
+export const expectRowsSubTotalsToBeUnchecked = () =>
+    cy
+        .getBySel(rowSubTotalsOptionEl)
+        .find('[type="checkbox"]')
+        .should('not.be.checked')
