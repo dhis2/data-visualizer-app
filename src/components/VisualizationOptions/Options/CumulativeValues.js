760897b2a0b73178058448ee42dd69abd8c330e0
Edoardo Sabadelli <edoardo@dhis2.org>
2023-08-25 15:43:27 +0200

    refactor: add helper text when used in PT DHIS2-15727

--- src/components/VisualizationOptions/Options/CumulativeValues.js
+++ src/components/VisualizationOptions/Options/CumulativeValues.js
@@ -1,14 +1,23 @@
 import i18n from '@dhis2/d2-i18n'
+import PropTypes from 'prop-types'
 import React from 'react'
 import { CheckboxBaseOption } from './CheckboxBaseOption.js'
 
-const CumulativeValues = () => (
+const CumulativeValues = ({ forPT }) => (
     <CheckboxBaseOption
         label={i18n.t('Cumulative values')}
+        helperText={
+            forPT ? i18n.t('Accumulate values horizontally TODO') : null
+        }
         option={{
             name: 'cumulativeValues',
         }}
+        dataTest="option-cumulative-values"
     />
 )
+
+CumulativeValues.propTypes = {
+    forPT: PropTypes.bool,
+}
 
 export default CumulativeValues
