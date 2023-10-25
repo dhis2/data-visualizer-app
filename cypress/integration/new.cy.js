e9c64feb9da97f191cbd5de7e07c96d2a4ba8569
Jen Jones Arnesen <jennifer@dhis2.org>
2023-10-25 10:39:50 +0200

    chore: test cleanup and use non-recording e2e-prod

--- cypress/integration/new.cy.js
+++ cypress/integration/new.cy.js
@@ -41,64 +41,61 @@ const TEST_INDICATOR_NAMES = TEST_INDICATORS.slice(1, 3).map(
 )
 
 describe('creating a new AO', () => {
-    it('navigates to the start page', () => {
-        goToStartPage()
-    })
     visTypes.forEach((visType) => {
         const visTypeName = visTypeDisplayNames[visType]
-        describe(visTypeName, () => {
-            it(`create AO of type ${visTypeName}`, () => {
-                // creates a new AO
-                createNewAO()
-                expectStoreCurrentToBeEmpty()
-                expectVisualizationToNotBeVisible()
-                expectVisTypeToBeDefault()
+        it(`create AO of type ${visTypeName}`, () => {
+            // navigates to start page
+            goToStartPage()
 
-                // changes vis type
-                changeVisType(visTypeName)
-                expectVisTypeToBeValue(visTypeName)
+            // creates a new AO
+            createNewAO()
+            expectStoreCurrentToBeEmpty()
+            expectVisualizationToNotBeVisible()
+            expectVisTypeToBeDefault()
 
-                // adds dimensions
-                openDimension(DIMENSION_ID_DATA)
+            // changes vis type
+            changeVisType(visTypeName)
+            expectVisTypeToBeValue(visTypeName)
 
-                if (visType === VIS_TYPE_SCATTER) {
-                    selectIndicators(TEST_INDICATOR_NAMES.slice(0, 1))
-                    switchDataTab('Horizontal')
-                    selectDataElements(TEST_DATA_ELEMENT_NAMES.slice(0, 1))
-                } else {
-                    if (getAxisMaxNumberOfItems(visType, TEST_AXIS_ID) === 1) {
-                        // Gauge and SV can only have 1 data item
-                        TEST_DATA_ELEMENT_NAMES.splice(1)
-                    }
+            // adds dimensions
+            openDimension(DIMENSION_ID_DATA)
 
-                    selectDataElements(TEST_DATA_ELEMENT_NAMES)
+            if (visType === VIS_TYPE_SCATTER) {
+                selectIndicators(TEST_INDICATOR_NAMES.slice(0, 1))
+                switchDataTab('Horizontal')
+                selectDataElements(TEST_DATA_ELEMENT_NAMES.slice(0, 1))
+            } else {
+                if (getAxisMaxNumberOfItems(visType, TEST_AXIS_ID) === 1) {
+                    // Gauge and SV can only have 1 data item
+                    TEST_DATA_ELEMENT_NAMES.splice(1)
                 }
 
-                clickDimensionModalUpdateButton()
+                selectDataElements(TEST_DATA_ELEMENT_NAMES)
+            }
 
-                expectVisualizationToBeVisible(visType)
+            clickDimensionModalUpdateButton()
 
-                isYearOverYear(visType) && expectAOTitleToBeUnsaved()
+            expectVisualizationToBeVisible(visType)
 
-                // FIXME: Store is always in default state
-                /* !isYearOverYear(visType)
+            isYearOverYear(visType) && expectAOTitleToBeUnsaved()
+
+            // FIXME: Store is always in default state
+            /* !isYearOverYear(visType)
                     ? expectStoreCurrentColumnsToHaveLength(1)
                     : expectAOTitleToBeUnsaved() */
 
-                if (visType !== VIS_TYPE_SCATTER) {
-                    TEST_DATA_ELEMENT_NAMES.forEach((item) =>
-                        expectChartToContainDimensionItem(visType, item)
-                    )
-                }
-            })
+            if (visType !== VIS_TYPE_SCATTER) {
+                TEST_DATA_ELEMENT_NAMES.forEach((item) =>
+                    expectChartToContainDimensionItem(visType, item)
+                )
+            }
 
             if ([VIS_TYPE_SINGLE_VALUE, VIS_TYPE_GAUGE].includes(visType)) {
-                it('Data is locked to Series', () => {
-                    expectDimensionOnAxisToHaveLockIcon(
-                        DIMENSION_ID_DATA,
-                        AXIS_ID_COLUMNS
-                    )
-                })
+                // Data is locked to Series
+                expectDimensionOnAxisToHaveLockIcon(
+                    DIMENSION_ID_DATA,
+                    AXIS_ID_COLUMNS
+                )
             }
         })
     })
