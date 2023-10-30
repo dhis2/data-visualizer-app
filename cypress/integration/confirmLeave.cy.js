8e9e2df9ba38812ee5667fc19ca8ddec1cbbca1a
Jen Jones Arnesen <jennifer@dhis2.org>
2023-10-30 15:40:56 +0100

    chore: one more

--- cypress/integration/confirmLeave.cy.js
+++ cypress/integration/confirmLeave.cy.js
@@ -17,35 +17,37 @@ import {
 import { TEST_AOS } from '../utils/data.js'
 import { getRandomArrayItem } from '../utils/random.js'
 
-test('Confirm leave modal preserves AO changes when user cancels leave, and discards changes when user approves leave', () => {
-    const TEST_AO = getRandomArrayItem(TEST_AOS)
-    // navigates to the start page and loads a random saved AO
-    goToStartPage()
-    openAOByName(TEST_AO.name)
-    expectVisualizationToBeVisible(TEST_AO.type)
-    expectAOTitleToNotBeDirty()
+describe('Confirm leave modal', () => {
+    it('Confirm leave modal preserves AO changes when user cancels leave, and discards changes when user approves leave', () => {
+        const TEST_AO = getRandomArrayItem(TEST_AOS)
+        // navigates to the start page and loads a random saved AO
+        goToStartPage()
+        openAOByName(TEST_AO.name)
+        expectVisualizationToBeVisible(TEST_AO.type)
+        expectAOTitleToNotBeDirty()
 
-    // replaces the selected period
-    replacePeriodItems(TEST_AO.type)
-    expectVisualizationToBeVisible(TEST_AO.type)
-    expectAOTitleToBeDirty()
+        // replaces the selected period
+        replacePeriodItems(TEST_AO.type)
+        expectVisualizationToBeVisible(TEST_AO.type)
+        expectAOTitleToBeDirty()
 
-    // tries to open a new AO
-    createNewAO()
-    expectConfirmLeaveModalToBeVisible()
+        // tries to open a new AO
+        createNewAO()
+        expectConfirmLeaveModalToBeVisible()
 
-    // cancels leave
-    confirmLeave(false)
-    expectVisualizationToBeVisible(TEST_AO.type)
-    expectAOTitleToBeDirty()
+        // cancels leave
+        confirmLeave(false)
+        expectVisualizationToBeVisible(TEST_AO.type)
+        expectAOTitleToBeDirty()
 
-    // tries to open a new AO
-    createNewAO()
-    expectConfirmLeaveModalToBeVisible()
+        // tries to open a new AO
+        createNewAO()
+        expectConfirmLeaveModalToBeVisible()
 
-    // confirms leave
-    confirmLeave(true)
-    expectStartScreenToBeVisible()
-    expectVisualizationToNotBeVisible()
-    expectAOTitleToNotBeDirty()
+        // confirms leave
+        confirmLeave(true)
+        expectStartScreenToBeVisible()
+        expectVisualizationToNotBeVisible()
+        expectAOTitleToNotBeDirty()
+    })
 })
