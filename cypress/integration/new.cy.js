cadf29126f16e0679daa6ac0de14d13623a52f56
Jen Jones Arnesen <jennifer@dhis2.org>
2023-10-31 11:23:22 +0100

    chore: consolidate cypress tests (#2890)
    To follow cypress best practice, consolidate assertions into single tests when
    they represent a single logical user journey
    
    Positive side effects (in addition to those listed in the best practice page):
    * less tests contributing to our monthly cypress subscription total
    * less skewed impression of coverage
    
    With these changes the number of tests has gone down from 637 to 569

--- cypress/integration/new.cy.js
+++ cypress/integration/new.cy.js
@@ -41,62 +41,61 @@ const TEST_INDICATOR_NAMES = TEST_INDICATORS.slice(1, 3).map(
 )
 
 describe('creating a new AO', () => {
-    it('navigates to the start page', () => {
-        goToStartPage()
-    })
     visTypes.forEach((visType) => {
         const visTypeName = visTypeDisplayNames[visType]
-        describe(visTypeName, () => {
-            it('creates a new AO', () => {
-                createNewAO()
-                expectStoreCurrentToBeEmpty()
-                expectVisualizationToNotBeVisible()
-                expectVisTypeToBeDefault()
-            })
-            it('changes vis type', () => {
-                changeVisType(visTypeName)
-                expectVisTypeToBeValue(visTypeName)
-            })
-            it('adds dimensions', () => {
-                openDimension(DIMENSION_ID_DATA)
 
-                if (visType === VIS_TYPE_SCATTER) {
-                    selectIndicators(TEST_INDICATOR_NAMES.slice(0, 1))
-                    switchDataTab('Horizontal')
-                    selectDataElements(TEST_DATA_ELEMENT_NAMES.slice(0, 1))
-                } else {
-                    if (getAxisMaxNumberOfItems(visType, TEST_AXIS_ID) === 1) {
-                        // Gauge and SV can only have 1 data item
-                        TEST_DATA_ELEMENT_NAMES.splice(1)
-                    }
+        it(`create AO of type ${visTypeName}`, () => {
+            // navigates to start page
+            goToStartPage()
 
-                    selectDataElements(TEST_DATA_ELEMENT_NAMES)
+            // creates a new AO
+            createNewAO()
+            expectStoreCurrentToBeEmpty()
+            expectVisualizationToNotBeVisible()
+            expectVisTypeToBeDefault()
+
+            // changes vis type
+            changeVisType(visTypeName)
+            expectVisTypeToBeValue(visTypeName)
+
+            // adds dimensions
+            openDimension(DIMENSION_ID_DATA)
+
+            if (visType === VIS_TYPE_SCATTER) {
+                selectIndicators(TEST_INDICATOR_NAMES.slice(0, 1))
+                switchDataTab('Horizontal')
+                selectDataElements(TEST_DATA_ELEMENT_NAMES.slice(0, 1))
+            } else {
+                if (getAxisMaxNumberOfItems(visType, TEST_AXIS_ID) === 1) {
+                    // Gauge and SV can only have 1 data item
+                    TEST_DATA_ELEMENT_NAMES.splice(1)
                 }
 
-                clickDimensionModalUpdateButton()
+                selectDataElements(TEST_DATA_ELEMENT_NAMES)
+            }
 
-                expectVisualizationToBeVisible(visType)
+            clickDimensionModalUpdateButton()
 
-                isYearOverYear(visType) && expectAOTitleToBeUnsaved()
+            expectVisualizationToBeVisible(visType)
 
-                // FIXME: Store is always in default state
-                /* !isYearOverYear(visType)
+            isYearOverYear(visType) && expectAOTitleToBeUnsaved()
+
+            // FIXME: Store is always in default state
+            /* !isYearOverYear(visType)
                     ? expectStoreCurrentColumnsToHaveLength(1)
                     : expectAOTitleToBeUnsaved() */
 
-                if (visType !== VIS_TYPE_SCATTER) {
-                    TEST_DATA_ELEMENT_NAMES.forEach((item) =>
-                        expectChartToContainDimensionItem(visType, item)
-                    )
-                }
-            })
+            if (visType !== VIS_TYPE_SCATTER) {
+                TEST_DATA_ELEMENT_NAMES.forEach((item) =>
+                    expectChartToContainDimensionItem(visType, item)
+                )
+            }
+
             if ([VIS_TYPE_SINGLE_VALUE, VIS_TYPE_GAUGE].includes(visType)) {
-                it('Data is locked to Series', () => {
-                    expectDimensionOnAxisToHaveLockIcon(
-                        DIMENSION_ID_DATA,
-                        AXIS_ID_COLUMNS
-                    )
-                })
+                expectDimensionOnAxisToHaveLockIcon(
+                    DIMENSION_ID_DATA,
+                    AXIS_ID_COLUMNS
+                )
             }
         })
     })
