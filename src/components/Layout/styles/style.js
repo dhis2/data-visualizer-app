1dd2b8638403c4a9b25832249012ffee75e2d1f6
Joseph John Aas Cooper <joe@dhis2.org>
2023-12-14 14:40:06 +0100

    fix: adjust layout height for new chip design

--- src/components/Layout/styles/style.js
+++ src/components/Layout/styles/style.js
@@ -1,10 +1,10 @@
 import { colors } from '@dhis2/ui'
 
 // Layout
-export const LAYOUT_HEIGHT = '78px'
+export const LAYOUT_HEIGHT = '70px'
 
 // Axis
-export const AXIS_PADDING = '4px 4px 2px 6px'
+export const AXIS_PADDING = '4px 4px 4px 6px'
 export const AXIS_LABEL_PADDING = '2px 0px 0px 4px'
 export const AXIS_BORDER_COLOR = colors.grey300
 export const AXIS_BORDER_STYLE = 'solid'
