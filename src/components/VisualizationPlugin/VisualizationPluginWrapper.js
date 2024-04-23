827ef3712a5a83687451a5545bae475d02ce601a
Jen Jones Arnesen <jennifer@dhis2.org>
2024-04-23 09:53:32 +0200

    fix: plugin flashes when interacting with Interpretations modal (#3017)
    Fixes DHIS2-15570
    Fixes DHIS2-17137
    
    Prevent re-requests for analytics, and resulting re-renders that were giving a flashing effect.
    
    * InterpretationModal: memoize the callback to avoid a re-render due to a "new" function
    being passed with each render of InterpretationModal.
    The effect of this change is minor, probably just 1 rerender
    
    * VisualizationPlugin: ensure that the size of the plugin is set when it is coming
    from the modal so that when changing the width of the viewport,
    the visualization adjusts its size accordingly. (Previously this "worked" but only
    because of all the re-fetch/re-renders)
    
    * VisualizationPluginWrapper: need to listen to the individual properties,
    rather than the whole props object since that was triggering lots of
    unnecessary re-fetch/renders.
    
    * set the max-height to respect viewport so that vertical resize causes
    the chart to adjust accordingly

--- src/components/VisualizationPlugin/VisualizationPluginWrapper.js
+++ src/components/VisualizationPlugin/VisualizationPluginWrapper.js
@@ -1,4 +1,5 @@
 import { CenteredContent, CircularLoader, ComponentCover } from '@dhis2/ui'
+import PropTypes from 'prop-types'
 import React, { useCallback, useEffect, useState } from 'react'
 import { VisualizationPlugin } from '../VisualizationPlugin/VisualizationPlugin.js'
 
@@ -28,9 +29,38 @@ const VisualizationPluginWrapper = (props) => {
     )
 
     useEffect(() => {
+        setPluginProps({
+            displayProperty: props.displayProperty,
+            visualization: props.visualization,
+            filters: props.filters,
+            forDashboard: props.forDashboard,
+            id: props.id,
+            isInModal: props.isInModal,
+            style: props.style,
+            onChartGenerated: props.onChartGenerated,
+            onDrill: props.onDrill,
+            onError: props.onError,
+            onResponsesReceived: props.onResponsesReceived,
+        })
+    }, [
+        props.displayProperty,
+        props.visualization,
+        props.filters,
+        props.forDashboard,
+        props.id,
+        props.isInModal,
+        props.style,
+        props.onChartGenerated,
+        props.onDrill,
+        props.onError,
+        props.onResponsesReceived,
+    ])
+
+    // set loading state only for props that will cause
+    // VisualizationPlugin to fetch and call onLoadingComplete
+    useEffect(() => {
         setIsLoading(true)
-        setPluginProps(props)
-    }, [props])
+    }, [props.filters, props.forDashboard, props.visualization])
 
     const onLoadingComplete = () => setIsLoading(false)
 
@@ -50,6 +80,21 @@ const VisualizationPluginWrapper = (props) => {
             />
         </>
     )
+}
+
+VisualizationPluginWrapper.propTypes = {
+    displayProperty: PropTypes.string.isRequired,
+    visualization: PropTypes.object.isRequired,
+    className: PropTypes.string,
+    filters: PropTypes.object,
+    forDashboard: PropTypes.bool,
+    id: PropTypes.number,
+    isInModal: PropTypes.bool,
+    style: PropTypes.object,
+    onChartGenerated: PropTypes.func,
+    onDrill: PropTypes.func,
+    onError: PropTypes.func,
+    onResponsesReceived: PropTypes.func,
 }
 
 export { VisualizationPluginWrapper }
