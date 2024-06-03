f02d622f7bddf33f5e59963a25ce0b9fc7e71ac1
Edoardo Sabadelli <edoardo@dhis2.org>
2024-06-03 15:35:02 +0200

    refactor: use shared plugin wrapper component

--- src/PluginWrapper.js
+++ src/PluginWrapper.js
@@ -1,105 +1,14 @@
-import { useCacheableSection, CacheableSection } from '@dhis2/app-runtime'
-import { CssVariables, CenteredContent, CircularLoader, Layer } from '@dhis2/ui'
-import postRobot from '@krakenjs/post-robot'
+import { DashboardPluginWrapper } from '@dhis2/analytics'
 import debounce from 'lodash-es/debounce'
-import PropTypes from 'prop-types'
 import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { VisualizationPluginWrapper } from './components/VisualizationPlugin/VisualizationPluginWrapper.js'
-import { getPWAInstallationStatus } from './modules/getPWAInstallationStatus.js'
+import './locales/index.js'
 
-const LoadingMask = () => {
-    return (
-        <Layer>
-            <CenteredContent>
-                <CircularLoader />
-            </CenteredContent>
-        </Layer>
-    )
-}
-
-const CacheableSectionWrapper = ({
-    id,
-    children,
-    cacheNow,
-    isParentCached,
-}) => {
-    const { startRecording, isCached, remove } = useCacheableSection(id)
-
-    useEffect(() => {
-        if (cacheNow) {
-            startRecording({ onError: console.error })
-        }
-
-        // NB: Adding `startRecording` to dependencies causes
-        // an infinite recording loop as-is (probably need to memoize it)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [cacheNow])
-
-    useEffect(() => {
-        const listener = postRobot.on(
-            'removeCachedData',
-            // todo: check domain too; differs based on deployment env though
-            { window: window.parent },
-            () => remove()
-        )
-
-        return () => listener.cancel()
-    }, [remove])
-
-    useEffect(() => {
-        // Synchronize cache state on load or prop update
-        // -- a back-up to imperative `removeCachedData`
-        if (!isParentCached && isCached) {
-            remove()
-        }
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isParentCached])
-
-    return (
-        <CacheableSection id={id} loadingMask={LoadingMask}>
-            {children}
-        </CacheableSection>
-    )
-}
-CacheableSectionWrapper.propTypes = {
-    cacheNow: PropTypes.bool,
-    children: PropTypes.node,
-    id: PropTypes.string,
-    isParentCached: PropTypes.bool,
-}
-
-const sendInstallationStatus = (installationStatus) => {
-    postRobot.send(window.parent, 'installationStatus', { installationStatus })
-}
-
-const PluginWrapper = () => {
-    const [propsFromParent, setPropsFromParent] = useState()
+const PluginWrapper = (props) => {
+    const [propsFromParent, setPropsFromParent] = useState(props)
     const [renderId, setRenderId] = useState(null)
 
-    const receivePropsFromParent = (event) => setPropsFromParent(event.data)
-
-    useEffect(() => {
-        postRobot
-            .send(window.parent, 'getProps')
-            .then(receivePropsFromParent)
-            .catch((err) => console.error(err))
-
-        // Get & send PWA installation status now, and also prepare to send
-        // future updates (installing/ready)
-        getPWAInstallationStatus({
-            onStateChange: sendInstallationStatus,
-        }).then(sendInstallationStatus)
-
-        // Allow parent to update props
-        const listener = postRobot.on(
-            'newProps',
-            { window: window.parent /* Todo: check domain */ },
-            receivePropsFromParent
-        )
-
-        return () => listener.cancel()
-    }, [])
+    useEffect(() => setPropsFromParent(props), [props])
 
     useLayoutEffect(() => {
         const updateRenderId = debounce(
@@ -115,27 +24,14 @@ const PluginWrapper = () => {
         return () => window.removeEventListener('resize', updateRenderId)
     }, [])
 
-    return propsFromParent ? (
-        <div
-            style={{
-                display: 'flex',
-                height: '100%',
-                overflow: 'hidden',
+    return (
+        <DashboardPluginWrapper {...propsFromParent}>
+            {(props) => {
+                console.log('props from shared', props)
+                return <VisualizationPluginWrapper id={renderId} {...props} />
             }}
-        >
-            <CacheableSectionWrapper
-                id={propsFromParent.cacheId}
-                cacheNow={propsFromParent.recordOnNextLoad}
-                isParentCached={propsFromParent.isParentCached}
-            >
-                <VisualizationPluginWrapper
-                    id={renderId}
-                    {...propsFromParent}
-                />
-            </CacheableSectionWrapper>
-            <CssVariables colors spacers elevations />
-        </div>
-    ) : null
+        </DashboardPluginWrapper>
+    )
 }
 
 export default PluginWrapper
