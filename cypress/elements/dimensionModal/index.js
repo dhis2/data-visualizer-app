ebc08061fe1d099f08bf2a07cfbf7d60b6615f56
Edoardo Sabadelli <edoardo@dhis2.org>
2024-10-18 18:24:11 +0200

    fix: compute subtotals/totals for boolean types (DHIS2-9155) (#3194)
    * fix: compute subtotals/totals for boolean types (DHIS2-9155)
    
    * test: add regression test for totals with boolean value types
    
    * fix: solve sizing/visibility issues in dashboard
    
    Since cli-app-scripts has already been updated to a version with the new
    plugin components, charts and PT have problems in dashboard because they
    still use the postRobot communication internally and the
    dashboard version does not use the new platform components.
    The skipPluginLogic flag avoids the issues by disabling the whole
    implementation in the platform plugin components at build time.
    
    * chore: bump analytics version
    
    * test: add tests for totals/cumulative with mixed valueTypes/totalAggregationTypes

--- cypress/elements/dimensionModal/index.js
+++ cypress/elements/dimensionModal/index.js
@@ -107,6 +107,7 @@ export const expectSelectedItemsAmountToBe = (amount) =>
 
 export {
     selectDataElements,
+    selectDataItems,
     selectIndicators,
     switchDataTab,
     expectDataDimensionModalWarningToContain,
