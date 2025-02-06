f387686f4d1a897824e803cfac50881c618ae773
Kai Vandivier <kpvandivier@gmail.com>
2025-02-06 16:25:10 +0100

    chore: format all files

--- config/testSetup.js
+++ config/testSetup.js
@@ -1,5 +1,5 @@
 import 'jest-enzyme'
-import Enzyme from 'enzyme'
 import Adapter from '@cfaester/enzyme-adapter-react-18'
+import Enzyme from 'enzyme'
 
 Enzyme.configure({ adapter: new Adapter() })
