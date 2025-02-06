e24265bd46662f948144dab9851765de76292104
Kai Vandivier <kpvandivier@gmail.com>
2025-02-06 16:24:59 +0100

    chore: update test dependencies

--- config/testSetup.js
+++ config/testSetup.js
@@ -1,5 +1,5 @@
 import 'jest-enzyme'
 import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@cfaester/enzyme-adapter-react-18'
 
 Enzyme.configure({ adapter: new Adapter() })
