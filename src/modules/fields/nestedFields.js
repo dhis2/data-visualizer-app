23fee6170d7815ca870ddbc7173110c36e7e40e8
Edoardo Sabadelli <edoardo@dhis2.org>
2025-02-18 12:11:39 +0100

    fix: fetch and extract option set id
    This information is not returned as part of columns/rows/filters but it
    can be extracted from dataDimensionItems.
    The optionSetId is also added to the metadata when fetching the
    dataItems in the Data modal.
    The rest of the code relies on the presence of optionSetId in metadata
    in order to enable the option view mode.

--- src/modules/fields/nestedFields.js
+++ src/modules/fields/nestedFields.js
@@ -9,16 +9,25 @@ const DIMENSION_ITEM = `dimensionItem~rename(${ID})`
 const LEGEND_SET = `${ID},${NAME}`
 const USER = `${NAME},userCredentials[username]`
 
-const ITEMS = `${DIMENSION_ITEM},${NAME},dimensionItemType,expression,access,optionSetId`
+const ITEMS = `${DIMENSION_ITEM},${NAME},dimensionItemType,expression,access`
 
 const AXIS = `dimension,filter,legendSet[${LEGEND_SET}],items[${ITEMS}]`
 const INTERPRETATIONS = 'id,created'
 const LEGEND = `showKey,style,strategy,set[${LEGEND_SET}]`
 
+const OPTION_SET = `optionSet[${ID}]`
+
 const DIMENSION_PROPS = dimensionMetadataProps
-    .map((prop) => `${prop}[${ITEMS}]`)
+    .map((prop) => {
+        if (prop === 'programAttribute') {
+            return `${prop}[${ITEMS},attribute[${OPTION_SET}]]`
+        } else if (prop === 'programDataElement') {
+            return `${prop}[${ITEMS},dataElement[${OPTION_SET}]]`
+        } else {
+            return `${prop}[${ITEMS}]`
+        }
+    })
     .join(',')
-const DIMENSION_ITEMS = `dataDimensionItemType,${DIMENSION_PROPS}`
 
 // nested fields map
 export const nestedFields = {
@@ -28,7 +37,7 @@ export const nestedFields = {
     user: USER,
     interpretations: INTERPRETATIONS,
     legend: LEGEND,
-    dataDimensionItems: DIMENSION_ITEMS,
+    dataDimensionItems: DIMENSION_PROPS,
 }
 
 export const extendFields = (field) =>
