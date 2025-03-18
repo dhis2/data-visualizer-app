4e710c215bea9b853ad0b6f9ec214fbf72143565
Kai Vandivier <49666798+KaiVandivier@users.noreply.github.com>
2025-03-18 10:46:06 +0100

    fix: add hash routing and plugin support for the upcoming global shell [DHIS2-19061] (#3363)
    To enable shareable app deep links and to maintain hash route location, the Global Shell
    watches for popstate events inside client apps. To make listenable events for the global
    shell, the main history.listen() listener now publishes a popstate
    event that can be watched.
    
    Also adds a fix for loading the app as a plugin in Dashboard within the global shell.

--- src/components/__tests__/App.spec.js
+++ src/components/__tests__/App.spec.js
@@ -130,6 +130,42 @@ describe('App', () => {
             })
         })
 
+        it('triggers popstate events on history push events', (done) => {
+            props.location.pathname = '/rarity'
+
+            const popstateHandler = jest.fn()
+            window.addEventListener('popstate', popstateHandler)
+
+            app()
+
+            setTimeout(() => {
+                history.push('/rainbowdash')
+                expect(popstateHandler).toBeCalledTimes(1)
+
+                window.removeEventListener('popstate', popstateHandler)
+
+                done()
+            })
+        })
+
+        it('triggers popstate events on history replace events', (done) => {
+            props.location.pathname = '/rarity'
+
+            const popstateHandler = jest.fn()
+            window.addEventListener('popstate', popstateHandler)
+
+            app()
+
+            setTimeout(() => {
+                history.replace('/rainbowdash')
+                expect(popstateHandler).toBeCalledTimes(1)
+
+                window.removeEventListener('popstate', popstateHandler)
+
+                done()
+            })
+        })
+
         it('reloads visualization when opening the same visualization', (done) => {
             props.location.pathname = '/fluttershy'
 
