c189e2348c39e77dc58cb59d4c351fd93de94c06
HendrikThePendric <hendrik@dhis2.org>
2025-04-15 13:34:36 +0200

    chore: use old login mechanism combined with cy.loginByApi

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,4 +1,3 @@
-import { enableAutoLogin } from '@dhis2/cypress-commands'
 import './commands.js'
 
 Cypress.on('uncaught:exception', (err) => {
@@ -20,4 +19,56 @@ Cypress.on('uncaught:exception', (err) => {
     }
 })
 
-enableAutoLogin()
+const SESSION_COOKIE_NAME = 'JSESSIONID'
+const LOCAL_STORAGE_KEY = 'DHIS2_BASE_URL'
+
+// '2.39' or 39?
+const computeEnvVariableName = (instanceVersion) =>
+    typeof instanceVersion === 'number'
+        ? `${SESSION_COOKIE_NAME}_${instanceVersion}`
+        : `${SESSION_COOKIE_NAME}_${instanceVersion.split('.').pop()}`
+
+const findSessionCookieForBaseUrl = (baseUrl, cookies) =>
+    cookies.find(
+        (cookie) =>
+            cookie.name === SESSION_COOKIE_NAME && baseUrl.includes(cookie.path)
+    )
+
+before(() => {
+    const username = Cypress.env('dhis2Username')
+    const password = Cypress.env('dhis2Password')
+    const baseUrl = Cypress.env('dhis2BaseUrl')
+    const instanceVersion = Cypress.env('dhis2InstanceVersion')
+
+    cy.loginByApi({ username, password, baseUrl })
+
+    cy.getAllCookies()
+        .should((cookies) => {
+            expect(cookies.length).to.be.at.least(1)
+        })
+        .then((cookies) => {
+            const sessionCookieForBaseUrl = findSessionCookieForBaseUrl(
+                baseUrl,
+                cookies
+            )
+            Cypress.env(
+                computeEnvVariableName(instanceVersion),
+                JSON.stringify(sessionCookieForBaseUrl)
+            )
+        })
+})
+
+beforeEach(() => {
+    const baseUrl = Cypress.env('dhis2BaseUrl')
+    const instanceVersion = Cypress.env('dhis2InstanceVersion')
+    const envVariableName = computeEnvVariableName(instanceVersion)
+    const { name, value, ...options } = JSON.parse(Cypress.env(envVariableName))
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, baseUrl)
+    cy.setCookie(name, value, options)
+
+    cy.getAllCookies().should((cookies) => {
+        expect(findSessionCookieForBaseUrl(baseUrl, cookies)).to.exist
+        expect(localStorage.getItem(LOCAL_STORAGE_KEY)).to.equal(baseUrl)
+    })
+})
