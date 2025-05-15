4dc099b2d8de670fbf501174621896daa1c337a9
HendrikThePendric <hendrik@dhis2.org>
2025-05-15 16:49:02 +0200

    fix: fall back to default colors when exporting line charts with pattern to PDF

--- src/components/DownloadMenu/useDownload.js
+++ src/components/DownloadMenu/useDownload.js
@@ -16,6 +16,7 @@ import {
     sGetUiLayoutRows,
 } from '../../reducers/ui.js'
 import { useChartContext } from '../ChartProvider.js'
+import { computeChartOptionsForExport } from './computeChartOptionsForExport.js'
 import {
     DOWNLOAD_TYPE_PLAIN,
     DOWNLOAD_TYPE_TABLE,
@@ -48,73 +49,6 @@ const addCommonParameters = (req, visualization, options) => {
     return req
 }
 
-const getChartOptionsForExportType = (isPdfExport, titleStyle, subtitleStyle) =>
-    isPdfExport
-        ? {
-              /* Custom visualization types (i.e. SingleValue) that need some
-               * specific handling for PDF export can read this when they
-               * re-render before exporting */
-              isPdfExport: true,
-              /* The font-family selected here impacts text alignment in the PDF
-               * that is produced. We set it to Helvetica here because this is a
-               * standard PDF font and a fallback font of our app. In theory it
-               * would make more sense to select base font-family Noto Sans which
-               * is actually used in the generated PDF, but for some reason that
-               * proved to result in poorer text alignment than Helvetica.
-               * Specifically we observed label/legend text overlap. */
-              chart: {
-                  style: { fontFamily: 'Helvetica' },
-              },
-              /* Text ellipsis and text shadow are not supported in PDF exports
-               * so the corresponding style properties need to be set to their
-               * default values when exporting to PDF */
-              title: {
-                  style: {
-                      overflow: 'auto',
-                      textOverflow: 'clip',
-                      whiteSpace: 'wrap',
-                  },
-              },
-              subtitle: {
-                  style: {
-                      overflow: 'auto',
-                      textOverflow: 'clip',
-                      textShadow: 'none',
-                      whiteSpace: 'wrap',
-                  },
-              },
-          }
-        : {
-              isPdfExport: false,
-              /* Currently preserving webfonts when exporting to PNG is not
-               * working in Highcharts. I filed an issue about that, see
-               * https://github.com/highcharts/highcharts/issues/22914
-               * For now it is better to first set the font to a websafe font
-               * before exporting to avoid alignment issues. The font-family for
-               * our charts is:
-               * `Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif`
-               * The first websafe font among these is Arial. */
-              chart: { style: { fontFamily: 'Arial, sans-serif' } },
-              /* Text ellipsis and shadow styles do work for PNG export.
-               * They need to be explicitely set to the original chart values
-               * in case a chart is first exported to PDF and then to PNG */
-              title: {
-                  style: {
-                      overflow: titleStyle.overflow,
-                      textOverflow: titleStyle.textOverflow,
-                      whiteSpace: titleStyle.whiteSpace,
-                  },
-              },
-              subtitle: {
-                  style: {
-                      overflow: subtitleStyle.overflow,
-                      textOverflow: subtitleStyle.textOverflow,
-                      textShadow: subtitleStyle.textShadow,
-                      whiteSpace: subtitleStyle.whiteSpace,
-                  },
-              },
-          }
-
 const useDownload = (relativePeriodDate) => {
     const displayProperty = useSelector(sGetSettingsDisplayProperty)
     const visualization = useSelector(sGetCurrent)
@@ -140,10 +74,10 @@ const useDownload = (relativePeriodDate) => {
             }
 
             const isPdfExport = format === FILE_FORMAT_PDF
-            const chartOptions = getChartOptionsForExportType(
+            const chartOptions = computeChartOptionsForExport(
                 isPdfExport,
-                chart.options.title.style,
-                chart.options.subtitle.style
+                visualization.type,
+                chart.options
             )
 
             /* In theory it should be possible to specify the chart options in the call
