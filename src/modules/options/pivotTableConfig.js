d72c840fff3392ccc4efe7c5fdc8b5e668e1da53
Edoardo Sabadelli <edoardo@dhis2.org>
2025-05-20 14:53:37 +0200

    fix: add missing measureCriteria option in PT options list

--- src/modules/options/pivotTableConfig.js
+++ src/modules/options/pivotTableConfig.js
@@ -158,6 +158,8 @@ export const pivotTableOptionNames = () => [
     'fixRowHeaders',
     'showHierarchy',
 
+    'measureCriteria',
+
     'reportingPeriod',
     'organisationUnit',
     'parentOrganisationUnit',
