f2b0385ea140c396fa8b6878e980f4b34fd38f63
Jen Jones Arnesen <jennifer@dhis2.org>
2025-10-30 09:22:56 +0100

    test: add cypress test for pivot table

--- cypress/integration/interpretations.cy.js
+++ cypress/integration/interpretations.cy.js
@@ -1,4 +1,4 @@
-import { VIS_TYPE_BAR } from '@dhis2/analytics'
+import { VIS_TYPE_BAR, VIS_TYPE_PIVOT_TABLE } from '@dhis2/analytics'
 import {
     expectAOTitleToBeValue,
     expectVisualizationToBeVisible,
@@ -11,6 +11,38 @@ describe('Interpretations', () => {
         const ao = {
             name: 'ANC: 1 and 3 coverage Yearly',
             type: VIS_TYPE_BAR,
+        }
+
+        // Open the saved AO
+        goToStartPage()
+        openAOByName(ao.name)
+        expectAOTitleToBeValue(ao.name)
+        expectVisualizationToBeVisible(ao.type)
+
+        // Open the interpretations panel
+        cy.getBySel('dhis2-analytics-interpretationsanddetailstoggler').click()
+
+        cy.getBySel('interpretations-list')
+            .find('button')
+            .contains('See interpretation')
+            .click()
+
+        cy.getBySel('interpretation-modal').should('be.visible')
+
+        cy.get('.highcharts-container').should('be.visible')
+
+        cy.getBySel('interpretation-modal')
+            .find('button')
+            .contains('Hide interpretation')
+            .click()
+
+        cy.getBySel('interpretation-modal').should('not.exist')
+    })
+
+    it('opens the interpretations modal on a saved pivot table', () => {
+        const ao = {
+            name: 'ANC: ANC at facilities in Bo this month with hidden empty columns',
+            type: VIS_TYPE_PIVOT_TABLE,
         }
 
         // Open the saved AO
