457233527e038a32d308f67e621bec78b530aebb
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2025-11-03 10:17:57 +0100

    chore: update comments

--- src/modules/icon.js
+++ src/modules/icon.js
@@ -1,18 +1,15 @@
-// DHIS2-20388: In 2.42 the analytics api started to return icon names instead of absolute urls as an agreed breaking change
-// This means that "input" can be either an icon name or a full url
-// "getIconName" extracts the icon name from either input format
+// "getIconName" extracts and returns the icon name regardless of input being a name or a an absolute url"
 export const getIconName = (input) => {
     if (typeof input !== 'string' || !input.trim()) {
         return null
     }
-
     const match = input.match(/\/([^/]+)\/icon\.svg$/)
-
     return match ? match[1] : input
 }
 
-// The url coming from the api was sometimes wrong because it relied on manual input in dhis.conf
-// "getIconUrl" reconstructs the url even if input is already a url
+// "getIconUrl" gets the icon name and returns a reconstructed absolute url
+// We want to always return a reconstructed url because the url provided by the api was sometimes wrong
+// DHIS2-20388: From 2.42 the analytics api will start to return icon names instead of absolute urls as an agreed breaking change
 export const getIconUrl = (input, baseUrl) => {
     const iconName = getIconName(input)
     return iconName ? `${baseUrl}/api/icons/${iconName}/icon.svg` : null
